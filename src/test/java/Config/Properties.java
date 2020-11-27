87716fe223ac0c6dd62b238a1158960d29b91b64
pa.burinsky <burinskiypavel@gmail.com>
2020-11-27 11:35:40 +0200

    update tests

--- src/test/java/Config/Properties.java
+++ src/test/java/Config/Properties.java
@@ -5,5 +5,6 @@ public class Properties {
     public static String site = "DIPOCKET";
     public static String deviceuuid = "eC10LFCnS1mDsuNoQaa-KH";
     public static String phoneNumber = "380685448615";
+    public static String devUrl = "https://dipocket3.intranet:8900/ClientServices/v1/";
 
 }
