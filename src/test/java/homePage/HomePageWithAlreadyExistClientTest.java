67f1890c85cd70062dd646e2592d8eb015c12838
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-01-14 14:32:38 +0200

    update HomePageWithAlreadyExistClientTest

--- src/test/java/homePage/HomePageWithAlreadyExistClientTest.java
+++ src/test/java/homePage/HomePageWithAlreadyExistClientTest.java
@@ -38,6 +38,7 @@ public class HomePageWithAlreadyExistClientTest extends BaseTest {
                 .then()
                 .log().all()
                 .statusCode(400)
+                .body("errDesc", equalTo("Введите код (#1) из SMS, что б подтвердить вход на этом устройстве"))
                 .body("errCode", equalTo("DIP-00591"));
     }
 
