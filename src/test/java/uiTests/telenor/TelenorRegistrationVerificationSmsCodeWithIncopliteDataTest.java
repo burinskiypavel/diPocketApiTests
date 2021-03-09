a78f4c6f55e73d5efda514bf7d8d79d19a2a4b36
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-03-09 15:40:21 +0200

    add TelenorRegistrationVerificationSmsCodeWithIncopliteDataTest

---
+++ src/test/java/uiTests/telenor/TelenorRegistrationVerificationSmsCodeWithIncopliteDataTest.java
@@ -0,0 +1,30 @@
+package uiTests.telenor;
+
+import base.UITestBase;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertFalse;
+
+public class TelenorRegistrationVerificationSmsCodeWithIncopliteDataTest extends UITestBase {
+    String token = "515161232";
+    String phone = "380502128466";
+    String smsCode = "12345";
+
+    @Test
+    public void testRegistrationVerificationSmsCodeWithIncopliteData() {
+        gotoTelenorSiteAndDoneBasicAuth("telenor-test.dipocket.org","dipocket", "LeprechauN");
+        gotoRegisterPaymentBandPage();
+        type(By.name("publicToken"), token);
+        type(By.id("mainPhone"), phone);
+        clickCheckbox(By.id("agreeProcessInfo"));
+        submitPublicTokenAndPhone();
+        fillSmsCode(smsCode);
+        submitSmsCode();
+
+        assertFalse(isButtonEnabled(By.cssSelector("button[data-dpwa-action='register-verify-code']")));
+    }
+}
