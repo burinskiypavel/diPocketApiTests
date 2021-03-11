164af3023894806107a9e1dea529c4c216daa43e
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-03-11 15:59:28 +0200

    add TelenorLoginVerificationPhoneFieldWithIncompletePhoneTest

--- src/test/java/uiTests/telenor/TelenorLoginVerificationPhoneFieldWithIncompletePhoneTest.java
+++ src/test/java/uiTests/telenor/TelenorLoginVerificationPhoneFieldWithIncompletePhoneTest.java
@@ -8,18 +8,19 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertTrue;
 
-public class TelenorLoginVerificationWithEmptyPhoneFieldTest extends UITestBase {
+public class TelenorLoginVerificationPhoneFieldWithIncompletePhoneTest extends UITestBase {
+    String phone = "3806344";
 
-    @Test(priority = 1)
-    public void testLoginVerificationWithEmptyPhoneField() {
+    @Test
+    public void testLoginVerificationPhoneFieldWithIncompletePhon() {
         gotoTelenorSiteAndDoneBasicAuth("telenor-test.dipocket.org","dipocket", "LeprechauN");
         gotoLoginPage();
-        type(By.id("phone_number"), "");
+        type(By.id("phone_number"), phone);
         click(By.cssSelector("button.request-otp"));
         String popUpMessage = getTextFromPopUp2(By.cssSelector("div.uk-margin"));
         closePopUp2(By.xpath("//button[contains(text(), 'Ok')]"));
 
         assertTrue(isPopUpClosed3(By.cssSelector("div.uk-margin")));
-        assertThat(popUpMessage, equalTo("Phone number cannot be empty"));
+        assertThat(popUpMessage, equalTo("The phone number you entered seems invalid. Please check again"));
     }
 }
