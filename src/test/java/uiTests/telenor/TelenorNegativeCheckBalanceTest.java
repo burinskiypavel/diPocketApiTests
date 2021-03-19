97cfbcca5f6409b3257b3baeb4234412ed4cddfc
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-03-19 12:51:14 +0200

    update TelenorNegativeCheckBalanceTest

--- src/test/java/uiTests/telenor/TelenorNegativeCheckBalanceTest.java
+++ src/test/java/uiTests/telenor/TelenorNegativeCheckBalanceTest.java
@@ -2,10 +2,12 @@ package uiTests.telenor;
 
 import base.UITestBase;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertTrue;
 
 public class TelenorNegativeCheckBalanceTest extends UITestBase {
 
@@ -19,5 +21,19 @@ public class TelenorNegativeCheckBalanceTest extends UITestBase {
         String hexColor = getColorOfElement(By.id("token"), "border-color");
 
         assertThat(hexColor, equalTo(app.hexRedColor));
+    }
+
+    @Test(priority = 2)
+    public void testCheckCardNumberFieldWithInvalidData() {
+        gotoTelenorSiteAndDoneBasicAuth("telenor-test.dipocket.org","dipocket", "LeprechauN");
+        click(By.cssSelector("a[href='/en/check/balance']"));
+        waitForSeveralItems(new String[]{"Check balance", "Card number", "Cancel", "Show balance"});
+        type(By.id("token"), "888888888");
+        click(By.cssSelector("button[data-dpwa-action='check-balance']"));
+        String popUpMessage = getTextFromPopUp();
+        closePopUp(By.xpath("//button[contains(text(), 'Ok')]"));
+
+        assertTrue(isPopUpClosed());
+        assertThat(popUpMessage, equalTo("Token 888-888-888 not found. Please check token and try again"));
     }
 }
