16b0c0cb328749edc8c9f516319d036a5f31375d
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-03-22 12:13:22 +0200

    add testCheckFieldTopUpAmountWithoutData

--- src/test/java/uiTests/telenor/TelenorTopUpTest.java
+++ src/test/java/uiTests/telenor/TelenorTopUpTest.java
@@ -4,8 +4,6 @@ import base.UITestBase;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-import java.sql.SQLException;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertTrue;
@@ -14,7 +12,7 @@ public class TelenorTopUpTest extends UITestBase {
     String smsCode = "111111"; //app.generateRandomNumber(6);
 
     @Test(priority = 1)
-    public void testCheckFieldTopUpAmountWithDataGreaterThanHUF45000() throws SQLException, ClassNotFoundException, InterruptedException {
+    public void testCheckFieldTopUpAmountWithDataGreaterThanHUF45000() {
         navigateToTelenorAndLogin2(app.telenorRegistrationPhone, smsCode);
         type(By.id("dpwa-amount"), "11111111");
         click(By.cssSelector("button[data-dpwa-action='dpwa-topup']"));
@@ -23,5 +21,15 @@ public class TelenorTopUpTest extends UITestBase {
 
         assertTrue(isPopUpClosed());
         assertThat(popUpMessage, equalTo("The amount you entered is above the maximum limit, which is HUF 45000.00 for this type of transaction"));
+    }
+
+    @Test(priority = 2)
+    public void testCheckFieldTopUpAmountWithoutData() throws InterruptedException {
+        navigateToTelenorAndLogin2(app.telenorRegistrationPhone, smsCode);
+        type(By.id("dpwa-amount"), "");
+        click(By.cssSelector("button[data-dpwa-action='dpwa-topup']"));
+        String hexColor = getColorOfElement(By.id("dpwa-amount"), "border-color");
+
+        assertThat(hexColor, equalTo(app.hexRedColor));
     }
 }
