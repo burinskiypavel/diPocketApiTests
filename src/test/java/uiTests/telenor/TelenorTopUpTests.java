fc68e633c698b9f75c177c6d9dd4c185d63f8bb3
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-03-31 15:52:19 +0300

    update TelenorTopUpTest

--- src/test/java/uiTests/telenor/TelenorTopUpTests.java
+++ src/test/java/uiTests/telenor/TelenorTopUpTests.java
@@ -7,9 +7,10 @@ import org.testng.annotations.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
-public class TelenorTopUpTest extends UITestBase {
+public class TelenorTopUpTests extends UITestBase {
     String smsCode = "111111"; //app.generateRandomNumber(6);
 
     @Test(priority = 1) // moved to API tests
@@ -23,6 +24,32 @@ public class TelenorTopUpTest extends UITestBase {
         assertTrue(isPopUpClosed());
         assertThat(popUpMessage, equalTo("Minimum top-up amount is Ft 200, maximum Ft 45000"));
         assertThat(hexColor, equalTo(app.hexRedColor));
+    }
+
+    @Test(priority = 2) // moved to API tests
+    public void testCheckFieldTopUpAmountWithInvalidData() throws InterruptedException {
+        if(isElementPresent(By.id("dpwa-amount"))){
+            type(By.id("dpwa-amount"), "-0.00");
+            String popUpMessage = getTextFromPopUp();
+            String hexColor = getColorOfElement(By.id("dpwa-amount"), "border-color");
+            closePopUp(By.xpath("//button[contains(text(), 'Ok')]"));
+
+            assertTrue(isPopUpClosed());
+            assertThat(popUpMessage,equalTo("Minimum top-up amount is Ft 200, maximum Ft 45000"));
+            assertThat(hexColor, equalTo(app.hexRedColor));
+
+        } else {
+
+        navigateToTelenorAndLogin2(app.telenorRegistrationPhone, smsCode);
+        type(By.id("dpwa-amount"), "-0.00");
+        String popUpMessage = getTextFromPopUp();
+        String hexColor = getColorOfElement(By.id("dpwa-amount"), "border-color");
+        closePopUp(By.xpath("//button[contains(text(), 'Ok')]"));
+
+        assertTrue(isPopUpClosed());
+        assertThat(popUpMessage,equalTo("Minimum top-up amount is Ft 200, maximum Ft 45000"));
+        assertThat(hexColor, equalTo(app.hexRedColor));
+        }
     }
 
     @Test(priority = 3)
@@ -53,29 +80,15 @@ public class TelenorTopUpTest extends UITestBase {
         }
     }
 
-    @Test(priority = 2) // moved to API tests
-    public void testCheckFieldTopUpAmountWithInvalidData() throws InterruptedException {
-        if(isElementPresent(By.id("dpwa-amount"))){
-            type(By.id("dpwa-amount"), "-0.00");
-            String popUpMessage = getTextFromPopUp();
-            String hexColor = getColorOfElement(By.id("dpwa-amount"), "border-color");
-            closePopUp(By.xpath("//button[contains(text(), 'Ok')]"));
-
-            assertTrue(isPopUpClosed());
-            assertThat(popUpMessage,equalTo("Minimum top-up amount is Ft 200, maximum Ft 45000"));
-            assertThat(hexColor, equalTo(app.hexRedColor));
+    @Test(priority = 4)
+    public void testCheckSystemBehaviorWithEmptyFields() throws InterruptedException {
+        if (isElementPresent(By.id("dpwa-amount"))) {
+            assertFalse(isButtonEnabled(By.cssSelector("button[data-dpwa-action='dpwa-topup']")));
 
         } else {
 
-        navigateToTelenorAndLogin2(app.telenorRegistrationPhone, smsCode);
-        type(By.id("dpwa-amount"), "-0.00");
-        String popUpMessage = getTextFromPopUp();
-        String hexColor = getColorOfElement(By.id("dpwa-amount"), "border-color");
-        closePopUp(By.xpath("//button[contains(text(), 'Ok')]"));
-
-        assertTrue(isPopUpClosed());
-        assertThat(popUpMessage,equalTo("Minimum top-up amount is Ft 200, maximum Ft 45000"));
-        assertThat(hexColor, equalTo(app.hexRedColor));
+            navigateToTelenorAndLogin2(app.telenorRegistrationPhone, smsCode);
+            assertFalse(isButtonEnabled(By.cssSelector("button[data-dpwa-action='dpwa-topup']")));
         }
     }
 }
