2212052e09f4f3b2e68f1a169ad3bc573c3b506a
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-04-06 19:20:11 +0300

    add LinkNewPaymentBandWithSomeoneElseDataPaymentBandTest

---
+++ src/test/java/uiTests/telenor/manageSecurity/LinkNewPaymentBandWithSomeoneElseDataPaymentBandTest.java
@@ -0,0 +1,43 @@
+package uiTests.telenor.manageSecurity;
+
+import base.UITestBase;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class LinkNewPaymentBandWithSomeoneElseDataPaymentBandTest extends UITestBase {
+    String smsCode = "111111"; //app.generateRandomNumber(6);
+
+    @Test
+    public void testLinkNewPaymentBandWithSomeoneElseDataPaymentBand() throws InterruptedException {
+        navigateToTelenorAndLogin2(app.telenorRegistrationPhone2, smsCode);
+        gotoManageSecurityPage();
+        if (isElementPresent(By.cssSelector("a[href='/en/security/block']"))) {
+            blockPaymentBandTelenor("QA");
+            click(By.cssSelector("a[href='/en/security/relink']"));
+            waitForSeveralItems(new String[]{"To link your payment band please enter your 9-digits payment band token", "Cancel", "Link new payment band", "Card number"});
+            type(By.id("token"), "513855360");
+            click(By.cssSelector("button[data-dpwa-action='band-relink']"));
+//            type(By.id("secAnswer"), "QA");
+//            pressConfirm(By.cssSelector("button[data-dpwa-action='sa-send']"));
+            String popUpMessage = getTextFromPopUp();
+
+            assertThat(popUpMessage, equalTo("Sorry, something went wrong - please try again"));
+
+        } else {
+
+            click(By.cssSelector("a[href='/en/security/relink']"));
+            waitForSeveralItems(new String[]{"To link your payment band please enter your 9-digits payment band token", "Cancel", "Link new payment band", "Card number"});
+            type(By.id("token"), "513855360");
+            click(By.cssSelector("button[data-dpwa-action='band-relink']"));
+            type(By.id("secAnswer"), "QA");
+            pressConfirm(By.cssSelector("button[data-dpwa-action='sa-send']"));
+            String popUpMessage = getTextFromPopUp();
+
+
+            assertThat(popUpMessage, equalTo("Sorry, something went wrong - please try again"));
+        }
+    }
+}
