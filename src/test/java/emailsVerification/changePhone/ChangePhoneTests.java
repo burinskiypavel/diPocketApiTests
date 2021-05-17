fcdffe07b6be8277e843595fffc75d1ead283d70
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-05-17 18:14:24 +0300

    refactor

--- src/test/java/emailsVerification/changePhone/ChangePhoneTests.java
+++ src/test/java/emailsVerification/changePhone/ChangePhoneTests.java
@@ -13,7 +13,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ChangePhoneDipocketTests extends TestBase {
+public class ChangePhoneTests extends TestBase {
     String site = "DiPocket";
     String firstName = "Salat";
     String emailFrom = "customer.service@dipocket.org";
@@ -22,26 +22,26 @@ public class ChangePhoneDipocketTests extends TestBase {
     String SITE_REG = "DiPocket®";
     String newPhone = "12345678";
 
-    public String body(int landId){
+    public String body(int landId, final String site){
         return "{\n" +
                 "\"id\": 30457,\n" +
-                "\"clientFirstName\": \""+firstName+"\",\n" +
+                "\"clientFirstName\": \"" + firstName + "\",\n" +
                 "\"clientLastName\": \"Test\",\n" +
                 "\"countryId\": 826,\n" +
-                "\"langId\": "+landId+",\n" +
+                "\"langId\": " + landId + ",\n" +
                 "\"mainPhone\": \"380661470959\",\n" +
-                "\"email\": \""+testEmail+"\",\n" +
+                "\"email\": \"" + testEmail + "\",\n" +
                 "\"currencyId\": 978,\n" +
-                "\"site\": \"DIPOCKET\",\n" +
-                "\"siteEnum\": \"DIPOCKET\",\n" +
-                "\"programNickName\": \"DIPOCKET\"\n" +
+                "\"site\": \"" + site + "\",\n" +
+                "\"siteEnum\": \"" + site + "\",\n" +
+                "\"programNickName\": \"" + site + "\"\n" +
                 "}";
     }
 
-    public void postSendChangePhoneMailRealFlow(int landId) {
+    public void postSendChangePhoneMailRealFlow(int landId, String site) {
         given()
                 .header("Content-Type", "application/json")
-                .body(body(landId))
+                .body(body(landId, site))
                 .when()
                 .post( app.dipocket3_intranet+"/EmailService/sendChangePhoneMailRealFlow?newPhoneNumber="+newPhone+"")
                 .then().log().all()
@@ -50,7 +50,7 @@ public class ChangePhoneDipocketTests extends TestBase {
 
     @Test(priority = 1)
     public void testChangePhoneDipocketEN() throws InterruptedException, MessagingException, IOException {
-        postSendChangePhoneMailRealFlow(1);
+        postSendChangePhoneMailRealFlow(1, "DIPOCKET");
 
         String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
         String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
@@ -64,7 +64,7 @@ public class ChangePhoneDipocketTests extends TestBase {
 
     @Test(priority = 2)
     public void testChangePhoneDipocketUA() throws InterruptedException, MessagingException, IOException {
-        postSendChangePhoneMailRealFlow(2);
+        postSendChangePhoneMailRealFlow(2, "DIPOCKET");
 
         String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
         String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
@@ -78,7 +78,7 @@ public class ChangePhoneDipocketTests extends TestBase {
 
     @Test(priority = 3)
     public void testChangePhoneDipocketPL() throws InterruptedException, MessagingException, IOException {
-        postSendChangePhoneMailRealFlow(3);
+        postSendChangePhoneMailRealFlow(3, "DIPOCKET");
 
         String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
         String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
@@ -92,7 +92,7 @@ public class ChangePhoneDipocketTests extends TestBase {
 
     @Test(priority = 4)
     public void testChangePhoneDipocketRU() throws InterruptedException, MessagingException, IOException {
-        postSendChangePhoneMailRealFlow(4);
+        postSendChangePhoneMailRealFlow(4, "DIPOCKET");
 
         String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
         String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
@@ -102,5 +102,33 @@ public class ChangePhoneDipocketTests extends TestBase {
         assertThat(emailSender, equalTo(emailFrom));
         assertThat(emailBody, equalTo("Здравствуйте, "+firstName+"! Номер телефона, указанный для Вашей учетной записи был изменен. Новое значение: "+newPhone+". Если Вы этого не делали, перейдите по этой ссылке , чтобы заблокировать вашу учетную запись и защитить свои средства. После этого Вы можете связаться с нами для возобновления доступа. С уважением, Служба поддержки клиентов"));
         assertThat(emailFooter, equalTo(""+SITE_REG+" Для вашего спокойствия, "+site+" UAB авторизован и контролируется Банком Литвы как эмитент электронных денег (#75) Upės str. 23, 08128 Vilnius, LT"));
+    }
+
+    @Test(priority = 5)
+    public void testChangePhoneDiscontuEN() throws InterruptedException, MessagingException, IOException {
+        postSendChangePhoneMailRealFlow(1, "DISCONTU");
+
+        String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
+        String emailBody = getEmailBodyText(emailText, 41, 380);
+        String emailFooter = getEmailFooterText(emailText, 381);
+
+        assertThat(emailSender, equalTo(emailFrom));
+        assertThat(emailBody, equalTo("Dear "+firstName+", The mobile phone number associated with your discontu account was changed to: 12345678. If you didn't change it, please click on this link , to block your account and protect your money. You can then contact us at your earliest convenience so that we can safely reactivate your account. With kind regards, Customer Service Team"));
+        assertThat(emailFooter, equalTo(""+SITE_REG+" discontu is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
+    }
+
+    @Test(priority = 6)
+    public void testChangePhoneDiscontuPL() throws InterruptedException, MessagingException, IOException {
+        postSendChangePhoneMailRealFlow(3, "DISCONTU");
+
+        String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
+        String emailBody = getEmailBodyText(emailText, 0, 336);
+        String emailFooter = getEmailFooterText(emailText, 337);
+
+        assertThat(emailSender, equalTo(emailFrom));
+        assertThat(emailBody, equalTo("Witaj, "+firstName+"! Numer telefonu powiązany z Twoim kontem discontu został zmieniony na: "+newPhone+". Jeśli nie zmieniałeś numeru telefonu, kliknij w ten link aby zablokować konto i zabezpieczyć Twoje środki. Następnie skontaktuj się z nami w wybranym momencie, aby bezpiecznie odblokować Twoje konto. Z wyrazami szacunku, Dział Obsługi Klienta"));
+        assertThat(emailFooter, equalTo(""+SITE_REG+" discontu dostarcza DiPocket UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"));
     }
 }
