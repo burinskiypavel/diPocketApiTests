be5446f88564df4bd0ad19c50a82c3cc745be179
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-05-28 14:58:52 +0300

    refactor StatementEmailTests

--- src/test/java/emailsVerification/statement/StatementEmailTests.java
+++ src/test/java/emailsVerification/statement/StatementEmailTests.java
@@ -76,17 +76,17 @@ public class StatementEmailTests extends TestBase {
         assertThat(emailFooter, equalTo(""+SITE_REG+" Для Вашого спокою, "+site+" UAB авторизований та контролюється Банком Литви, як емітент електронних грошей (#75) Upės str. 23, 08128 Vilnius, LT"));
     }
 
-    @Test(priority = 3, enabled = false)
+    @Test(priority = 3)
     public void testStatementEmailDipocketPL() throws InterruptedException, MessagingException, IOException {
         postStatementEmail(3, "DIPOCKET");
 
         String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
         String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
-        String emailBody = getEmailBodyText(emailText, 0, 336);
-        String emailFooter = getEmailFooterText(emailText, 337);
+        String emailBody = getEmailBodyText(emailText, 0, 153);
+        String emailFooter = getEmailFooterText(emailText, 154);
 
         assertThat(emailSender, equalTo(emailFrom));
-        assertThat(emailBody, equalTo("Witaj, "+firstName+"! Numer telefonu powiązany z Twoim kontem "+site+" został zmieniony na: "+newPhone+". Jeśli nie zmieniałeś numeru telefonu, kliknij w ten link aby zablokować konto i zabezpieczyć Twoje środki. Następnie skontaktuj się z nami w wybranym momencie, aby bezpiecznie odblokować Twoje konto. Z wyrazami szacunku, Dział Obsługi Klienta"));
+        assertThat(emailBody, equalTo("Witaj "+firstName+", W załączniku znajduje się zamówiony wyciąg z konta DiPocket. Dziękujemy za korzystanie z serwisu DiPocket. Z wyrazami szacunku, Dział Prawny"));
         assertThat(emailFooter, equalTo(""+SITE_REG+" "+site+" UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"));
     }
 
