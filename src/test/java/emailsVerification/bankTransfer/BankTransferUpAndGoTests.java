0f1e6e3d038dda9c89d86a40961afd948450c0e7
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-05-28 16:56:13 +0300

    refactor

--- src/test/java/emailsVerification/bankTransfer/BankTransferUpAndGoTests.java
+++ src/test/java/emailsVerification/bankTransfer/BankTransferUpAndGoTests.java
@@ -14,18 +14,22 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BankTransferUpAndGoTests extends TestBase {
+    String site = "up and go";
     String firstName = "Salat";
-    String email = "wsparcie@upcard.pl";
+    String emailFrom = "wsparcie@upcard.pl";
+    String testEmail = "testdipocket@gmail.com";
+    String pass = "password1<";
+    String SITE_REG = "DiPocket®";
 
     public String body(int landId, String firstName){
         return "{\n" +
                 "\"id\": 31019,\n" +
                 "\"clientFirstName\": \""+firstName+"\",\n" +
-                "\"clientLastName\": \"Sergeyt\",\n" +
+                "\"clientLastName\": \"Test\",\n" +
                 "\"countryId\": 826,\n" +
                 "\"langId\": "+landId+",\n" +
                 "\"mainPhone\": \"380661470959\",\n" +
-                "\"email\": \"testdipocket@gmail.com\",\n" +
+                "\"email\": \""+testEmail+"\",\n" +
                 "\"currencyId\": 978,\n" +
                 "\"site\": \"UPANDGO\",\n" +
                 "\"siteEnum\": \"UPANDGO\",\n" +
@@ -47,55 +51,55 @@ public class BankTransferUpAndGoTests extends TestBase {
     public void testBankTransferUpAndGoEN() throws InterruptedException, MessagingException, IOException {
         postSendBankTransferEmail(1);
 
-        String emailSender =  EmailIMAPHelper3.getEmailSender("testdipocket@gmail.com", "password1<");
-        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", "testdipocket@gmail.com", "password1<");
+        String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
         String emailBody = getEmailBodyText(emailText, 39, 159);
         String emailFooter = getEmailFooterText(emailText, 160);
 
-        assertThat(emailSender, equalTo(email));
+        assertThat(emailSender, equalTo(emailFrom));
         assertThat(emailBody, equalTo("Dear "+firstName+", As requested, please find attached your bank transfer confirmation. With kind regards, Customer Service Team"));
-        assertThat(emailFooter, equalTo("DiPocket® up and go is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
+        assertThat(emailFooter, equalTo(""+SITE_REG+" "+site+" is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
     }
 
     @Test(priority = 2)
     public void testBankTransferUpAndGoUA() throws InterruptedException, MessagingException, IOException {
         postSendBankTransferEmail(2);
 
-        String emailSender =  EmailIMAPHelper3.getEmailSender("testdipocket@gmail.com", "password1<");
-        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", "testdipocket@gmail.com", "password1<");
+        String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
         String emailBody = getEmailBodyText(emailText, 29, 155);
         String emailFooter = getEmailFooterText(emailText, 156);
 
-        assertThat(emailSender, equalTo(email));
+        assertThat(emailSender, equalTo(emailFrom));
         assertThat(emailBody, equalTo("Вітаємо, "+firstName+"! В додатку знаходиться замовлене Вами підтвердження банківського переказу. З повагою, Відділ підтримки клієнтів"));
-        assertThat(emailFooter, equalTo("DiPocket® Для Вашого спокою, up and go працює при підтримці DiPocket UAB, що авторизований та контролюється Банком Литви, як емітент електронних грошей (#75) Upės str. 23, 08128 Vilnius, LT"));
+        assertThat(emailFooter, equalTo(""+SITE_REG+" Для Вашого спокою, "+site+" працює при підтримці DiPocket UAB, що авторизований та контролюється Банком Литви, як емітент електронних грошей (#75) Upės str. 23, 08128 Vilnius, LT"));
     }
 
     @Test(priority = 3)
     public void testBankTransferUpAndGoPL() throws InterruptedException, MessagingException, IOException {
         postSendBankTransferEmail(3);
 
-        String emailSender =  EmailIMAPHelper3.getEmailSender("testdipocket@gmail.com", "password1<");
-        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", "testdipocket@gmail.com", "password1<");
-        String emailBody = getEmailBodyText(emailText, 13, 126);
+        String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
+        String emailBody = getEmailBodyText(emailText, 0, 126);
         String emailFooter = getEmailFooterText(emailText, 127);
 
-        assertThat(emailSender, equalTo(email));
-        assertThat(emailBody, equalTo("W załączniku znajduje się zamówione potwierdzenie przelewu bankowego. Z wyrazami szacunku, Zespół Obsługi Klienta"));
-        assertThat(emailFooter, equalTo("DiPocket® up and go dostarcza DiPocket UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"));
+        assertThat(emailSender, equalTo(emailFrom));
+        assertThat(emailBody, equalTo("Witaj "+firstName+", W załączniku znajduje się zamówione potwierdzenie przelewu bankowego. Z wyrazami szacunku, Zespół Obsługi Klienta"));
+        assertThat(emailFooter, equalTo(""+SITE_REG+" "+site+" dostarcza DiPocket UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"));
     }
 
     @Test(priority = 4)
     public void testBankTransferUpAndGoRU() throws InterruptedException, MessagingException, IOException {
         postSendBankTransferEmail(4);
 
-        String emailSender =  EmailIMAPHelper3.getEmailSender("testdipocket@gmail.com", "password1<");
-        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", "testdipocket@gmail.com", "password1<");
+        String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
         String emailBody = getEmailBodyText(emailText, 29, 170);
         String emailFooter = getEmailFooterText(emailText, 171);
 
-        assertThat(emailSender, equalTo(email));
+        assertThat(emailSender, equalTo(emailFrom));
         assertThat(emailBody, equalTo("Здравствуйте, "+firstName+"! В приложении находится подтверждение банковского перевода, которое Вы заказывали. С уважением, Служба поддержки клиентов"));
-        assertThat(emailFooter, equalTo("DiPocket® Для вашего спокойствия, up and go осуществляет деятельность при поддержке DiPocket UAB, который авторизован и контролируется Банком Литвы как эмитент электронных денег (#75) Upės str. 23, 08128 Vilnius, LT"));
+        assertThat(emailFooter, equalTo(""+SITE_REG+" Для вашего спокойствия, "+site+" осуществляет деятельность при поддержке DiPocket UAB, который авторизован и контролируется Банком Литвы как эмитент электронных денег (#75) Upės str. 23, 08128 Vilnius, LT"));
     }
 }
