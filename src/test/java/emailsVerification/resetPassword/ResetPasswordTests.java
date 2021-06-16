95cda0cafa81a9ac36246f3c93af308f14a0db15
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-06-16 15:10:31 +0300

    refactor ResetPasswordTests

--- src/test/java/emailsVerification/resetPassword/ResetPasswordTests.java
+++ src/test/java/emailsVerification/resetPassword/ResetPasswordTests.java
@@ -85,17 +85,18 @@ public class ResetPasswordTests extends TestBase {
         assertThat(emailFooter, equalTo(""+SITE_REG+" discontu is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
     }
 
-    @Test(priority = 4, enabled = false)
-    public void testResetPasswordDiscontuPL() throws InterruptedException, MessagingException, IOException {
+    @Test(priority = 4)
+    public void testResetPasswordDiscontuPL() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
+        app.getDbHelper().updateClientLanguageFromDB(testEmail, "3", "DISCONTU");
         postSendResetPasswordEmail("DISCONTU");
 
         String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
         String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
-        String emailBody = getEmailBodyText(emailText, 0, 146);
-        String emailFooter = getEmailFooterText(emailText, 147);
+        String emailBody = getEmailBodyText(emailText, 0, 156);
+        String emailFooter = getEmailFooterText(emailText, 157);
 
         assertThat(emailSender, equalTo(emailFrom));
-        assertThat(emailBody, equalTo("Witaj "+firstName+", W załączniku znajdują się zamówione dokumenty prawne. Dziękujemy za korzystanie z serwisu discontu. Z wyrazami szacunku, Dział Prawny"));
+        assertThat(emailBody, equalTo("Otrzymaliśmy żądanie zmiany hasła do Twojego konta. Kliknij ten link , aby potwierdzić żądanie i ukończyć zmianę. Z wyrazami szacunku, Dział Obsługi Klienta"));
         assertThat(emailFooter, equalTo(""+SITE_REG+" discontu dostarcza DiPocket UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"));
     }
 
