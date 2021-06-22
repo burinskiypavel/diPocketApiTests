1237b5a00576d8415e8af97627ef9cb947958bd3
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-06-22 14:29:49 +0300

    refactor StatementEmailTests

--- src/test/java/emailsVerification/statement/StatementEmailTests.java
+++ src/test/java/emailsVerification/statement/StatementEmailTests.java
@@ -15,22 +15,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class StatementEmailTests extends TestBase {
     String site = "DiPocket";
-    String firstName = "Pavel";
     String emailFrom = "statements@dipocket.org";
-    String testEmail = "testdipocket@gmail.com";
-    String pass = "password1<";
-    String SITE_REG = "DiPocket®";
 
     public String body(int landId, String site, int id){
         return "{\n" +
                 "\"id\": " + id + ",\n" +
-                "\"clientFirstName\": \"" + firstName + "\",\n" +
-                "\"clientLastName\": \"Burinsky\",\n" +
-                "\"countryId\": 826,\n" +
+                "\"clientFirstName\": \"" + app.emailsVerificationsFirstName + "\",\n" +
+                "\"clientLastName\": \""+app.emailsVerificationsLastName+"\",\n" +
+                "\"countryId\": "+app.emailsVerificationsCountryId+",\n" +
                 "\"langId\": " + landId + ",\n" +
-                "\"mainPhone\": \"380685448615\",\n" +
-                "\"email\": \"" + testEmail + "\",\n" +
-                "\"currencyId\": 978,\n" +
+                "\"mainPhone\": \""+app.emailsVerificationsPhoneNumber+"\",\n" +
+                "\"email\": \"" + app.emailsVerificationsEmail + "\",\n" +
+                "\"currencyId\": "+app.emailsVerificationsCurrencyId+",\n" +
                 "\"site\": \"" + site + "\",\n" +
                 "\"siteEnum\": \"" + site + "\",\n" +
                 "\"programNickName\": \"" + site + "\"\n" +
@@ -51,97 +47,97 @@ public class StatementEmailTests extends TestBase {
     public void testStatementEmailDipocketEN() throws InterruptedException, MessagingException, IOException {
         postStatementEmail(1, "DIPOCKET", 32761);
 
-        String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
-        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
+        String emailSender =  EmailIMAPHelper3.getEmailSender(app.emailsVerificationsEmail, app.emailsVerificationsPass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", app.emailsVerificationsEmail, app.emailsVerificationsPass);
         String emailBody = getEmailBodyText(emailText, 28, 170);
         String emailFooter = getEmailFooterText(emailText, 171);
 
         assertThat(emailSender, equalTo(emailFrom));
-        assertThat(emailBody, equalTo("Dear "+firstName+", As requested, please find attached your "+site+" account statement(s). Thank you for using "+site+". With kind regards, Legal Team"));
-        assertThat(emailFooter, equalTo(""+SITE_REG+" "+site+" UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
+        assertThat(emailBody, equalTo("Dear "+app.emailsVerificationsFirstName+", As requested, please find attached your "+site+" account statement(s). Thank you for using "+site+". With kind regards, Legal Team"));
+        assertThat(emailFooter, equalTo(""+app.SITE_REG+" "+site+" UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
     }
 
     @Test(priority = 2)
     public void testStatementEmailDipocketUA() throws InterruptedException, MessagingException, IOException {
         postStatementEmail(2, "DIPOCKET", 32761);
 
-        String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
-        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
+        String emailSender =  EmailIMAPHelper3.getEmailSender(app.emailsVerificationsEmail, app.emailsVerificationsPass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", app.emailsVerificationsEmail, app.emailsVerificationsPass);
         String emailBody = getEmailBodyText(emailText, 28, 191);
         String emailFooter = getEmailFooterText(emailText, 192);
 
         assertThat(emailSender, equalTo(emailFrom));
-        assertThat(emailBody, equalTo("Вітаємо, "+firstName+"! В додатку знаходиться замовлена Вами банківська виписка по рахунку "+site+". Дякуємо за користування додатком "+site+". З повагою, Юридичний відділ"));
-        assertThat(emailFooter, equalTo(""+SITE_REG+" Для Вашого спокою, "+site+" UAB авторизований та контролюється Банком Литви, як емітент електронних грошей (#75) Upės str. 23, 08128 Vilnius, LT"));
+        assertThat(emailBody, equalTo("Вітаємо, "+app.emailsVerificationsFirstName+"! В додатку знаходиться замовлена Вами банківська виписка по рахунку "+site+". Дякуємо за користування додатком "+site+". З повагою, Юридичний відділ"));
+        assertThat(emailFooter, equalTo(""+app.SITE_REG+" Для Вашого спокою, "+site+" UAB авторизований та контролюється Банком Литви, як емітент електронних грошей (#75) Upės str. 23, 08128 Vilnius, LT"));
     }
 
     @Test(priority = 3)
     public void testStatementEmailDipocketPL() throws InterruptedException, MessagingException, IOException {
         postStatementEmail(3, "DIPOCKET", 32761);
 
-        String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
-        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
+        String emailSender =  EmailIMAPHelper3.getEmailSender(app.emailsVerificationsEmail, app.emailsVerificationsPass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", app.emailsVerificationsEmail, app.emailsVerificationsPass);
         String emailBody = getEmailBodyText(emailText, 0, 153);
         String emailFooter = getEmailFooterText(emailText, 154);
 
         assertThat(emailSender, equalTo(emailFrom));
-        assertThat(emailBody, equalTo("Witaj "+firstName+", W załączniku znajduje się zamówiony wyciąg z konta DiPocket. Dziękujemy za korzystanie z serwisu DiPocket. Z wyrazami szacunku, Dział Prawny"));
-        assertThat(emailFooter, equalTo(""+SITE_REG+" "+site+" UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"));
+        assertThat(emailBody, equalTo("Witaj "+app.emailsVerificationsFirstName+", W załączniku znajduje się zamówiony wyciąg z konta DiPocket. Dziękujemy za korzystanie z serwisu DiPocket. Z wyrazami szacunku, Dział Prawny"));
+        assertThat(emailFooter, equalTo(""+app.SITE_REG+" "+site+" UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"));
     }
 
     @Test(priority = 4)
     public void testStatementEmailDipocketRU() throws InterruptedException, MessagingException, IOException {
         postStatementEmail(4, "DIPOCKET", 32761);
 
-        String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
-        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
+        String emailSender =  EmailIMAPHelper3.getEmailSender(app.emailsVerificationsEmail, app.emailsVerificationsPass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", app.emailsVerificationsEmail, app.emailsVerificationsPass);
         String emailBody = getEmailBodyText(emailText, 0, 146);
         String emailFooter = getEmailFooterText(emailText, 147);
 
         assertThat(emailSender, equalTo(emailFrom));
-        assertThat(emailBody, equalTo("Здравствуйте, "+firstName+"! К письму прикреплена выписка по счету, которую Вы заказывали. Спасибо за пользование "+site+". С уважением, Юридический отдел"));
-        assertThat(emailFooter, equalTo(""+SITE_REG+" Для вашего спокойствия, "+site+" UAB авторизован и контролируется Банком Литвы как эмитент электронных денег (#75) Upės str. 23, 08128 Vilnius, LT"));
+        assertThat(emailBody, equalTo("Здравствуйте, "+app.emailsVerificationsFirstName+"! К письму прикреплена выписка по счету, которую Вы заказывали. Спасибо за пользование "+site+". С уважением, Юридический отдел"));
+        assertThat(emailFooter, equalTo(""+app.SITE_REG+" Для вашего спокойствия, "+site+" UAB авторизован и контролируется Банком Литвы как эмитент электронных денег (#75) Upės str. 23, 08128 Vilnius, LT"));
     }
 
     @Test(priority = 5)
     public void testStatementEmailDiscontuEN() throws InterruptedException, MessagingException, IOException {
         postStatementEmail(1, "DISCONTU", 32717);
 
-        String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
-        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
+        String emailSender =  EmailIMAPHelper3.getEmailSender(app.emailsVerificationsEmail, app.emailsVerificationsPass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", app.emailsVerificationsEmail, app.emailsVerificationsPass);
         String emailBody = getEmailBodyText(emailText, 28, 170);
         String emailFooter = getEmailFooterText(emailText, 171);
 
         assertThat(emailSender, equalTo(emailFrom));
-        assertThat(emailBody, equalTo("Dear "+firstName+", As requested, please find attached your discontu account statement(s). Thank you for using discontu. With kind regards, Legal Team"));
-        assertThat(emailFooter, equalTo(""+SITE_REG+" discontu is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
+        assertThat(emailBody, equalTo("Dear "+app.emailsVerificationsFirstName+", As requested, please find attached your discontu account statement(s). Thank you for using discontu. With kind regards, Legal Team"));
+        assertThat(emailFooter, equalTo(""+app.SITE_REG+" discontu is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
     }
 
     @Test(priority = 6)
     public void testStatementEmailDiscontuPL() throws InterruptedException, MessagingException, IOException {
         postStatementEmail(3, "DISCONTU", 32717);
 
-        String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
-        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
+        String emailSender =  EmailIMAPHelper3.getEmailSender(app.emailsVerificationsEmail, app.emailsVerificationsPass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", app.emailsVerificationsEmail, app.emailsVerificationsPass);
         String emailBody = getEmailBodyText(emailText, 0, 153);
         String emailFooter = getEmailFooterText(emailText, 154);
 
         assertThat(emailSender, equalTo(emailFrom));
-        assertThat(emailBody, equalTo("Witaj "+firstName+", W załączniku znajduje się zamówiony wyciąg z konta discontu. Dziękujemy za korzystanie z serwisu discontu. Z wyrazami szacunku, Dział Prawny"));
-        assertThat(emailFooter, equalTo(""+SITE_REG+" discontu dostarcza DiPocket UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"));
+        assertThat(emailBody, equalTo("Witaj "+app.emailsVerificationsFirstName+", W załączniku znajduje się zamówiony wyciąg z konta discontu. Dziękujemy za korzystanie z serwisu discontu. Z wyrazami szacunku, Dział Prawny"));
+        assertThat(emailFooter, equalTo(""+app.SITE_REG+" discontu dostarcza DiPocket UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"));
     }
 
     @Test(priority = 7)
     public void testStatementEmailPlayITEN() throws InterruptedException, MessagingException, IOException {
         postStatementEmail(1, "PLAYIT", 32732);
 
-        String emailSender =  EmailIMAPHelper3.getEmailSender(testEmail, pass);
-        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", testEmail, pass);
+        String emailSender =  EmailIMAPHelper3.getEmailSender(app.emailsVerificationsEmail, app.emailsVerificationsPass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", app.emailsVerificationsEmail, app.emailsVerificationsPass);
         String emailBody = getEmailBodyText(emailText, 26, 164);
         String emailFooter = getEmailFooterText(emailText, 165);
 
         assertThat(emailSender, equalTo("PlayIT Card <statements@dipocket.org>"));
-        assertThat(emailBody, equalTo("Dear "+firstName+", As requested, please find attached your PlayIT account statement(s). Thank you for using PlayIT. With kind regards, Legal Team"));
-        assertThat(emailFooter, equalTo(""+SITE_REG+" PlayIT is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
+        assertThat(emailBody, equalTo("Dear "+app.emailsVerificationsFirstName+", As requested, please find attached your PlayIT account statement(s). Thank you for using PlayIT. With kind regards, Legal Team"));
+        assertThat(emailFooter, equalTo(""+app.SITE_REG+" PlayIT is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
     }
 }
