e4e1368dea222caeab9fe85f338b3d54269e483a
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-07-02 14:19:42 +0300

    refactor

--- src/test/java/emailsVerification/bankTransfer/BankTransferDipocketTests.java
+++ src/test/java/emailsVerification/bankTransfer/BankTransferDipocketTests.java
@@ -18,12 +18,13 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class BankTransferDipocketTests extends TestBase {
-    String site = "DiPocket";
-    String expectedEmailSender = "customer.service@dipocket.org";
+    String expectedSender = "customer.service@dipocket.org";
+    String expectedPlayITSender = "PlayIT Card <customer.service@dipocket.org>";
+    String expectedUpAndGoSender = "wsparcie@upcard.pl";
 
-    public String body(int landId){
+    public String body(int landId, String site, int id){
         return "{\n" +
-                "\"id\": 32726,\n" +
+                "\"id\": " + id + ",\n" +
                 "\"clientFirstName\": \""+app.emailsVerificationsFirstName+"\",\n" +
                 "\"clientLastName\": \""+app.emailsVerificationsLastName+"\",\n" +
                 "\"countryId\": "+app.emailsVerificationsCountryId+",\n" +
@@ -31,16 +32,16 @@ public class BankTransferDipocketTests extends TestBase {
                 "\"mainPhone\": \""+app.emailsVerificationsPhoneNumber+"\",\n" +
                 "\"email\": \""+app.emailsVerificationsEmail+"\",\n" +
                 "\"currencyId\": "+app.emailsVerificationsCurrencyId+",\n" +
-                "\"site\": \""+app.mobile_site+"\",\n" +
-                "\"siteEnum\": \""+app.mobile_site+"\",\n" +
-                "\"programNickName\": \""+app.mobile_site+"\"\n" +
+                "\"site\": \""+site+"\",\n" +
+                "\"siteEnum\": \""+site+"\",\n" +
+                "\"programNickName\": \""+site+"\"\n" +
                 "}";
     }
 
-    public void postSendBankTransferEmail(int langId) {
+    public void postSendBankTransferEmail(int langId, String site, int id) {
         given()
-                .header("Content-Type", "application/json")
-                .body(body(langId))
+                .contentType("application/json")
+                .body(body(langId,site, id))
                 .when()
                 .post( app.dipocket3_intranet+"/EmailService/sendBankTransferEmail")
                 .then().log().all()
@@ -50,16 +51,26 @@ public class BankTransferDipocketTests extends TestBase {
     @DataProvider
     public Iterator<Object[]> BankTransfer(){
         List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[] {"EN", 1, app.emailsVerificationsEmail, app.emailsVerificationsPass, 38, 158, 159, "Dear "+app.emailsVerificationsFirstName+", As requested, please find attached your bank transfer confirmation. With kind regards, Customer Service Team", ""+app.SITE_REG+" "+site+" UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"});
-        list.add(new Object[] {"UA", 2, app.emailsVerificationsEmail, app.emailsVerificationsPass, 28, 154, 155, "Вітаємо, "+app.emailsVerificationsFirstName+"! В додатку знаходиться замовлене Вами підтвердження банківського переказу. З повагою, Відділ підтримки клієнтів", ""+app.SITE_REG+" Для Вашого спокою, "+site+" UAB авторизований та контролюється Банком Литви, як емітент електронних грошей (#75) Upės str. 23, 08128 Vilnius, LT"});
-        list.add(new Object[] {"PL", 3, app.emailsVerificationsEmail,  app.emailsVerificationsPass, 0, 126, 127, "Witaj "+app.emailsVerificationsFirstName+", W załączniku znajduje się zamówione potwierdzenie przelewu bankowego. Z wyrazami szacunku, Zespół Obsługi Klienta", ""+app.SITE_REG+" "+site+" UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"});
-        list.add(new Object[] {"PL", 4, app.emailsVerificationsEmail, app.emailsVerificationsPass, 28, 169, 170, "Здравствуйте, "+app.emailsVerificationsFirstName+"! В приложении находится подтверждение банковского перевода, которое Вы заказывали. С уважением, Служба поддержки клиентов", ""+app.SITE_REG+" Для вашего спокойствия, "+site+" UAB авторизован и контролируется Банком Литвы как эмитент электронных денег (#75) Upės str. 23, 08128 Vilnius, LT"});
+        list.add(new Object[] {app.mobile_site, "EN", 1, 32726, app.emailsVerificationsEmail, app.emailsVerificationsPass, 38, 158, 159, expectedSender, "Dear "+app.emailsVerificationsFirstName+", As requested, please find attached your bank transfer confirmation. With kind regards, Customer Service Team", ""+app.SITE_REG+" "+app.site+" UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"});
+        list.add(new Object[] {app.mobile_site, "UA", 2, 32726, app.emailsVerificationsEmail, app.emailsVerificationsPass, 28, 154, 155, expectedSender, "Вітаємо, "+app.emailsVerificationsFirstName+"! В додатку знаходиться замовлене Вами підтвердження банківського переказу. З повагою, Відділ підтримки клієнтів", ""+app.SITE_REG+" Для Вашого спокою, "+app.site+" UAB авторизований та контролюється Банком Литви, як емітент електронних грошей (#75) Upės str. 23, 08128 Vilnius, LT"});
+        list.add(new Object[] {app.mobile_site, "PL", 3, 32726, app.emailsVerificationsEmail,  app.emailsVerificationsPass, 0, 126, 127, expectedSender, "Witaj "+app.emailsVerificationsFirstName+", W załączniku znajduje się zamówione potwierdzenie przelewu bankowego. Z wyrazami szacunku, Zespół Obsługi Klienta", ""+app.SITE_REG+" "+app.site+" UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"});
+        list.add(new Object[] {app.mobile_site, "PL", 4, 32726, app.emailsVerificationsEmail, app.emailsVerificationsPass, 28, 169, 170, expectedSender, "Здравствуйте, "+app.emailsVerificationsFirstName+"! В приложении находится подтверждение банковского перевода, которое Вы заказывали. С уважением, Служба поддержки клиентов", ""+app.SITE_REG+" Для вашего спокойствия, "+app.site+" UAB авторизован и контролируется Банком Литвы как эмитент электронных денег (#75) Upės str. 23, 08128 Vilnius, LT"});
+        list.add(new Object[] {app.mobile_site_discontu, "EN", 1, 32717, app.emailsVerificationsEmail, app.emailsVerificationsPass, 38, 158, 159, expectedSender, "Dear "+app.emailsVerificationsFirstName+", As requested, please find attached your bank transfer confirmation. With kind regards, Customer Service Team", ""+app.SITE_REG+" "+app.site_discontu+" is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"});
+        list.add(new Object[] {app.mobile_site_discontu, "PL", 2, 32717, app.emailsVerificationsEmail, app.emailsVerificationsPass, 0, 126, 127, expectedSender, "Witaj "+app.emailsVerificationsFirstName+", W załączniku znajduje się zamówione potwierdzenie przelewu bankowego. Z wyrazami szacunku, Zespół Obsługi Klienta", ""+app.SITE_REG+" "+app.site_discontu+" dostarcza DiPocket UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"});
+        list.add(new Object[] {app.mobile_site_playIt, "EN", 1, 32732, app.emailsVerificationsEmail, app.emailsVerificationsPass, 36, 156, 157, expectedPlayITSender, "Dear "+app.emailsVerificationsFirstName+", As requested, please find attached your bank transfer confirmation. With kind regards, Customer Service Team", ""+app.SITE_REG+" "+app.site_PlayIT+" is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"});
+        //list.add(new Object[] {app.mobile_site_playIt, "HU", 5, 32732, app.emailsVerificationsEmail, app.emailsVerificationsPass, 36, 156, 157, expectedPlayITSender, "Dear "+app.emailsVerificationsFirstName+", As requested, please find attached your bank transfer confirmation. With kind regards, Customer Service Team", ""+app.SITE_REG+" "+app.site_PlayIT+" is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"});//bug incorrect HU footer and body
+        list.add(new Object[] {app.mobile_site_snowAttack, "EN", 1, 32855, app.emailsVerificationsEmail, app.emailsVerificationsPass, 41, 161, 162, expectedSender, "Dear "+app.emailsVerificationsFirstName+", As requested, please find attached your bank transfer confirmation. With kind regards, Customer Service Team", ""+app.SITE_REG+" "+app.site_SnowAttack+" is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"});
+        //list.add(new Object[] {app.mobile_site_snowAttack, "HU", 5, 32855, app.emailsVerificationsEmail, app.emailsVerificationsPass, 41, 169, 170, expectedSender, "Kedves "+app.emailsVerificationsFirstName+", Kívánsága szerint, csatolva találja az Ön banki átutalásának megerősítése. Üdvözlettel, az Ügyfélszolgálati csapat", ""+app.SITE_REG+" "+app.site_SnowAttack+" is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"});//bug incorrect HU footer and body
+        list.add(new Object[] {app.mobile_site_upAndGo, "EN", 1, 32727, app.emailsVerificationsEmail, app.emailsVerificationsPass, 39, 159, 160, expectedUpAndGoSender, "Dear "+app.emailsVerificationsFirstName+", As requested, please find attached your bank transfer confirmation. With kind regards, Customer Service Team", ""+app.SITE_REG+" "+app.mobile_site_upAndGo+" is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"});
+        list.add(new Object[] {app.mobile_site_upAndGo, "UA", 2, 32727, app.emailsVerificationsEmail, app.emailsVerificationsPass, 29, 155, 156, expectedUpAndGoSender, "Вітаємо, "+app.emailsVerificationsFirstName+"! В додатку знаходиться замовлене Вами підтвердження банківського переказу. З повагою, Відділ підтримки клієнтів", ""+app.SITE_REG+" Для Вашого спокою, "+app.mobile_site_upAndGo+" працює при підтримці DiPocket UAB, що авторизований та контролюється Банком Литви, як емітент електронних грошей (#75) Upės str. 23, 08128 Vilnius, LT"});
+        list.add(new Object[] {app.mobile_site_upAndGo, "PL", 3, 32727, app.emailsVerificationsEmail, app.emailsVerificationsPass, 0, 126, 127, expectedUpAndGoSender, "Witaj "+app.emailsVerificationsFirstName+", W załączniku znajduje się zamówione potwierdzenie przelewu bankowego. Z wyrazami szacunku, Zespół Obsługi Klienta", ""+app.SITE_REG+" "+app.mobile_site_upAndGo+" dostarcza DiPocket UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"});
+        list.add(new Object[] {app.mobile_site_upAndGo, "RU", 4, 32727, app.emailsVerificationsEmail, app.emailsVerificationsPass, 29, 170, 171, expectedUpAndGoSender, "Здравствуйте, "+app.emailsVerificationsFirstName+"! В приложении находится подтверждение банковского перевода, которое Вы заказывали. С уважением, Служба поддержки клиентов", ""+app.SITE_REG+" Для вашего спокойствия, "+app.mobile_site_upAndGo+" осуществляет деятельность при поддержке DiPocket UAB, который авторизован и контролируется Банком Литвы как эмитент электронных денег (#75) Upės str. 23, 08128 Vilnius, LT"});
         return list.iterator();
     }
 
     @Test(dataProvider = "BankTransfer")
-    public void testBankTransferDipocket(String lang, int langId, String email, String pass, int bodyBegin, int bodyEnd, int footerEnd, String expectedEmailBody, String expectedEmailFooter) throws InterruptedException, MessagingException, IOException {
-        postSendBankTransferEmail(langId);
+    public void testBankTransferDipocket(String site, String lang, int langId, int id, String email, String pass, int bodyBegin, int bodyEnd, int footerEnd, String expectedEmailSender, String expectedEmailBody, String expectedEmailFooter) throws InterruptedException, MessagingException, IOException {
+        postSendBankTransferEmail(langId, site, id);
 
         String emailSender =  EmailIMAPHelper3.getEmailSender(email, pass);
         String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", email, pass);
