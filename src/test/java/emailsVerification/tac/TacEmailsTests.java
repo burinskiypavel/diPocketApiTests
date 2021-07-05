aa866cef3c20a315621bff299d632981d9fa1374
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-07-05 15:40:58 +0300

    refactor TacEmailsTests

--- src/test/java/emailsVerification/tac/TacEmailsTests.java
+++ src/test/java/emailsVerification/tac/TacEmailsTests.java
@@ -47,7 +47,7 @@ public class TacEmailsTests extends TestBase {
                 .statusCode(200);
     }
 
-    @Test(priority = 1)
+    @Test
     public void testTacDipocketEN() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
         app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "1", app.mobile_site);
         postSendTacEmail(app.mobile_site);
@@ -62,7 +62,7 @@ public class TacEmailsTests extends TestBase {
         assertThat(emailFooter, equalTo(""+app.SITE_REG+" "+app.site+" UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
     }
 
-    @Test(priority = 2)
+    @Test
     public void testTacDipocketUA() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
         app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "2", app.mobile_site);
         postSendTacEmail(app.mobile_site);
@@ -77,7 +77,7 @@ public class TacEmailsTests extends TestBase {
         assertThat(emailFooter, equalTo(""+app.SITE_REG+" Для Вашого спокою, "+app.site+" UAB авторизований та контролюється Банком Литви, як емітент електронних грошей (#75) Upės str. 23, 08128 Vilnius, LT"));
     }
 
-    @Test(priority = 3)
+    @Test
     public void testTacDipocketPL() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
         app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "3", app.mobile_site);
         postSendTacEmail(app.mobile_site);
@@ -92,7 +92,7 @@ public class TacEmailsTests extends TestBase {
         assertThat(emailFooter, equalTo(""+app.SITE_REG+" "+app.site+" UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"));
     }
 
-    @Test(priority = 4)
+    @Test
     public void testTacDipocketRU() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
         app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "4", app.mobile_site);
         postSendTacEmail(app.mobile_site);
@@ -107,7 +107,7 @@ public class TacEmailsTests extends TestBase {
         assertThat(emailFooter, equalTo(""+app.SITE_REG+" Для вашего спокойствия, "+app.site+" UAB авторизован и контролируется Банком Литвы как эмитент электронных денег (#75) Upės str. 23, 08128 Vilnius, LT"));
     }
 
-    @Test(priority = 5)
+    @Test
     public void tesTacDiscontuEN() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
         app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "1", app.mobile_site_discontu);
         postSendTacEmail(app.mobile_site_discontu);
@@ -122,7 +122,7 @@ public class TacEmailsTests extends TestBase {
         assertThat(emailFooter, equalTo(""+app.SITE_REG+" "+app.site_discontu+" is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
     }
 
-    @Test(priority = 6)
+    @Test
     public void testTacDiscontuPL() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
         app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "3", app.mobile_site_discontu);
         postSendTacEmail(app.mobile_site_discontu);
@@ -137,7 +137,7 @@ public class TacEmailsTests extends TestBase {
         assertThat(emailFooter, equalTo(""+app.SITE_REG+" "+app.site_discontu+" dostarcza DiPocket UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"));
     }
 
-    @Test(priority = 7, enabled = false)
+    @Test(enabled = false)
     public void testTacPlayITEN() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
         app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "1", app.mobile_site_playIt);
         postSendTacEmail(app.mobile_site_playIt);
@@ -152,7 +152,7 @@ public class TacEmailsTests extends TestBase {
         assertThat(emailFooter, equalTo(""+app.SITE_REG+" "+app.site_PlayIT+" is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
     }
 
-    @Test(priority = 8, enabled = false)
+    @Test(enabled = false)
     public void testTacUpAndGoEN() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
         app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "1", app.mobile_site_upAndGo);
         postSendTacEmail(app.mobile_site_upAndGo);
@@ -167,7 +167,22 @@ public class TacEmailsTests extends TestBase {
         assertThat(emailFooter, equalTo(""+app.SITE_REG+" "+app.site_upAndGo+" is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
     }
 
-    @Test(priority = 9, enabled = false)
+    @Test(enabled = false)
+    public void testTacUpAndGoUA() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
+        app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "2", app.mobile_site_upAndGo);
+        postSendTacEmail(app.mobile_site_upAndGo);
+
+        String emailSender =  EmailIMAPHelper3.getEmailSender(app.emailsVerificationsEmail, app.emailsVerificationsPass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", app.emailsVerificationsEmail, app.emailsVerificationsPass);
+        String emailBody = getEmailBodyText(emailText, 29, 1576);
+        String emailFooter = getEmailFooterText(emailText, 1577);
+
+        assertThat(emailSender, equalTo(expectedUpAndGoSender));
+        assertThat(emailBody, equalTo("Вітаємо, Pavel! Будь ласка, ознайомтесь з прикріпленим файлом \"up and go_Terms_and_Conditions\". Будь ласка, зверніть увагу на частину \"up and go_at_a_Glance\", в ньому знаходяться ключові правила користування up and go, актуальні ціни та ліміти для транзакцій. Стосовно особистої інформації, просимо Вас ознайомитись з розділом під назвою \"Personal Information\" - ми будемо обробляти будь-яку інформацію, яку ви надаєте нам відповідно до цих умов. Надавши нам інформацію (включаючи будь-які конфіденційні дані), Ви погоджуєтесь на її обробку для цілей, описаних у Договорі, з урахуванням права бути проінформованим про обробку вашої інформації, для виправлення помилок, для відмови у обробці, обмеження обробки або видалення інформації, для доручення копіювати або передавати інформацію за Вашою вказівкою. Якщо Ви приймаєте \"up and go_Terms_and_Conditions\", перейдіть за цим посиланням та продовжіть реєстрацію у додатку up and go. \"up and go_Terms_and_Conditions\" лишатимуться незмінним, доки ми не повідомимо про зміни, надсилаючи нові документи. Зміни будуть вислані принаймні за два місяці, щоб дати можливість ознайомитись із ними та прийняти їх. ВАЖЛИВО: Для прискорення процесу реєстрації можна почати користуватись додатком up and go без переходу за посиланням, яке зазначене вище. Але якщо Ви не зробите цього протягом 7 днів з моменту отримання доступу в додаток, Ваш рахунок up and go буде заблоковано, до моменту переходу за посиланням, що підтвердить що Ви ознайомились із \"up and go_Terms_and_Conditions\". З повагою, Юридичний відділ"));
+        assertThat(emailFooter, equalTo(""+app.SITE_REG+" Для Вашого спокою, "+app.site_upAndGo+" працює при підтримці DiPocket UAB, що авторизований та контролюється Банком Литви, як емітент електронних грошей (#75) Upės str. 23, 08128 Vilnius, LT"));
+    }
+
+    @Test(enabled = false)
     public void testTacUpAndGoPL() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
         app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "3", app.mobile_site_upAndGo);
         postSendTacEmail(app.mobile_site_upAndGo);
@@ -180,5 +195,20 @@ public class TacEmailsTests extends TestBase {
         assertThat(emailSender, equalTo(expectedUpAndGoSender));
         assertThat(emailBody, equalTo("Witaj "+app.emailsVerificationsFirstName+", Prosimy o zapoznanie się z załączonymi dokumentami: “Warunkami ogólnymi dla klientów indywidualnych "+app.site_upAndGo+"”, \"Tabelą Limitów\" oraz \"Tabelą Prowizji i Opłat\". Prosimy o uważne przeczytanie zawartych w nich warunków oraz aktualnego cennika i limitów transakcji. W odniesieniu do Informacji osobowych, prosimy o zapoznanie się z sekcją Warunków ogólnych, zatytułowaną „Informacje osobowe” – przetwarzamy wszelkie powierzone nam dane zgodnie z zapisami tej sekcji. Przekazanie danych jest jednoczesnym wyrażeniem zgody na ich przetwarzanie, w tym na przetwarzanie danych poufnych do celów opisanych w Umowie, z zastrzeżeniem szeregu praw, o których użytkownik został poinformowany w aspektach: sposobu przetwarzania Informacji, poprawienia błędów, całkowitej lub częściowej odmowie możliwości przetwarzania, usunięcia informacji lub nakazania nam skopiowania lub transferu danych zgodnie z twoją dyspozycją. Jeżeli akceptujesz “Ogólne warunki dla klientów indywidualnych "+app.site_upAndGo+"” kliknij w ten link aby kontynuować proces rejestracji w aplikacji mobilnej "+app.site_upAndGo+". “Ogólne warunki dla klientów indywidualnych "+app.site_upAndGo+"” mogą ulec zmianie jedynie, jeśli użytkownicy zostaną powiadomieni z co najmniej dwumiesięcznym wyprzedzeniem. W przypadku niezgłoszenia braku akceptacji, uznamy że akceptujesz nowe regulacje. WAŻNE: Aby przyspieszyć rejestrację, możesz zacząć korzystać z aplikacji "+app.site_upAndGo+" bez klikania w link zamieszczony powyżej. Jednakże w przypadku, gdy nie klikniesz w link w ciągu 7 dni, Twoje konto "+app.site_upAndGo+" zostanie tymczasowo zablokowane do momentu zaakceptowania „Ogólnych warunków korzystania z aplikacji "+app.site_upAndGo+"”. Z wyrazami szacunku, Dział Prawny"));
         assertThat(emailFooter, equalTo(""+app.SITE_REG+" "+app.site_upAndGo+" dostarcza DiPocket UAB, autoryzowana Instytucja Pieniądza Elektronicznego, podlegająca nadzorowi Banku Litwy (numer licencji 75) | Licencjonowana przez Mastercard do działania na Europejskim Obszarze Gospodarczego Upės g. 23, 08128 Vilnius, LT"));
+    }
+
+    @Test(enabled = false)
+    public void testTacUpAndGoRU() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
+        app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "4", app.mobile_site_upAndGo);
+        postSendTacEmail(app.mobile_site_upAndGo);
+
+        String emailSender =  EmailIMAPHelper3.getEmailSender(app.emailsVerificationsEmail, app.emailsVerificationsPass);
+        String emailText =  EmailIMAPHelper3.getTextFromEmail("pop.gmail.com", app.emailsVerificationsEmail, app.emailsVerificationsPass);
+        String emailBody = getEmailBodyText(emailText, 29, 1551);
+        String emailFooter = getEmailFooterText(emailText, 1552);
+
+        assertThat(emailSender, equalTo(expectedUpAndGoSender));
+        assertThat(emailBody, equalTo("Здравствуйте, "+app.emailsVerificationsFirstName+"! Ознакомьтесь с прикрепленным файлом \""+app.site_upAndGo+"_Terms_and_Conditions\". Обратите внимание на документ \""+app.site_upAndGo+"_at_a_Glance\", в нём вы найдете ключевые правила пользования "+app.site_upAndGo+", актуальные цены и тарифы. Относительно личных данных, просьба обратить внимание на раздел \"Personal Information\" в \""+app.site_upAndGo+"_Terms_and_Conditions\", поскольку вся личная информация будет обрабатываться согласно этим условиям. Предоставляя нам информацию (включая конфиденциальную), вы даёте согласие на ее обработку в целях, описанных в Договоре. Вы имеете право быть в курсе использования данных, чтобы иметь возможность исправить ошибки, чтобы отказаться от обработки или остановить обработку, или удалить информацию или проинструктировать нас о копировании или передачи информации по Вашему указанию. Если Вы принимаете \"Условия пользования приложением "+app.site_upAndGo+"\",  перейдите по этой ссылке и закончите регистрацию в приложении, если еще не закончили. \""+app.site_upAndGo+"_Terms_and_Conditions\" остаются неизменными до тех пор, пока не будет уведомлено об обратном. Уведомление об изменении условий будет выслано, по меньшей мере, за два месяца, чтобы дать Вам возможность ознакомится и принять их. ВАЖНО: можно закончить регистрацию без перехода по ссылке выше. Но, если вы не сделаете этого в течение 7 дней после получения доступа к приложению, учетная запись в up and go будет заблокирована до тех пор, пока Вы не перейдете по ссылке, принимая таким образом \""+app.site_upAndGo+"_Terms_and_Conditions\". С уважением, Юридический отдел"));
+        assertThat(emailFooter, equalTo(""+app.SITE_REG+" Для вашего спокойствия, "+app.site_upAndGo+" осуществляет деятельность при поддержке DiPocket UAB, который авторизован и контролируется Банком Литвы как эмитент электронных денег (#75) Upės str. 23, 08128 Vilnius, LT"));
     }
 }
