718145b7de7c7d03ff998d5ae74919ddfaa7c571
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-07-14 20:05:10 +0300

    refactor TelenorHomePageTest

--- src/test/java/telenor/TelenorCheckBalanceTest.java
+++ src/test/java/telenor/TelenorCheckBalanceTest.java
@@ -1,29 +1,30 @@
 package telenor;
 
 import base.TestBase;
-import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class TelenorCheckBalanceTest extends TestBase {
     String token = "512047269";
 
     @Test(priority = 1)
     public void test_WebServices_v1_anonymous_bandBalance(){
-        Response res = given().log().uri().log().headers().log().body()
+        given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .header("content-type", "application/json; charset=utf-8")
                 .header("site", app.telenorSite)
                 .body("{\n" +
                         "  \"publicToken\" : \""+token+"\"\n" +
                         "}\n")
                 .when()
-                .post(app.dipocket3_intranet+"/WebServices/v1/anonymous/bandBalance");
-        res.then().log().all();
-
-        app.getResponseValidationHelper().checkStatusCodeIs200(res);
-        app.getResponseValidationHelper().checkResponseHasItemWithValue(res, "publicToken", "512047269");
-        app.getResponseValidationHelper().checkResponseHasItemWithValue(res, "ccy", "HUF");
-        app.getResponseValidationHelper().checkResponseHasItemWithValue(res, "availableBalance", 102000);
+                .post("/WebServices/v1/anonymous/bandBalance")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("publicToken", equalTo("512047269"))
+                .body("ccy", equalTo("HUF"))
+                .body("availableBalance", equalTo(102000));
     }
 }
