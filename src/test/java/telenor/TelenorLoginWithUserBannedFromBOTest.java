194d4071e421ff966a4cd04637ab3ff500ddb63e
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-07-15 12:05:43 +0300

    refactor

--- src/test/java/telenor/TelenorLoginWithUserBannedFromBOTest.java
+++ src/test/java/telenor/TelenorLoginWithUserBannedFromBOTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class TelenorLoginWithUserBannedFromBOTest extends TestBase {
     String smsCode = app.generateRandomNumber(6);
@@ -16,50 +17,47 @@ public class TelenorLoginWithUserBannedFromBOTest extends TestBase {
     @Test(priority = 1)
     public void test_TestServices_v1_telenor_sendOtpForPhone_smsCode(){
         System.out.println("smsCod:" + smsCode);
-        Response res = given().log().uri().log().headers().log().body()
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
+        given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .body("{\n" +
                         "  \"phoneNumber\" : \""+app.bannedPhone+"\"\n" +
                         "}")
                 .when()
-                .post(app.dipocket3_intranet+"/TestServices/v1/telenor/sendOtpForPhone/"+smsCode+"");
-        res.then().log().all();
-
-        assertThat(res.getStatusCode(), equalTo(200));
+                .post("/TestServices/v1/telenor/sendOtpForPhone/"+smsCode+"")
+                .then()
+                .log().all()
+                .statusCode(200);
     }
 
     @Test(priority = 2)
     public void test_WebServices_v1_auth_authenticate() {
         Response res = given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .auth().preemptive().basic(app.loginBannedPhone, smsCode)
                 .header("accept", "application/json, text/json;q=0.8, text/plain;q=0.6, */*;q=0.1")
-                .header("site", app.telenorSite)
-                .header("content-type", "application/json; charset=utf-8")
                 .when()
-                .post(app.dipocket3_intranet+"/WebServices/v1/auth/authenticate");
+                .post("/WebServices/v1/auth/authenticate");
 
         cliSessionId = res.getHeader("cliSessionId");
         System.out.println(res.getHeaders());
         System.out.println("cliSessionId " + cliSessionId);
         res.then().log().all();
 
-        Assert.assertEquals(res.getStatusCode(), 200);
+        assertEquals(res.getStatusCode(), 200);
         app.getResponseValidationHelper().checkResponseHasItemWithValue(res, "stateId", -100);
     }
 
     @Test(priority = 3)
     public void test_WebServices_v1_account_clientDiPAccounts2(){
         Response res = given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .auth().preemptive().basic(app.loginBannedPhone, smsCode)
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
                 .header("clisessionid", cliSessionId)
                 .when()
-                .get(app.dipocket3_intranet+"/WebServices/v1/account/clientDiPAccounts2");
+                .get("/WebServices/v1/account/clientDiPAccounts2");
         res.then().log().all();
 
-        Assert.assertEquals(res.getStatusCode(), 400);
+        assertEquals(res.getStatusCode(), 400);
         app.getResponseValidationHelper().checkResponseHasItemWithValue(res, "errCode", "DIP-00425");
     }
 }
