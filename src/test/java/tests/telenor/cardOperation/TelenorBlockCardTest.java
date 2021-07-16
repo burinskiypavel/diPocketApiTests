b2eaf238e823dc8a4484b487180cdddd4f3d17aa
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-07-16 12:12:01 +0300

    refactor

--- src/test/java/tests/telenor/cardOperation/TelenorBlockCardTest.java
+++ src/test/java/tests/telenor/cardOperation/TelenorBlockCardTest.java
@@ -5,44 +5,42 @@ import io.restassured.mapper.ObjectMapperType;
 import io.restassured.response.Response;
 import model.telenor.login.auth_authenticate.AuthAuthenticate;
 import model.telenor.login.clientDiPAccounts2.ClientDiPAccounts;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class TelenorBlockCardTest extends TestBase {
-    String smsCode = "111111"; //app.generateRandomNumber(6);
+    String smsCode = app.generateRandomNumber(6);
     String cliSessionId = null;
     String secretAnswer = "QA";
 
     @Test(priority = 1)
     public void test_TestServices_v1_telenor_sendOtpForPhone_smsCode(){
         System.out.println("smsCod:" + smsCode);
-        Response res = given().log().uri().log().headers().log().body()
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
+        given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .body("{\n" +
                         "  \"phoneNumber\" : \""+app.telenorLoginPhone+"\"\n" +
                         "}")
                 .when()
-                .post(app.dipocket3_intranet+"/TestServices/v1/telenor/sendOtpForPhone/"+smsCode+"");
-        res.then().log().all();
-
-        assertThat(res.getStatusCode(), equalTo(200));
+                .post("/TestServices/v1/telenor/sendOtpForPhone/"+smsCode+"")
+                .then()
+                .log().all()
+                .statusCode(200);
     }
 
     @Test(priority = 2)
     public void test_WebServices_v1_auth_authenticate() {
         Response res = given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .auth().preemptive().basic(app.loginPhone, smsCode)
                 .header("accept", "application/json, text/json;q=0.8, text/plain;q=0.6, */*;q=0.1")
-                .header("site", app.telenorSite)
-                .header("content-type", "application/json; charset=utf-8")
                 .queryParam("value", "org.springframework.security.web.header.HeaderWriterFilter$HeaderWriterResponse@7211c1e4")
                 .when()
-                .post(app.dipocket3_intranet+"/WebServices/v1/auth/authenticate");
+                .post("/WebServices/v1/auth/authenticate");
 
         cliSessionId = res.getHeader("cliSessionId");
         System.out.println(res.getHeaders());
@@ -50,113 +48,107 @@ public class TelenorBlockCardTest extends TestBase {
         res.then().log().all();
         AuthAuthenticate authAuthenticate = res.as(AuthAuthenticate.class, ObjectMapperType.GSON);
 
-        Assert.assertEquals(res.getStatusCode(), 200);
+        assertEquals(res.getStatusCode(), 200);
         app.getTelenorHelper().checkFieldsInTelenorAuthAuthenticateResponse(authAuthenticate);
     }
 
     @Test(priority = 3)
     public void test_WebServices_v1_account_clientDiPAccounts2(){
         Response res = given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .auth().preemptive().basic(app.loginPhone, smsCode)
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
                 .header("clisessionid", cliSessionId)
                 .when()
-                .get(app.dipocket3_intranet+"/WebServices/v1/account/clientDiPAccounts2");
+                .get("/WebServices/v1/account/clientDiPAccounts2");
         res.then().log().all();
         ClientDiPAccounts clientDiPAccounts = res.as(ClientDiPAccounts.class, ObjectMapperType.GSON);
 
-        Assert.assertEquals(res.getStatusCode(), 200);
+        assertEquals(res.getStatusCode(), 200);
         app.getTelenorHelper().checkAllFieldsFromTelenorClientDiPAccounts2Response(clientDiPAccounts);
     }
 
     @Test(priority = 4)
     public void test_WebServices_v1_tile_messages(){
-        Response res = given().log().uri().log().headers().log().body()
+        given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .auth().preemptive().basic(app.loginPhone, smsCode)
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
                 .header("clisessionid", cliSessionId)
                 .when()
-                .get(app.dipocket3_intranet+"/WebServices/v1/tile/messages");
-        res.then().log().all();
-
-        Assert.assertEquals(res.getStatusCode(), 200);
+                .get("/WebServices/v1/tile/messages")
+                .then()
+                .log().all()
+                .statusCode(200);
     }
 
     @Test(priority = 5)
     public void test_WebServices_v1_account_tokenByCardId(){
         Response res = given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .auth().preemptive().basic(app.loginPhone, smsCode)
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
                 .header("clisessionid", cliSessionId)
                 .body("{\n" +
                         "  \"value\" : \"14932\"\n" +
                         "}")
                 .when()
-                .post(app.dipocket3_intranet+"/WebServices/v1/account/tokenByCardId");
+                .post("/WebServices/v1/account/tokenByCardId");
         res.then().log().all();
 
         assertThat(res.getStatusCode(), equalTo(200));
-        assertThat(res.getBody().jsonPath().get("value"),equalTo("512047269"));
+        assertThat(res.path("value"),equalTo("512047269"));
     }
 
     @Test(priority = 6)
     public void test_WebServices_v1_account_plasticCardStatus(){
         Response res = given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .auth().preemptive().basic(app.loginPhone, smsCode)
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
                 .header("clisessionid", cliSessionId)
                 .body("{\n" +
                         "  \"accountId\" : 9434\n" +
                         "}\n")
                 .when()
-                .post(app.dipocket3_intranet+"/WebServices/v1/account/plasticCardStatus");
+                .post("/WebServices/v1/account/plasticCardStatus");
         res.then().log().all();
 
         assertThat(res.getStatusCode(), equalTo(200));
-        app.getResponseValidationHelper().checkResponseHasItemWithValue(res, "status", "ACTIVE");
-        app.getResponseValidationHelper().checkResponseHasItemWithValue(res, "canActivate", false);
-        app.getResponseValidationHelper().checkResponseHasItemWithValue(res, "noName", true);
-        app.getResponseValidationHelper().checkResponseHasItemWithValue(res, "pinIsSet", true);
+        assertThat(res.path("status"),equalTo("ACTIVE"));
+        assertThat(res.path("canActivate"),equalTo(false));
+        assertThat(res.path("noName"),equalTo(true));
+        assertThat(res.path("pinIsSet"),equalTo(true));
     }
 
     @Test(priority = 7)
     public void test_WebServices_v1_clientProfile_checkSecAnswAttempts(){
         Response res = given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .auth().preemptive().basic(app.loginPhone, smsCode)
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
                 .header("clisessionid", cliSessionId)
                 .body("{\n" +
                         "  \"secAnswer\" : \""+secretAnswer+"\"\n" +
                         "}")
                 .when()
-                .post(app.dipocket3_intranet+"/WebServices/v1/clientProfile/checkSecAnswAttempts");
+                .post("/WebServices/v1/clientProfile/checkSecAnswAttempts");
         res.then().log().all();
 
         assertThat(res.getStatusCode(), equalTo(200));
-        assertThat(res.getBody().jsonPath().get("attemptsCount"),equalTo(0));
-        assertThat(res.getBody().jsonPath().get("attemptsLeft"),equalTo(3));
+        assertThat(res.path("attemptsCount"),equalTo(0));
+        assertThat(res.path("attemptsLeft"),equalTo(3));
     }
 
     @Test(priority = 8)
     public void test_WebServices_v1_account_blockCard(){
-        Response res = given().log().uri().log().headers().log().body()
+        given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .auth().preemptive().basic(app.loginPhone, smsCode)
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
                 .header("clisessionid", cliSessionId)
                 .header("secretanswer", secretAnswer)
                 .body("{\n" +
                         "  \"accountId\" : 9434\n" +
                         "}\n")
                 .when()
-                .post(app.dipocket3_intranet+"/WebServices/v1/account/blockCard");
-        res.then().log().all();
-
-        assertThat(res.getStatusCode(), equalTo(200));
+                .post("/WebServices/v1/account/blockCard")
+                .then()
+                .log().all()
+                .statusCode(200);
     }
 }
