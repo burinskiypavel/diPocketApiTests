9fd9720f440cca93354c9115e11b8452849a275b
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-07-19 09:09:48 +0300

    refactor

--- src/test/java/tests/telenor/manageSecurity/TelenorWrongSecretAnswerTest.java
+++ src/test/java/tests/telenor/manageSecurity/TelenorWrongSecretAnswerTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.testng.Assert.assertEquals;
 
 public class TelenorWrongSecretAnswerTest extends TestBase {
     String smsCode = "111111"; // app.generateRandomNumber(6);
@@ -16,33 +17,32 @@ public class TelenorWrongSecretAnswerTest extends TestBase {
     public void test_TestServices_v1_telenor_sendOtpForPhone_smsCode(){
         System.out.println("smsCod:" + smsCode);
         given().log().uri().log().headers().log().body()
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
+                .spec(app.requestSpecTelenor)
                 .body("{\n" +
                         "  \"phoneNumber\" : \""+app.telenorRegistrationPhone+"\"\n" +
                         "}")
                 .when()
-                .post(app.dipocket3_intranet+"/TestServices/v1/telenor/sendOtpForPhone/"+smsCode+"")
-                .then().log().all()
-                .assertThat().statusCode(200);
+                .post("/TestServices/v1/telenor/sendOtpForPhone/"+smsCode+"")
+                .then()
+                .log().all()
+                .statusCode(200);
     }
 
     @Test(priority = 2)
     public void test_WebServices_v1_auth_authenticate() {
         Response res = given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .auth().preemptive().basic(app.fullRegistrationTelenorLoginPhone, smsCode)
                 .header("accept", "application/json, text/json;q=0.8, text/plain;q=0.6, */*;q=0.1")
-                .header("site", app.telenorSite)
-                .header("content-type", "application/json; charset=utf-8")
                 .when()
-                .post(app.dipocket3_intranet+"/WebServices/v1/auth/authenticate");
+                .post("/WebServices/v1/auth/authenticate");
 
         cliSessionId = res.getHeader("cliSessionId");
         System.out.println(res.getHeaders());
         System.out.println("cliSessionId " + cliSessionId);
         res.then().log().all();
 
-        Assert.assertEquals(res.getStatusCode(), 200);
+        assertEquals(res.getStatusCode(), 200);
         res.then().assertThat().body("clientFirstName", equalTo("Pavel"));
         res.then().assertThat().body("clientLastName", equalTo("TestQA"));
         res.then().assertThat().body("mainPhone", equalTo("380980316499"));
@@ -51,35 +51,35 @@ public class TelenorWrongSecretAnswerTest extends TestBase {
     @Test(priority = 3)
     public void test_WebServices_v1_clientProfile_checkSecAnswAttempts(){
         given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .auth().preemptive().basic(app.fullRegistrationTelenorLoginPhone, smsCode)
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
                 .header("clisessionid", cliSessionId)
                 .body("{\n" +
                         "  \"secAnswer\" : \"QA\"\n" +
                         "}")
                 .when()
-                .post(app.dipocket3_intranet+"/WebServices/v1/clientProfile/checkSecAnswAttempts")
-                .then().log().all()
-                .assertThat().statusCode(200)
-                .assertThat().body("attemptsCount", equalTo(0))
-                .assertThat().body("attemptsLeft", equalTo(3));
+                .post("/WebServices/v1/clientProfile/checkSecAnswAttempts")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("attemptsCount", equalTo(0))
+                .body("attemptsLeft", equalTo(3));
     }
 
     @Test(priority = 4)
     public void test_WebServices_v1_auth_unblockClient(){
         given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
                 .auth().preemptive().basic(app.fullRegistrationTelenorLoginPhone, smsCode)
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
                 .header("clisessionid", cliSessionId)
                 .header("secretanswer", "test")
                 .body("")
                 .when()
-                .post(app.dipocket3_intranet+"/WebServices/v1/auth/unblockClient")
-                .then().log().all()
-                .assertThat().statusCode(400)
-                .assertThat().body("errCode", equalTo("-1"))
-                .assertThat().body("errDesc", equalTo("Wrong Secret Answer"));
+                .post("/WebServices/v1/auth/unblockClient")
+                .then()
+                .log().all()
+                .statusCode(400)
+                .body("errCode", equalTo("-1"))
+                .body("errDesc", equalTo("Wrong Secret Answer"));
     }
 }
