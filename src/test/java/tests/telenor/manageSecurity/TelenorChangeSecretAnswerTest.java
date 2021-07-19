484edac6f0b486fc6487be2051ddebbf14c118cb
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-07-19 10:18:39 +0300

    refactor TelenorChangeSecretAnswerTest

--- src/test/java/tests/telenor/manageSecurity/TelenorChangeSecretAnswerTest.java
+++ src/test/java/tests/telenor/manageSecurity/TelenorChangeSecretAnswerTest.java
@@ -2,87 +2,126 @@ package tests.telenor.manageSecurity;
 
 import base.TestBase;
 import io.restassured.response.Response;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.blankOrNullString;
+import static org.testng.Assert.assertEquals;
 
 public class TelenorChangeSecretAnswerTest extends TestBase {
     String smsCode = app.generateRandomNumber(6);
     String cliSessionId = null;
+    String  secAnswer = "QA";
+    String  secAnswer2 = "qa";
+    String offloadFundsLoginPhone =  "$5_" + app.offloadFundsPhone;
 
     @Test(priority = 1)
     public void test_TestServices_v1_telenor_sendOtpForPhone_smsCode(){
         System.out.println("smsCod:" + smsCode);
         given().log().uri().log().headers().log().body()
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
+                .spec(app.requestSpecTelenor)
                 .body("{\n" +
-                        "  \"phoneNumber\" : \""+app.telenorRegistrationPhone+"\"\n" +
+                        "  \"phoneNumber\" : \""+app.offloadFundsPhone+"\"\n" +
                         "}")
                 .when()
-                .post(app.dipocket3_intranet+"/TestServices/v1/telenor/sendOtpForPhone/"+smsCode+"")
-                .then().log().all()
-                .assertThat().statusCode(200);
+                .post("/TestServices/v1/telenor/sendOtpForPhone/"+smsCode+"")
+                .then()
+                .log().all()
+                .statusCode(200);
     }
 
     @Test(priority = 2)
     public void test_WebServices_v1_auth_authenticate() {
         Response res = given().log().uri().log().headers().log().body()
-                .auth().preemptive().basic(app.fullRegistrationTelenorLoginPhone, smsCode)
+                .spec(app.requestSpecTelenor)
+                .auth().preemptive().basic(offloadFundsLoginPhone, smsCode)
                 .header("accept", "application/json, text/json;q=0.8, text/plain;q=0.6, */*;q=0.1")
-                .header("site", app.telenorSite)
-                .header("content-type", "application/json; charset=utf-8")
                 .when()
-                .post(app.dipocket3_intranet+"/WebServices/v1/auth/authenticate");
+                .post("/WebServices/v1/auth/authenticate");
 
         cliSessionId = res.getHeader("cliSessionId");
         System.out.println(res.getHeaders());
         System.out.println("cliSessionId " + cliSessionId);
         res.then().log().all();
 
-        Assert.assertEquals(res.getStatusCode(), 200);
-        res.then().assertThat().body("clientFirstName", equalTo("Pavel"));
-        res.then().assertThat().body("clientLastName", equalTo("TestQA"));
-        res.then().assertThat().body("mainPhone", equalTo("380980316499"));
+        assertEquals(res.getStatusCode(), 200);
+        res.then().body("clientFirstName", equalTo("Test"),
+                "clientLastName", equalTo("QA"),
+                        "mainPhone", equalTo(app.offloadFundsPhone));
     }
 
     @Test(priority = 3)
     public void test_WebServices_v1_clientProfile_checkSecAnswAttempts(){
         given().log().uri().log().headers().log().body()
-                .auth().preemptive().basic(app.fullRegistrationTelenorLoginPhone, smsCode)
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
+                .spec(app.requestSpecTelenor)
+                .auth().preemptive().basic(offloadFundsLoginPhone, smsCode)
                 .header("clisessionid", cliSessionId)
                 .body("{\n" +
-                        "  \"secAnswer\" : \"QA\"\n" +
+                        "  \"secAnswer\" : \""+secAnswer+"\"\n" +
                         "}")
                 .when()
-                .post(app.dipocket3_intranet+"/WebServices/v1/clientProfile/checkSecAnswAttempts")
-                .then().log().all()
-                .assertThat().statusCode(200)
-                .assertThat().body("attemptsCount", equalTo(0))
-                .assertThat().body("attemptsLeft", equalTo(3));
+                .post("/WebServices/v1/clientProfile/checkSecAnswAttempts")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("attemptsCount", equalTo(0),
+                        "attemptsLeft", equalTo(3));
     }
 
     @Test(priority = 4)
     public void test_WebServices_v1_clientProfile_updateAnswer(){
         given().log().uri().log().headers().log().body()
-                .auth().preemptive().basic(app.fullRegistrationTelenorLoginPhone, smsCode)
-                .header("content-type", "application/json; charset=utf-8")
-                .header("site", app.telenorSite)
+                .spec(app.requestSpecTelenor)
+                .auth().preemptive().basic(offloadFundsLoginPhone, smsCode)
                 .header("clisessionid", cliSessionId)
-                .header("secretanswer", "QA")
+                .header("secretanswer", secAnswer)
                 .body("{\n" +
                         "  \"secQuestion\" : \"QA\",\n" +
-                        "  \"secAnswer\" : \"qa\"\n" +
+                        "  \"secAnswer\" : \""+secAnswer2+"\"\n" +
                         "}\n")
                 .when()
-                .post(app.dipocket3_intranet+"/WebServices/v1/clientProfile/updateAnswer")
-                .then().log().all()
-                .assertThat().statusCode(200)
-                .assertThat().body(blankOrNullString());
+                .post("/WebServices/v1/clientProfile/updateAnswer")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(blankOrNullString());
+    }
+
+    @Test(priority = 5)
+    public void test_WebServices_v1_clientProfile_checkSecAnswAttempts_(){
+        given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
+                .auth().preemptive().basic(offloadFundsLoginPhone, smsCode)
+                .header("clisessionid", cliSessionId)
+                .body("{\n" +
+                        "  \"secAnswer\" : \""+secAnswer2+"\"\n" +
+                        "}")
+                .when()
+                .post("/WebServices/v1/clientProfile/checkSecAnswAttempts")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("attemptsCount", equalTo(0),
+                        "attemptsLeft", equalTo(3));
+    }
+
+    @Test(priority = 6)
+    public void test_WebServices_v1_clientProfile_updateAnswer_(){
+        given().log().uri().log().headers().log().body()
+                .spec(app.requestSpecTelenor)
+                .auth().preemptive().basic(offloadFundsLoginPhone, smsCode)
+                .header("clisessionid", cliSessionId)
+                .header("secretanswer", secAnswer2)
+                .body("{\n" +
+                        "  \"secQuestion\" : \"QA\",\n" +
+                        "  \"secAnswer\" : \""+secAnswer+"\"\n" +
+                        "}\n")
+                .when()
+                .post("/WebServices/v1/clientProfile/updateAnswer")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body(blankOrNullString());
     }
 }
