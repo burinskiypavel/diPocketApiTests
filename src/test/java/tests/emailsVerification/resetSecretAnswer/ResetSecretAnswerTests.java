e16ca26baeb469ed089b32b8e33526d5ce8312a2
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-08-16 14:07:53 +0300

    refactor

--- src/test/java/tests/emailsVerification/resetSecretAnswer/ResetSecretAnswerTests.java
+++ src/test/java/tests/emailsVerification/resetSecretAnswer/ResetSecretAnswerTests.java
@@ -40,16 +40,16 @@ public class ResetSecretAnswerTests extends TestBase {
         String actualSubject = senderAndSubject.get(1);
 
         String emailText =  EmailVerificationHelper.getTextFromEmail("pop.gmail.com", app.emailsVerificationsEmail, app.emailsVerificationsPass);
-        String actualBody = getEmailBodyText(emailText, 28, 217);
-        String actualFooter = getEmailFooterText(emailText, 218);
+        String actualBody = getEmailBodyText(emailText, 10, 215);
+        String actualFooter = getEmailFooterText(emailText, 216);
 
-        //assertThat(actualSender, equalTo(expectedSender));
+        assertThat(actualSender, equalTo(expectedSender));
         assertThat(actualSubject, equalTo("Your secret answer reset request"));
-        //assertThat(actualBody, equalTo(""));
-        //assertThat(actualFooter, equalTo(""+app.SITE_REG+" "+app.site_Telenor+" is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Masterсard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
+        assertThat(actualBody, equalTo("Dear Test. We received a request to reset the secret answer associated with your account. Please click on this link to confirm your request and finalise the change. With kind regards, Customer Service Team"));
+        assertThat(actualFooter, equalTo(""+app.SITE_REG+" "+app.site_Telenor+" is powered by DiPocket UAB, authorised Electronic Money Institution regulated by the Bank of Lithuania (#75) | Licensed by Mastercard for the European Economic Area Upės str. 23, 08128 Vilnius, LT"));
     }
 
-    @Test(enabled = false)//incorrect subject, footer, body
+    @Test()//incorrect body, footer
     public void testResetSecretAnswerTelenorHU() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
         app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "5", "TELENOR");
         postSendResetPasswordEmail();
@@ -62,10 +62,10 @@ public class ResetSecretAnswerTests extends TestBase {
         String actualBody = getEmailBodyText(emailText, 28, 217);
         String actualFooter = getEmailFooterText(emailText, 218);
 
-        //assertThat(actualSender, equalTo(expectedSender));
-        assertThat(actualSubject, equalTo(""));
-        assertThat(actualBody, equalTo(""));
-        assertThat(actualFooter, equalTo(""));
+        assertThat(actualSender, equalTo(expectedSender));
+        assertThat(actualSubject, equalTo("Biztonsági kérdés visszaállítási kérése"));
+        //assertThat(actualBody, equalTo(""));
+        //assertThat(actualFooter, equalTo(""+app.SITE_REG+" "+app.site_Telenor+", a DiPocket UAB támogatásával, mely vállalatot a Litván Nemzeti Bank elektronikus pénzintézetenként (# 75) engedélyezett és felügyel | a Mastercard licencével rendelkezik az Európai Gazdasági Térségre vonatkozva Upės str. 23, 08128 Vilnius, LT"));
     }
 
 
