da2ae3b35152280b0f2e802ca5ed18ef601e64b7
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-09-17 13:03:57 +0300

    refactor

--- src/test/java/tests/emailsVerification/statement/StatementAttachmentsUpAndGoTests.java
+++ src/test/java/tests/emailsVerification/statement/StatementAttachmentsUpAndGoTests.java
@@ -3,7 +3,6 @@ package tests.emailsVerification.statement;
 import appmanager.EmailVerificationHelper;
 import appmanager.HelperBase;
 import base.TestBase;
-import io.restassured.response.Response;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
@@ -16,75 +15,22 @@ import java.util.List;
 import static appmanager.EmailIMAPHelper.getEmailBodyText;
 import static appmanager.EmailIMAPHelper.getEmailFooterText;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.hasItems;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.testng.Assert.assertEquals;
 
 public class StatementAttachmentsUpAndGoTests extends TestBase {
     String cliSessionId = null;
-    String email = "testdipocket3@gmail.com";
-    String emailPass = "pasword12!";
-
-    //String cliSessionId = null;
+    //String emailPass = "pasword12!";
     //String email = "testdipocket@gmail.com";
     String pass = "DvAUtN6";
     String phone = "380685448615";
     String deviceuuid = "380685448615-AutoTest-Login";
     String expectedUpAndGoSender = "zestawienia@upcard.pl";
 
-    @Test(priority = 1)
-    public void test_ClientServices_v1_homePage_AutintificateMobileApp() throws SQLException, ClassNotFoundException {
-        app.getDbHelper().deleteClientDeviceFromDB(deviceuuid);
-        given().log().all()
-                .auth().preemptive().basic("10_" + phone, pass)
-                .header("deviceuuid", deviceuuid)
-                .header("site", app.mobile_site_upAndGo)
-                .contentType("application/json; charset=UTF-8")
-                .body("{\n" +
-                        "  \"devToken\" : \"eGy9q-lDQBGKz-bgdz1U6q:APA91bF8bT00_Cj-KVTiTSLlB-LBL8itr4LKxJVSxKJGZs3eyvHMbLZ4mZWYyo_r290PQFuKhx7mQOgAFeisGhBByoHXzQ0ANETYA-nTnDGM29zXKxcaIh47qJ7dyFQymXolPLYtmeM8\",\n" +
-                        "  \"devType\" : \"android\",\n" +
-                        "  \"deviceUUID\" : \""+ deviceuuid+"\",\n" +
-                        "  \"appVersion\" : \"1.1.11\"\n" +
-                        "}")
-                .when()
-                .post( HelperBase.prop.getProperty("mobile.base.url")+"homePage/authenticateMobileApp")
-                .then().log().all()
-                .statusCode(400)
-                //.body("errDesc", equalTo("Введите код (#1) из SMS, что б подтвердить вход на этом устройстве"))
-                .body("errCode", equalTo("DIP-00591"));
-
-        String loginSMSCode = app.getDbHelper().getLoginSMSFromDB(phone, deviceuuid, "UPANDGO");
-
-        Response res =  given().log().all()
-                .auth().preemptive().basic("10_" + phone, pass)
-                .header("deviceuuid", deviceuuid)
-                .header("site", app.mobile_site_upAndGo)
-                .contentType("application/json; charset=UTF-8")
-                .body("{\n" +
-                        "  \"devToken\" : \"eGy9q-lDQBGKz-bgdz1U6q:APA91bF8bT00_Cj-KVTiTSLlB-LBL8itr4LKxJVSxKJGZs3eyvHMbLZ4mZWYyo_r290PQFuKhx7mQOgAFeisGhBByoHXzQ0ANETYA-nTnDGM29zXKxcaIh47qJ7dyFQymXolPLYtmeM8\",\n" +
-                        "  \"devType\" : \"android\",\n" +
-                        "  \"deviceUUID\" : \""+ deviceuuid+"\",\n" +
-                        "  \"appVersion\" : \"1.1.11\",\n" +
-                        "  \"otp\" : \""+loginSMSCode+"\"\n" +
-                        "}")
-                .when()
-                .post( HelperBase.prop.getProperty("mobile.base.url")+"homePage/authenticateMobileApp");
-        cliSessionId = res.getHeader("cliSessionId");
-        System.out.println(res.getHeaders());
-        System.out.println("cliSessionId " + cliSessionId);
-        //app.getDbHelper().storeDataToTheFile("files\\tds\\cliSessionId.txt",cliSessionId);
-        int StatusCode = res.getStatusCode();
-        assertEquals(StatusCode, 200);
-    }
 
     @Test(priority = 2)
     public void test_dashBoard_customerStatementRequestList() throws SQLException, ClassNotFoundException {
-        //app.getDbHelper().deleteClientDeviceFromDB("380980316499-AutoTest-Login");
         //app.getDbHelper().updateClientLanguageFromDB(email, "1", app.mobile_site_upAndGo);
-
-        //cliSessionId = login("380980316499", "reset246740", "380980316499-AutoTest-Login", "DIPOCKET");
-
+        cliSessionId = app.getLogin_registrationHelper().loginUpAndGo(phone, pass, deviceuuid);
 
         given()
                 .baseUri(HelperBase.prop.getProperty("mobile.base.url"))
@@ -100,35 +46,10 @@ public class StatementAttachmentsUpAndGoTests extends TestBase {
                         "statementRequestList.year", hasItems("2021"));
     }
 
-    public void sendCustomerStatements(String phone, String pass, String cliSessionId) {
-        given().log().uri().log().headers().log().body()
-                //.spec(app.requestSpecDipocketHomePage)
-                .baseUri(HelperBase.prop.getProperty("mobile.base.url"))
-                .header("site", HelperBase.prop.getProperty("mobile.site.upAndGo"))
-                .header("deviceuuid", deviceuuid)
-                .auth().preemptive().basic("10_" + phone, pass)
-                .header("clisessionid", cliSessionId)
-                .contentType("application/json")
-                .body("{\n" +
-                        "  \"reportTypeId\": 1,\n" +
-                        "  \"statementRequestList\": [\n" +
-                        "    {\n" +
-                        //"    \"isOn\": false,\n" +
-                        "      \"month\": \"07\",\n" +
-                        "      \"year\": \"2021\"\n" +
-                        "    }\n" +
-                        "  ]\n" +
-                        "}")
-                .when()
-                .put("/dashBoard/sendCustomerStatements")
-                .then().log().all()
-                .statusCode(200);
-    }
-
     @Test(priority = 3)
     public void test_dashBoard_sendCustomerStatements_UpAndGoRU() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
         app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "4", app.mobile_site_upAndGo);
-        sendCustomerStatements(phone, pass, "" + cliSessionId + "");
+        app.getAttachmentHelper().sendCustomerStatements(phone, pass, "" + cliSessionId + "", "07", "2021", app.mobile_site_upAndGo, deviceuuid, "10_");
 
         List<String> senderAndSubject = EmailVerificationHelper.getEmailSenderAndSubject(app.emailsVerificationsEmail, app.emailsVerificationsPass);
         String actualSender = senderAndSubject.get(0);
@@ -150,7 +71,7 @@ public class StatementAttachmentsUpAndGoTests extends TestBase {
     @Test(priority = 4)
     public void test_dashBoard_sendCustomerStatements_UpAndGoEN() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
         app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "1", app.mobile_site_upAndGo);
-        sendCustomerStatements(phone, pass, "" + cliSessionId + "");
+        app.getAttachmentHelper().sendCustomerStatements(phone, pass, "" + cliSessionId + "", "06", "2021", app.mobile_site_upAndGo, deviceuuid, "10_");
 
         List<String> senderAndSubject = EmailVerificationHelper.getEmailSenderAndSubject(app.emailsVerificationsEmail, app.emailsVerificationsPass);
         String actualSender = senderAndSubject.get(0);
@@ -161,7 +82,7 @@ public class StatementAttachmentsUpAndGoTests extends TestBase {
         String actualFooter = getEmailFooterText(emailText, 174);
 
         SoftAssert softAssert = new SoftAssert();
-        softAssert.assertEquals(actualAttachedFileNames, Arrays.asList("statement-07.2021.pdf"), "File name is not correct");
+        softAssert.assertEquals(actualAttachedFileNames, Arrays.asList("statement-06.2021.pdf"), "File name is not correct");
         //softAssert.assertEquals(actualSender, expectedUpAndGoSender, "Sender is not correct");
         softAssert.assertEquals(actualSubject, "Your "+app.site_upAndGo+" account statement", "Subject is not correct");
         softAssert.assertEquals(actualBody, "Dear "+app.emailsVerificationsFirstName+", As requested, please find attached your up and go account statement(s). Thank you for using "+app.site_upAndGo+". With kind regards, Legal Team", "Body is not correct");
@@ -172,7 +93,7 @@ public class StatementAttachmentsUpAndGoTests extends TestBase {
     @Test(priority = 5)
     public void test_dashBoard_sendCustomerStatements_UpAndGoPL() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
         app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "3", app.mobile_site_upAndGo);
-        sendCustomerStatements(phone, pass, "" + cliSessionId + "");
+        app.getAttachmentHelper().sendCustomerStatements(phone, pass, "" + cliSessionId + "", "07", "2021", app.mobile_site_upAndGo, deviceuuid, "10_");
 
         List<String> senderAndSubject = EmailVerificationHelper.getEmailSenderAndSubject(app.emailsVerificationsEmail, app.emailsVerificationsPass);
         String actualSender = senderAndSubject.get(0);
@@ -194,7 +115,7 @@ public class StatementAttachmentsUpAndGoTests extends TestBase {
     @Test(priority = 6)
     public void test_dashBoard_sendCustomerStatements_UpAndGoUA() throws InterruptedException, MessagingException, IOException, SQLException, ClassNotFoundException {
         app.getDbHelper().updateClientLanguageFromDB(app.emailsVerificationsEmail, "2", app.mobile_site_upAndGo);
-        sendCustomerStatements(phone, pass, "" + cliSessionId + "");
+        app.getAttachmentHelper().sendCustomerStatements(phone, pass, "" + cliSessionId + "", "07", "2021", app.mobile_site_upAndGo, deviceuuid, "10_");
 
         List<String> senderAndSubject = EmailVerificationHelper.getEmailSenderAndSubject(app.emailsVerificationsEmail, app.emailsVerificationsPass);
         String actualSender = senderAndSubject.get(0);
@@ -210,47 +131,5 @@ public class StatementAttachmentsUpAndGoTests extends TestBase {
         softAssert.assertEquals(actualBody,"Вітаємо, "+app.emailsVerificationsFirstName+"! В додатку знаходиться замовлена Вами банківська виписка по рахунку "+app.site_upAndGo+". Дякуємо за користування додатком "+app.site_upAndGo+". З повагою, Юридичний відділ", "Body is not correct");
         softAssert.assertEquals(actualFooter,""+app.SITE_REG+" Для Вашого спокою, "+app.site_upAndGo+" працює при підтримці DiPocket UAB, що авторизований та контролюється Банком Литви, як емітент електронних грошей (#75) Upės str. 23, 08128 Vilnius, LT", "Footer is not correct");
         softAssert.assertAll();
-    }
-
-    public String login(String phone, String pass, final String deviceUUID, String site) throws ClassNotFoundException, SQLException {
-        given()
-                .spec(app.requestSpecDipocketHomePage)
-                .auth().preemptive().basic(phone, pass)
-                .contentType("application/json; charset=UTF-8")
-                .body("{\n" +
-                        "  \"devToken\" : \"eGy9q-lDQBGKz-bgdz1U6q:APA91bF8bT00_Cj-KVTiTSLlB-LBL8itr4LKxJVSxKJGZs3eyvHMbLZ4mZWYyo_r290PQFuKhx7mQOgAFeisGhBByoHXzQ0ANETYA-nTnDGM29zXKxcaIh47qJ7dyFQymXolPLYtmeM8\",\n" +
-                        "  \"devType\" : \"android\",\n" +
-                        "  \"deviceUUID\" : \"" + deviceUUID + "\",\n" +
-                        "  \"appVersion\" : \"2.2.7\"\n" +
-                        "}")
-                .when()
-                .post( "homePage/authenticateMobileApp")
-                .then().log().all()
-                .statusCode(400)
-                //.body("errDesc", equalTo("Введите код (#1) из SMS, что б подтвердить вход на этом устройстве"))
-                .body("errCode", equalTo("DIP-00591"));
-
-
-        String loginSMSCode = app.getDbHelper().getLoginSMSFromDB(phone, deviceUUID, site);
-
-        Response res =  given()
-                .spec(app.requestSpecDipocketHomePage)
-                .auth().preemptive().basic(phone, pass)
-                .contentType("application/json; charset=UTF-8")
-                .body("{\n" +
-                        "  \"devToken\" : \"eGy9q-lDQBGKz-bgdz1U6q:APA91bF8bT00_Cj-KVTiTSLlB-LBL8itr4LKxJVSxKJGZs3eyvHMbLZ4mZWYyo_r290PQFuKhx7mQOgAFeisGhBByoHXzQ0ANETYA-nTnDGM29zXKxcaIh47qJ7dyFQymXolPLYtmeM8\",\n" +
-                        "  \"devType\" : \"android\",\n" +
-                        "  \"deviceUUID\" : \""+deviceUUID+"\",\n" +
-                        "  \"appVersion\" : \"2.2.7\",\n" +
-                        "  \"otp\" : \""+loginSMSCode+"\"\n" +
-                        "}")
-                .when()
-                .post( "homePage/authenticateMobileApp");
-        String cliSessionId = res.getHeader("cliSessionId");
-        System.out.println(res.getHeaders());
-        System.out.println("cliSessionId " + cliSessionId);
-        int StatusCode = res.getStatusCode();
-        assertEquals(StatusCode, 200);
-        return cliSessionId;
     }
 }
