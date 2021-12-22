0e2769e31b2e4ac6c37d2794198ed3ea4b0e9733
pa.burinsky <pa.burinsky@csltd.com.ua>
2021-12-22 11:15:28 +0200

    refactor

---
+++ src/test/java/tests/uiTests/bo/boUser/BOUserRolesCBOAllUsersTableTest.java
@@ -0,0 +1,21 @@
+package tests.uiTests.bo.boUser;
+
+import base.UITestBase;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class BOUserRolesCBOAllUsersTableTest extends UITestBase {
+
+    @Test
+    public void testBOUserRolesCBOAllUsersTable() throws InterruptedException {
+        gotoBOSiteAndLoginWithCBOUserRole("Viktoria", "kWmaB0s");
+        click(By.xpath("//p[contains(text(), 'BO Users')]"));
+        click(By.id("p-tabpanel-1-label"));
+
+        assertTrue(isElementPresent(By.xpath("//a[@id='p-tabpanel-1-label'][@aria-selected='true']")));
+        assertTrue(isElementPresent(By.xpath("//div[@id='p-tabpanel-1'][@aria-hidden='false']")));
+        assertTrue(isElementPresent(By.cssSelector("table")));
+    }
+}
