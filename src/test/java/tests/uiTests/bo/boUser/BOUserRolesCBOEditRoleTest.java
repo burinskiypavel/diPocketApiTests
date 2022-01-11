aeeed5a2c39f477d9bae86dd9129340dda91fce9
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-01-11 16:27:24 +0200

    refactor

--- src/test/java/tests/uiTests/bo/boUser/BOUserRolesCBOEditRoleTest.java
+++ src/test/java/tests/uiTests/bo/boUser/BOUserRolesCBOEditRoleTest.java
@@ -11,31 +11,14 @@ public class BOUserRolesCBOEditRoleTest extends UITestBase {
     @Test
     public void testBOUserRolesCBOEditRole() throws InterruptedException {
         gotoBOSiteAndLoginWithCBOUserRole("Viktoria", "kWmaB0s");
-        click(By.xpath("//p[contains(text(), 'BO Users')]"));
-        waitFor(By.id("p-tabpanel-2-label"));
-        click(By.id("p-tabpanel-2-label"));
-
-        //click(By.cssSelector("app-button[label='+ Add']"));
-        //waitForSeveralItems(new String[]{"Role ID:", "Role name:", "Add Role"});
-
-        //type(By.cssSelector("app-input[ng-reflect-label='Role ID'] input[type='text']"), "testqa");
-
-        //type(By.cssSelector("app-input[ng-reflect-label='Role name'] input[type='text']"), "testqa2");
-
-        //click(By.cssSelector("app-button[ng-reflect-label='Add']"));
-
-
-        click(By.cssSelector("p-dropdown[placeholder='Role']"));
-        click(By.cssSelector("li[aria-label='1']"));
+        gotoBOUsersPage();
+        gotoRolesTab();
+        selectRoleFromDropDown("1");
 
         click(By.cssSelector("app-button[label='Edit']"));
-
-        type(By.cssSelector("div[role='dialog'] input[type='text']"), "test1");
-
+        type(By.cssSelector("div[role='dialog'] input[type='text']"), "test2");
         click(By.cssSelector("div[role='dialog'] app-button[ng-reflect-label='Edit']"));
 
-        //clickCheckbox(By.cssSelector("div[role='checkbox']"));
-        //click(By.cssSelector("app-button[label='Update']"));
 
         //assertTrue(isElementPresent(By.xpath("//div[contains(text(), 'User role changed successfully')]")));
 
