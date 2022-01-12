1375643a7538d1e1a9066e996e6a344591a9cf50
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-01-12 14:25:31 +0200

    refactor

--- src/test/java/tests/uiTests/bo/boUser/BOUserRolesCBORolesTableTest.java
+++ src/test/java/tests/uiTests/bo/boUser/BOUserRolesCBORolesTableTest.java
@@ -3,32 +3,32 @@ package tests.uiTests.bo.boUser;
 import base.UITestBase;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
 
 public class BOUserRolesCBORolesTableTest extends UITestBase {
+    SoftAssert softAssert = new SoftAssert();
 
     @Test
     public void testBOUserRolesCBORolesTable() throws InterruptedException {
         gotoBOSiteAndLoginWithCBOUserRole("Viktoria", "kWmaB0s");
-        click(By.xpath("//p[contains(text(), 'BO Users')]"));
-        waitFor(By.id("p-tabpanel-2-label"));
-        click(By.id("p-tabpanel-2-label"));
+        gotoBOUsersPage();
+        gotoRolesTab();
 
-        assertTrue(isTabActiveAndSelected(By.xpath("//a[@id='p-tabpanel-2-label'][@aria-selected='true']")));
-        assertTrue(isPageOpen(By.xpath("//div[@id='p-tabpanel-2'][@aria-hidden='false']")));
+        softAssert.assertTrue(isTabActiveAndSelected(By.xpath("//a[@id='p-tabpanel-2-label'][@aria-selected='true']")));
+        softAssert.assertTrue(isPageOpen(By.xpath("//div[@id='p-tabpanel-2'][@aria-hidden='false']")));
 
         click(By.cssSelector("p-dropdown[placeholder='Role']"));
         click(By.cssSelector("li[aria-label='1']"));
 
-        assertTrue(isButtonEnabled(By.cssSelector("app-button[label='Edit")));
-        assertTrue(isButtonEnabled(By.cssSelector("app-button[label='Delete")));
+        softAssert.assertTrue(isButtonEnabled(By.cssSelector("app-button[label='Edit")));
+        softAssert.assertTrue(isButtonEnabled(By.cssSelector("app-button[label='Delete")));
 
-        assertFalse(!areElementsPresent(new String[]{"//th[contains(text(), 'Code')]",
+        softAssert.assertFalse(!areElementsPresent(new String[]{"//th[contains(text(), 'Code')]",
                 "//th[contains(text(), 'Name')]", "//th[contains(text(), 'Description')]",
                 "//th[contains(text(), 'Checked')]", }));
 
-        assertTrue(isButtonEnabled(By.cssSelector("app-button[label='Update']")));
+        softAssert.assertTrue(isButtonEnabled(By.cssSelector("app-button[label='Update']")));
+        softAssert.assertAll();
     }
 }
