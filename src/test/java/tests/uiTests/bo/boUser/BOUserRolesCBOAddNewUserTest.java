23590eda2f868597202af3499ad95e1009926b5a
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-01-13 15:50:39 +0200

    refactor

---
+++ src/test/java/tests/uiTests/bo/boUser/BOUserRolesCBOAddNewUserTest.java
@@ -0,0 +1,36 @@
+package tests.uiTests.bo.boUser;
+
+import base.UITestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptException;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class BOUserRolesCBOAddNewUserTest extends UITestBase {
+
+    @Test
+    public void testBOUserRolesCBOAddNewUser() throws InterruptedException {
+        gotoBOSiteAndLoginWithCBOUserRole("Viktoria", "kWmaB0s");
+        gotoBOUsersPage();
+        click(By.cssSelector("app-button[ng-reflect-label='Add User']"));
+        waitForSeveralItems(new String[]{"Role:", "Site:", "Firstname:",
+                "Lastname:", "Phone:", "Email:", "Login (Username):",
+                "Portal client(optional):", "Portal clients for management (optional):", "Upload Photo:"});
+
+        assertTrue(isButtonEnabled(By.cssSelector("span.p-fileupload-choose span.p-button-label")));
+        assertTrue(isButtonEnabled(By.cssSelector("app-button[ng-reflect-label='Add user")));
+
+        click(By.cssSelector("app-select-async[ng-reflect-label='Role']"));
+        click(By.xpath("//ul[@role='listbox'] //span[contains(text(), 'CBO')]"));
+
+        click(By.cssSelector("app-select-async[ng-reflect-label='Site']"));
+        click(By.xpath("//ul[@role='listbox'] //span[contains(text(), 'SODEXO')]"));
+
+        type(By.cssSelector("app-input[ng-reflect-label='Firstname'] input[type='text']"), "Pavel");
+    }
+}
