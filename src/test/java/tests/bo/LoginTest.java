55747b48488110ed27a4c04f6752fa6e100f7091
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-01-18 18:37:43 +0200

    refactor

--- src/test/java/tests/bo/LoginTest.java
+++ src/test/java/tests/bo/LoginTest.java
@@ -3,37 +3,40 @@ package tests.bo;
 import appmanager.HelperBase;
 import base.TestBase;
 import config.Properties;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 public class LoginTest extends TestBase {
+    String cookie = null;
 
     @Test(priority = 1)
     public void test_BOServices_v1_user_authentication(){
-        given()
+        Response response = given()
                 .log().uri().log().headers()
                 .auth().preemptive().basic("PAVELB", "D5kHO7a")
                 .contentType("application/json")
                 .when()
-                .post( "https://support.dipocket.dev/BOServices/v1/user/authentication")
-                .then().log().all()
+                .post( "https://support.dipocket.dev/BOServices/v1/user/authentication");
+        response.then().log().all()
                 .statusCode(200)
                 .body("username", equalTo("PAVELB"));
+        cookie = response.getHeader("Set-Cookie");
+        System.out.println("cookie: " + cookie);
     }
 
     @Test(priority = 2)
     public void test_BOServices_v1_user_checkAuthentication(){
         given()
                 .log().uri().log().headers()
-                //.auth().preemptive().basic("PAVELB", "D5kHO7a")
-                .cookie("JSESSIONID=5AEF1371E86541E0B545BC09CC579A46")
+                .cookie(cookie)
                 .contentType("application/json")
                 .when()
                 .get( "https://support.dipocket.dev/BOServices/v1/user/checkAuthentication")
                 .then().log().all()
-                .statusCode(200);
-                //.body("username", equalTo("PAVELB"));
+                .statusCode(200)
+                .body("value", equalTo(true));
     }
 }
