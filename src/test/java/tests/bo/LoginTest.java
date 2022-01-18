f35453b6ac49c9c4272828679d0606422f166ad1
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-01-18 18:22:20 +0200

    refactor

--- src/test/java/tests/bo/LoginTest.java
+++ src/test/java/tests/bo/LoginTest.java
@@ -10,7 +10,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 public class LoginTest extends TestBase {
 
-    @Test
+    @Test(priority = 1)
     public void test_BOServices_v1_user_authentication(){
         given()
                 .log().uri().log().headers()
@@ -21,6 +21,19 @@ public class LoginTest extends TestBase {
                 .then().log().all()
                 .statusCode(200)
                 .body("username", equalTo("PAVELB"));
+    }
 
+    @Test(priority = 2)
+    public void test_BOServices_v1_user_checkAuthentication(){
+        given()
+                .log().uri().log().headers()
+                //.auth().preemptive().basic("PAVELB", "D5kHO7a")
+                .cookie("JSESSIONID=5AEF1371E86541E0B545BC09CC579A46")
+                .contentType("application/json")
+                .when()
+                .get( "https://support.dipocket.dev/BOServices/v1/user/checkAuthentication")
+                .then().log().all()
+                .statusCode(200);
+                //.body("username", equalTo("PAVELB"));
     }
 }
