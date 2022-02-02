fa872221608bc9cccf92fe9fe925588e8941f81d
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-02-02 13:20:37 +0200

    refactor

--- src/test/java/tests/tds/TDSV2NativeIncorrectTranTest.java
+++ src/test/java/tests/tds/TDSV2NativeIncorrectTranTest.java
@@ -19,7 +19,7 @@ public class TDSV2NativeIncorrectTranTest extends TestBase {
     String dsTransId = app.generateRandomNumber(10) + "-integrTest-dsTransId-v2";
 
     @Test(priority = 1)
-    public void test_AReq_DiPocket3ds_acs_bgAuth() throws IOException, SAXException, ParserConfigurationException {
+    public void test_AReq_TDSServices_acs_bgAuth() throws IOException, SAXException, ParserConfigurationException {
         String now = app.getTimeStamp("YYYYMMddHHmmss");
         String pan4TestSMS = "5455980666358066";
         Response res = given()
@@ -70,7 +70,7 @@ public class TDSV2NativeIncorrectTranTest extends TestBase {
                         "   </backgroundAReq>\n" +
                         "</backgroundRequest2>")
                 .when()
-                .post("/DiPocket3ds/acs/bgAuth");
+                .post("/TDSServices/acs/bgAuth");
 
         res.then().log().all().statusCode(200)
                 .body("backgroundResponse2.backgroundARes.messageExtension", equalTo(""));
