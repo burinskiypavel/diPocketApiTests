5652e530603c53213e750af8c5c50be28e3f910f
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-02-04 16:08:36 +0200

    refactor

--- src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientsPageEditProfileDataTest.java
+++ src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientsPageEditProfileDataTest.java
@@ -2,8 +2,9 @@ package tests.uiTests.bo.boClient;
 
 import base.UITestBase;
 import org.openqa.selenium.By;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
 
 public class RolesBOUserClientsPageEditProfileDataTest extends UITestBase {
 
@@ -16,18 +17,23 @@ public class RolesBOUserClientsPageEditProfileDataTest extends UITestBase {
         click(By.cssSelector("td[ng-reflect-text='380634413376']"));
         waitFor(By.cssSelector("p.user-name"));
         click(By.cssSelector("div.edit-button"));
+        waitFor(By.xpath("//app-select-async[@ng-reflect-name='gender']"));
+        waitFor(By.xpath("//app-button[@ng-reflect-label='Save']"));
 
-        Assert.assertTrue(isElementPresent(By.cssSelector("app-input[ng-reflect-name='firstName'] input[ng-reflect-model='Nona']")));
-        Assert.assertTrue(isElementPresent(By.cssSelector("app-input[ng-reflect-name='lastName'] input[ng-reflect-model='Qwerty']")));
-        Assert.assertTrue(isElementPresent(By.cssSelector("p-calendar[ng-reflect-model='Wed Oct 06 1976 03:00:00 GMT+0']")));
-        Assert.assertTrue(isElementPresent(By.xpath("//app-select-async[@ng-reflect-name='gender'] //span[contains(text(), 'F')]")));
-        Assert.assertTrue(isElementPresent(By.cssSelector("app-input[ng-reflect-name='email'] input[ng-reflect-model='vikarez20@gmail.com']")));
-        Assert.assertTrue(isElementPresent(By.xpath("//app-select-async[@ng-reflect-label='Document type'] //span[contains(text(), 'Passport')]")));
-        Assert.assertTrue(isElementPresent(By.cssSelector("app-input[ng-reflect-label='Pesel']")));
-        Assert.assertTrue(isElementPresent(By.cssSelector("app-input[ng-reflect-label='Doc serial number'] input[ng-reflect-model='CGH164279']")));
-
-
-        Assert.assertTrue(isElementPresent(By.xpath("//app-select-async[@ng-reflect-label='Doc country of issue'] //span[contains(text(), 'Aland Islands')]")));
-
+        assertTrue(isElementPresent(By.cssSelector("app-input[ng-reflect-name='firstName'] input[ng-reflect-model='Nona']")));
+        assertTrue(isElementPresent(By.cssSelector("app-input[ng-reflect-name='lastName'] input[ng-reflect-model='Qwerty']")));
+        assertTrue(isElementPresent(By.cssSelector("p-calendar[ng-reflect-model='Wed Oct 06 1976 03:00:00 GMT+0']")));
+        assertTrue(isElementPresent(By.xpath("//app-select-async[@ng-reflect-name='gender'] //span[contains(text(), 'F')]")));
+        assertTrue(isElementPresent(By.cssSelector("app-input[ng-reflect-name='email'] input[ng-reflect-model='vikarez20@gmail.com']")));
+        assertTrue(isElementPresent(By.xpath("//app-select-async[@ng-reflect-label='Document type'] //span[contains(text(), 'Passport')]")));
+        assertTrue(isElementPresent(By.cssSelector("app-input[ng-reflect-label='Pesel']")));
+        assertTrue(isElementPresent(By.cssSelector("app-input[ng-reflect-label='Doc serial number'] input[ng-reflect-model='CGH164279']")));
+        assertTrue(isElementPresent(By.xpath("//app-select-async[@ng-reflect-label='Doc country of issue'] //span[contains(text(), 'Aland Islands')]")));
+        assertTrue(isElementPresent(By.cssSelector("app-input[ng-reflect-name='mailingStreetLine1']")));
+        assertTrue(isElementPresent(By.cssSelector("app-input[ng-reflect-name='mailingStreetLine2']")));
+        assertTrue(isElementPresent(By.cssSelector("app-input[ng-reflect-name='mailingCity'] input[ng-reflect-model='City']")));
+        assertTrue(isElementPresent(By.xpath("//app-select-async[@ng-reflect-label='Country'] //span[contains(text(), 'Poland')]")));
+        assertTrue(isElementPresent(By.cssSelector("app-input[ng-reflect-name='mailingPostcode'] input[ng-reflect-model='11-11']")));
+        assertTrue(areButtonsPresent(new String[]{"//app-button[@ng-reflect-label='Save']"}));
     }
 }
