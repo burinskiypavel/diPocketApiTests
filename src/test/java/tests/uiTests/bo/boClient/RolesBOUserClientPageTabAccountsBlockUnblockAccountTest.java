ff6cd917dcef1f490be3b7d8c90b3e69440d0798
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-04-12 19:02:07 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageTabAccountsBlockUnblockAccountTest.java
+++ src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageTabAccountsBlockUnblockAccountTest.java
@@ -2,6 +2,7 @@ package tests.uiTests.bo.boClient;
 
 import base.UITestBase;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
@@ -168,5 +169,23 @@ public class RolesBOUserClientPageTabAccountsBlockUnblockAccountTest extends UIT
 
         closePopUp(By.cssSelector("span.p-dialog-header-close-icon"));
         waitForInvisibilityOfElement(By.cssSelector("div[role='dialog']"));
+    }
+
+    @Test(priority = 8)
+    public void testRolesBOUserClientPageTabAccountsResetContactlessCounters() throws InterruptedException {
+        gotoBOSiteAndLoginWithBOUserRole(app.BOuserLogin, app.BOuserPass);
+        gotoSearchPage();
+        search("id", clientId, phone);
+        goToClientPage(phone);
+        goToAccountsTab();
+        click(By.cssSelector("td[ng-reflect-text='"+accountName+"']"));
+        waitFor(By.cssSelector("td[ng-reflect-text='"+cardId+"']"));
+        performContextClick(By.cssSelector("td[ng-reflect-text='"+cardId+"']"));
+        click(By.xpath("//li //span[contains(text(), 'Reset contactless counter')]"));
+        String actualPopupText = getTextFromPopUp2(By.cssSelector("app-reset-contactless-counter-modal p"));
+        click(By.cssSelector("app-button[label='Reset']"));
+        waitFor(By.xpath("//div[contains(text(), 'Contactless counter was successfully reset')]"));
+
+        assertEquals(actualPopupText, "Are you sure you want to reset contactless counter for card 187562?");
     }
 }
