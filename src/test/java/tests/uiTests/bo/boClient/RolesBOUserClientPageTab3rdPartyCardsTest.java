fa86af45ff49b480a1b76cb901b55618f04cbef0
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-04-14 14:43:00 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageTab3rdPartyCardsTest.java
+++ src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageTab3rdPartyCardsTest.java
@@ -4,14 +4,10 @@ import base.UITestBase;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class RolesBOUserClientPageTab3rdPartyCardsTest extends UITestBase {
-    String accountName = "test";
     String phone = "380634413376";
-    String randomLimit = app.generateRandomNumber(4);
-    String cardId = "187562";
     String clientId = "33217";
 
     @Test
@@ -21,14 +17,13 @@ public class RolesBOUserClientPageTab3rdPartyCardsTest extends UITestBase {
         search("id", clientId, phone);
         goToClientPage(phone);
         goTo3rdPartyCardsTab();
-//        click(By.cssSelector("td[ng-reflect-text='"+accountName+"']"));
-//        waitFor(By.cssSelector("td[ng-reflect-text='"+cardId+"']"));
-//        performContextClick(By.cssSelector("td[ng-reflect-text='"+cardId+"']"));
-//        click(By.xpath("//li //span[contains(text(), 'Reset contactless counter')]"));
-//        String actualPopupText = getTextFromPopUp2(By.cssSelector("app-reset-contactless-counter-modal p"));
-//        click(By.cssSelector("app-button[label='Reset']"));
-//        waitFor(By.xpath("//div[contains(text(), 'Contactless counter was successfully reset')]"));
-//
-//        assertEquals(actualPopupText, "Are you sure you want to reset contactless counter for card 187562?");
+        waitFor(By.xpath("//thead //th[contains(text(), 'Card name')]"));
+
+        assertTrue(areElementsPresent(new String[]{
+                "//thead //th[contains(text(), 'Card name')]", "//thead //th[contains(text(), 'State')]",
+                "//thead //th[contains(text(), 'CCY')]", "//thead //th[contains(text(), 'Cardholder name')]",
+                "//thead //th[contains(text(), 'Registration date')]", "//thead //th[contains(text(), 'Card number')]",
+                "//thead //th[contains(text(), 'Card type')]", "//thead //th[contains(text(), '3DS Check')]",
+                "//thead //th[contains(text(), 'AVS Check')]"}));
     }
 }
