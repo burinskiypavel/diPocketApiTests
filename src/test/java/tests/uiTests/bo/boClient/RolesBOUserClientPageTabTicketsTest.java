b240087b75a2ae0be7b097638d38bd0a4730c6cd
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-04-22 15:33:52 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageTabTicketsTest.java
+++ src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageTabTicketsTest.java
@@ -2,6 +2,8 @@ package tests.uiTests.bo.boClient;
 
 import base.UITestBase;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
@@ -24,5 +26,10 @@ public class RolesBOUserClientPageTabTicketsTest extends UITestBase {
                 "//thead //th[contains(text(), 'Ticket Type')]", "//thead //th[contains(text(), 'User')]",
                 "//thead //th[contains(text(), 'Ticket State')]", "//thead //th[contains(text(), 'Closed')]",
                 "//thead //th[contains(text(), 'Last comment')]"}));
+
+        //setClientPageFilter("id", "25596");
+        WebElement element = driver.findElements(By.cssSelector("p-columnfilter[field='id'] input[type='text']")).get(1);
+        element.sendKeys("25596");
+        pressKeys(Keys.ENTER);
     }
 }
