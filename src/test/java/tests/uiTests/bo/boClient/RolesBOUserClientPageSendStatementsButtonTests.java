41bfb29b42099102f0b05925296405fc283802d9
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-05-12 18:09:45 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageSendStatementsButtonTests.java
+++ src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageSendStatementsButtonTests.java
@@ -1,15 +1,24 @@
 package tests.uiTests.bo.boClient;
 
+import appmanager.EmailVerificationHelper;
 import base.UITestBase;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.List;
+
+import static appmanager.EmailIMAPHelper.getEmailBodyText;
+import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class RolesBOUserClientPageSendStatementsButtonTests extends UITestBase {
     String phone = "380980316499";
     String clientId = "29818";
 
     @Test
-    public void testRolesBOUserClientPageSendStatementsButtonAllStatementsAndDefaultEmail() throws InterruptedException {
+    public void testRolesBOUserClientPageSendStatementsButtonAllStatementsAndDefaultEmail() throws InterruptedException, IOException, MessagingException {
         gotoBOSiteAndLoginWithBOUserRole(app.BOuserLogin, app.BOuserPass);
         gotoSearchPage();
         search("id", clientId, phone);
@@ -21,7 +30,19 @@ public class RolesBOUserClientPageSendStatementsButtonTests extends UITestBase {
         click(By.cssSelector("div[role='checkbox']"));
         click(By.cssSelector("span.p-multiselect-close-icon"));
         click(By.cssSelector("p-button[label='Send']"));
-
         waitFor(By.xpath("//div[contains(text(), 'Statements were sent successfully')]"));
+
+        List<String> senderAndSubject = EmailVerificationHelper.getEmailSenderAndSubject("testdipocket3@gmail.com", "pasword12!");
+        String actualSubject = senderAndSubject.get(1);
+
+        List<String> actualAttachedFileNames = EmailVerificationHelper.getFileNameFromEmail("pop.gmail.com", "testdipocket3@gmail.com", "pasword12!");
+
+
+        String emailText =  EmailVerificationHelper.getTextFromEmail("pop.gmail.com", "testdipocket3@gmail.com", "pasword12!");
+        String actualBody = getEmailBodyText(emailText, 28, 191);
+
+        assertEquals(actualSubject, "Виписка по рахунку "+app.site+"");
+        assertEquals(actualBody, "Вітаємо, "+app.emailsVerificationsFirstName+"! В додатку знаходиться замовлена Вами банківська виписка по рахунку "+app.site+". Дякуємо за користування додатком "+app.site+". З повагою, Юридичний відділ");
+        assertTrue(actualAttachedFileNames.size()>3);
     }
 }
