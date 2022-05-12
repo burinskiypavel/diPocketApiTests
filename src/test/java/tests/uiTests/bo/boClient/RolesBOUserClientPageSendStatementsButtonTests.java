ccb30749e7f0e8ee1c50afb30d319ddb4c3abd53
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-05-12 17:34:38 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageSendStatementsButtonTests.java
+++ src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageSendStatementsButtonTests.java
@@ -7,21 +7,27 @@ import org.testng.annotations.Test;
 import java.sql.SQLException;
 
 public class RolesBOUserClientPageSendStatementsButtonTests extends UITestBase {
-    String phone = "380634413376";
-    String clientId = "33217";
+    String phone = "380980316499";
+    String clientId = "29818";
     String forgotPhone = "380685448615";
 
 
     @Test
-    public void testRolesBOUserClientPageSendStatementsButton() throws InterruptedException, SQLException, ClassNotFoundException {
-        //String clientId = app.getDbHelper().getClientIdFromDB(HelperBase.prop.getProperty("mobile.registration.email"), Site.DIPOCKET.toString());
+    public void testRolesBOUserClientPageSendStatementsButtonAllStatementsAndDefaultEmail() throws InterruptedException {
         gotoBOSiteAndLoginWithBOUserRole(app.BOuserLogin, app.BOuserPass);
         gotoSearchPage();
-        search("id", clientId, forgotPhone);
-        goToClientPage(forgotPhone);
-        click(By.xpath("//app-button[@ng-reflect-label='Change credentials']"));
-        changeCredentialsChagePhoneNumber("38098316499");
+        search("id", clientId, phone);
+        goToClientPage(phone);
+        click(By.xpath("//app-button[@ng-reflect-label='Send statements']"));
 
-        waitFor(By.xpath("//div[contains(text(), 'Credentials was changed successfully')]"));
+        click(By.id("formly_3_multi-select_statementRequestList_0"));
+
+        click(By.cssSelector("div[role='checkbox']"));
+
+        click(By.cssSelector("span.p-multiselect-close-icon"));
+
+        click(By.cssSelector("p-button[label='Send']"));
+
+        waitFor(By.xpath("//div[contains(text(), 'Statements were sent successfully')]"));
     }
 }
