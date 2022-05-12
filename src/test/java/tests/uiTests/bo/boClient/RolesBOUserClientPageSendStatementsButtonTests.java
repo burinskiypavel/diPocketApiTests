ed0884773ab53c780fa1a626a3b408a5385b621c
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-05-12 17:38:48 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageSendStatementsButtonTests.java
+++ src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageSendStatementsButtonTests.java
@@ -4,13 +4,9 @@ import base.UITestBase;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-import java.sql.SQLException;
-
 public class RolesBOUserClientPageSendStatementsButtonTests extends UITestBase {
     String phone = "380980316499";
     String clientId = "29818";
-    String forgotPhone = "380685448615";
-
 
     @Test
     public void testRolesBOUserClientPageSendStatementsButtonAllStatementsAndDefaultEmail() throws InterruptedException {
@@ -20,12 +16,10 @@ public class RolesBOUserClientPageSendStatementsButtonTests extends UITestBase {
         goToClientPage(phone);
         click(By.xpath("//app-button[@ng-reflect-label='Send statements']"));
 
+        waitFor(By.id("formly_3_multi-select_statementRequestList_0"));
         click(By.id("formly_3_multi-select_statementRequestList_0"));
-
         click(By.cssSelector("div[role='checkbox']"));
-
         click(By.cssSelector("span.p-multiselect-close-icon"));
-
         click(By.cssSelector("p-button[label='Send']"));
 
         waitFor(By.xpath("//div[contains(text(), 'Statements were sent successfully')]"));
