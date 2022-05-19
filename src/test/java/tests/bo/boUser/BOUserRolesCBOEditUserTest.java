c9b641d8a33e3fb16e494c544cefdf9286023c50
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-05-19 13:59:31 +0300

    refactor

--- src/test/java/tests/bo/boUser/BOUserRolesCBOEditUserTest.java
+++ src/test/java/tests/bo/boUser/BOUserRolesCBOEditUserTest.java
@@ -1,17 +1,16 @@
 package tests.bo.boUser;
 
 import base.TestBase;
+import com.cs.dipocketback.base.data.Site;
 import org.testng.annotations.Test;
 
-import java.sql.SQLException;
-
-import static io.restassured.RestAssured.basePath;
-import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.*;
 
 public class BOUserRolesCBOEditUserTest extends TestBase {
     String cookie = null;
-    String phone = "380685448615";
-    String username = "PAVELB2";
+    String phone = "380685448614";
+    String username = "PAVELB";
     String email = "burinskiypavel@gmail.com";
 
     @Test(priority = 1)
@@ -19,5 +18,50 @@ public class BOUserRolesCBOEditUserTest extends TestBase {
         baseURI = app.BOURL;
         basePath = "BOServices";
         cookie = app.getBoRequestsHelper().boServices_v1_user_authentication(app.CBOuserLogin, app.CBOuserPass, "VIKTORIA");
+    }
+
+    @Test(priority = 2)
+    public void test_BOServices_v1_user_roles(){
+        app.getBoRequestsHelper().boServices_v1_user_roles(cookie);
+    }
+
+    @Test(priority = 3)
+    public void test_BOServices_v1_user_all(){
+        given()
+                .log().uri().log().headers()
+                .cookie(cookie)
+                .contentType("application/json")
+                .when()
+                .get( "/v1/user/all")
+                .then().log().all()
+                .statusCode(200)
+                .body("username", hasItem("A.VAIVARS"),
+                        "phone", hasItem("37125680800"),
+                        "email", hasItem("arnis.vaivars@twino.eu"));
+    }
+
+    @Test(priority = 4)
+    public void test_BOServices_v1_user_PAVELB(){
+        given()
+                .log().uri().log().headers()
+                .cookie(cookie)
+                .contentType("application/json")
+                .when()
+                .get( "/v1/user/PAVELB")
+                .then().log().all()
+                .statusCode(200)
+                .body("username", equalTo(username),
+                        "phone", equalTo(phone),
+                        "email", equalTo(email));
+    }
+
+    @Test(priority = 5)
+    public void test_BOServices_v1_user_verifyPhone(){
+        app.getBoRequestsHelper().boServices_v1_user_verifyPhone(cookie, phone, true);
+    }
+
+    @Test(priority = 6)
+    public void test_BOServices_v1_user_corpClients_site_SODEXO(){
+        app.getBoRequestsHelper().boServices_v1_user_corpClients_site_SODEXO(cookie, "Sodexo New LE", Site.SODEXO.toString());
     }
 }
