b5c45cc56ef543fd337e378098cb2827f6642529
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-05-23 12:32:31 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boSearch/RolesCBOBOSearchByClientClientIDTest.java
+++ src/test/java/tests/uiTests/bo/boSearch/RolesCBOBOSearchByClientClientIDTest.java
@@ -10,8 +10,28 @@ public class RolesCBOBOSearchByClientClientIDTest extends UITestBase {
     SoftAssert softAssert = new SoftAssert();
 
     @Test
-    public void testRolesCBOBOSearchByClientClientID() throws InterruptedException {
+    public void testRolesBOSearchByClientClientID() throws InterruptedException {
         gotoBOSiteAndLoginWithBOUserRole(app.BOuserLogin, app.BOuserPass);
+        gotoSearchPage();
+
+        softAssert.assertTrue(isTabPresent(By.xpath("//a[@role='tab'] //span[contains(text(), 'Client')]")), "Is tab present");
+        softAssert.assertTrue(isTabPresent(By.xpath("//a[@role='tab'] //span[contains(text(), 'Card')]")), "Is tab present");
+
+        softAssert.assertTrue(isTabActiveAndSelected(By.xpath("//a[@id='p-tabpanel-0-label'][@aria-selected='true']")), "Is tab Client active and selected");
+
+        softAssert.assertTrue(areElementsPresent(new String[]{"//app-input-number[@ng-reflect-name='id']", "//app-input[@ng-reflect-name='email']", "//app-input[@ng-reflect-name='mainPhone']",
+                "//app-input[@ng-reflect-name='firstName']", "//app-input[@ng-reflect-name='lastName']", "//app-input[@ng-reflect-name='mailingAddress']",
+                "//app-calendar[@ng-reflect-name='birthDate']"}), "Are following options(fields) available");
+
+        search("id", "33217");
+
+        softAssert.assertTrue(areElementsPresent(new String[]{"//td[@ng-reflect-text='380634413376']", "//td[@ng-reflect-text='"+ Site.DIPOCKET.toString()+"']"}), "Are search results displays");
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void testRolesCBOSearchByClientClientID() throws InterruptedException {
+        gotoBOSiteAndLoginWithCBOUserRole(app.CBOuserLogin, app.CBOuserPass);
         gotoSearchPage();
 
         softAssert.assertTrue(isTabPresent(By.xpath("//a[@role='tab'] //span[contains(text(), 'Client')]")), "Is tab present");
