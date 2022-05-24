40001c2a0503d22b1ce7cdecbd27b4fc710fb209
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-05-24 15:44:22 +0300

    refactor

---
+++ src/test/java/tests/bo/boSearch/RolesCBOSearchByClientDateOfBirthTests.java
@@ -0,0 +1,32 @@
+package tests.bo.boSearch;
+
+import base.TestBase;
+import com.cs.dipocketback.base.data.Site;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.basePath;
+import static io.restassured.RestAssured.baseURI;
+
+public class RolesCBOSearchByClientDateOfBirthTests extends TestBase {
+    String cookie = null;
+    String username = "VIKTORIA";
+    String phone = "380634413376";
+    String email = "vikarezznik60@gmail.com";
+
+    @Test(priority = 1)
+    public void test_BOServices_v1_user_authentication() {
+        baseURI = app.BOURL;
+        basePath = "BOServices";
+        cookie = app.getBoRequestsHelper().boServices_v1_user_authentication(app.CBOuserLogin, app.CBOuserPass, username);
+    }
+
+    @Test(priority = 2)
+    public void test_BOServices_v1_user_authenticated(){
+        app.getBoRequestsHelper().boServices_v1_user_authenticated(cookie, username, phone, email);
+    }
+
+    @Test(priority = 3)
+    public void test_BOServices_v1_client_search(){
+        app.getBoRequestsHelper().boServices_v1_client_search(cookie, "birthDate",  "1.1.1998", "Jaleel", "Haley", "380667904157", "Shirley32@hotmail.com", Site.GETSBYCARD.toString());
+    }
+}
