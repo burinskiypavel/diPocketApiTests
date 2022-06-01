12cabc80a7b9e7c50bfef50214a3db945a9547b3
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-06-01 14:37:51 +0300

    refactor

---
+++ src/test/java/tests/bo/boSearch/RolesBOSearchByClientEmailTests.java
@@ -0,0 +1,32 @@
+package tests.bo.boSearch;
+
+import base.TestBase;
+import com.cs.dipocketback.base.data.Site;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.basePath;
+import static io.restassured.RestAssured.baseURI;
+
+public class RolesBOSearchByClientEmailTests extends TestBase {
+    String cookie = null;
+    String username = "EVGENYA";
+    String phone = "447459005206";
+    String email = "vikarez20@gmail.com";
+
+    @Test(priority = 1)
+    public void test_BOServices_v1_user_authentication() {
+        baseURI = app.BOURL;
+        basePath = "BOServices";
+        cookie = app.getBoRequestsHelper().boServices_v1_user_authentication(app.BOuserLogin, app.BOuserPass, username);
+    }
+
+    @Test(priority = 2)
+    public void test_BOServices_v1_client_states(){
+        app.getBoRequestsHelper().boServices_v1_client_states(cookie);
+    }
+
+    @Test(priority = 3)
+    public void test_BOServices_v1_client_search(){
+        app.getBoRequestsHelper().boServices_v1_client_search(cookie, "email",  email, "Luiza", "Qwerty", phone, email, Site.DIPOCKET.toString());
+    }
+}
