0eae08b53328acc65ec936ac1d20d46f3e9ed072
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-06-13 12:12:39 +0300

    refactor

--- src/test/java/tests/bo/boUser/BOUserRolesCBOResetPasswordTests.java
+++ src/test/java/tests/bo/boUser/BOUserRolesCBOResetPasswordTests.java
@@ -14,7 +14,7 @@ public class BOUserRolesCBOResetPasswordTests extends TestBase {
     String email = "sdafsdjflstalnd@gmail.com";
 
     @Test(priority = 1)
-    public void test_BOServices_v1_user_authentication() {
+    public void test_BOServices_v1_auth_authentication() {
         baseURI = app.BOURL;
         basePath = "BOServices";
         cookie = app.getBoRequestsHelper().boServices_v1_auth_authentication(app.CBOuserLogin, app.CBOuserPass, "VIKTORIA");
