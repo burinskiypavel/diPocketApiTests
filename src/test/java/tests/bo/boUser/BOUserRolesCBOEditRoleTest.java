3dfa67eabb4626e3fffec9e5e516ae09781319c4
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-06-13 12:39:38 +0300

    refactor

--- src/test/java/tests/bo/boUser/BOUserRolesCBOEditRoleTest.java
+++ src/test/java/tests/bo/boUser/BOUserRolesCBOEditRoleTest.java
@@ -5,11 +5,7 @@ import io.restassured.response.Response;
 import model.bo.User_roles;
 import org.testng.annotations.Test;
 
-import java.sql.SQLException;
-
-import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.equalTo;
+import static io.restassured.RestAssured.*;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
@@ -18,28 +14,19 @@ public class BOUserRolesCBOEditRoleTest extends TestBase {
     String newRoleName = app.generateRandomString(5);
 
     @Test(priority = 1)
-    public void test_BOServices_v1_user_authentication() {
+    public void test_BOServices_v1_auth_authentication() {
         baseURI = app.BOURL;
-        Response response = given()
-                .log().uri().log().headers()
-                .auth().preemptive().basic("Viktoria", "kWmaB0s")
-                .contentType("application/json")
-                .when()
-                .post( "/BOServices/v1/user/authentication");
-        response.then().log().all()
-                .statusCode(200)
-                .body("username", equalTo("VIKTORIA"));
-        cookie = response.getHeader("Set-Cookie");
+        basePath = "BOServices";
+        cookie = app.getBoRequestsHelper().boServices_v1_auth_authentication(app.CBOuserLogin, app.CBOuserPass, "VIKTORIA");
     }
 
     @Test(priority = 2)
     public void test_BOServices_v1_user_roles(){
         Response res =given()
-                .log().uri().log().headers()
+                .spec(app.requestSpecBO)
                 .cookie(cookie)
-                .contentType("application/json")
                 .when()
-                .get( "/BOServices/v1/user/roles");
+                .get( "/v1/user/roles");
         res.then().log().all().statusCode(200);
         User_roles[] user_roles = res.as(User_roles[].class);
 
@@ -49,13 +36,12 @@ public class BOUserRolesCBOEditRoleTest extends TestBase {
     @Test(priority = 3)
     public void test_BOServices_v1_user_role_update(){
         given()
-                .log().uri().log().headers()
+                .spec(app.requestSpecBO)
                 .cookie(cookie)
-                .contentType("application/json")
                 .queryParam("roleId", "1")
                 .queryParam("roleName",newRoleName)
                 .when()
-                .post( "/BOServices/v1/user/role/update")
+                .post( "/v1/user/role/update")
                 .then().log().all()
                 .statusCode(200);
     }
@@ -63,11 +49,10 @@ public class BOUserRolesCBOEditRoleTest extends TestBase {
     @Test(priority = 4)
     public void test_BOServices_v1_user_roles_(){
         Response res =given()
-                .log().uri().log().headers()
+                .spec(app.requestSpecBO)
                 .cookie(cookie)
-                .contentType("application/json")
                 .when()
-                .get( "/BOServices/v1/user/roles");
+                .get( "/v1/user/roles");
         res.then().log().all().statusCode(200);
         User_roles[] user_roles = res.as(User_roles[].class);
 
