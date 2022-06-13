13d4b5f4df57caa438e088026c292f4e30b7566a
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-06-13 13:21:05 +0300

    refactor

--- src/test/java/tests/bo/boUser/BOUserRolesCBOAddNewRoleTest.java
+++ src/test/java/tests/bo/boUser/BOUserRolesCBOAddNewRoleTest.java
@@ -15,16 +15,15 @@ public class BOUserRolesCBOAddNewRoleTest extends TestBase {
     String cookie = null;
 
     @Test(priority = 1)
-    public void test_BOServices_v1_user_authentication() throws SQLException, ClassNotFoundException {
+    public void test_BOServices_v1_auth_authentication() throws SQLException, ClassNotFoundException {
         baseURI = app.BOURL;
         basePath = "BOServices";
         cookie = app.getBoRequestsHelper().boServices_v1_auth_authentication(app.CBOuserLogin, app.CBOuserPass, "VIKTORIA");
 
         if(app.getDbHelper().isRoleExistInDB("a_roleID")){
             given()
-                    .log().uri().log().headers()
+                    .spec(app.requestSpecBO)
                     .cookie(cookie)
-                    .contentType("application/json")
                     .queryParam("roleId", "a_roleID")
                     .when()
                     .post( "/v1/user/role/delete")
@@ -36,9 +35,8 @@ public class BOUserRolesCBOAddNewRoleTest extends TestBase {
     @Test(priority = 2)
     public void test_BOServices_v1_user_checkAuthentication(){
         given()
-                .log().uri().log().headers()
                 .cookie(cookie)
-                .contentType("application/json")
+                .spec(app.requestSpecBO)
                 .when()
                 .get( "/v1/user/checkAuthentication")
                 .then().log().all()
@@ -49,9 +47,8 @@ public class BOUserRolesCBOAddNewRoleTest extends TestBase {
     @Test(priority = 3)
     public void test_BOServices_v1_user_authenticated(){
         given()
-                .log().uri().log().headers()
                 .cookie(cookie)
-                .contentType("application/json")
+                .spec(app.requestSpecBO)
                 .when()
                 .get( "/v1/user/authenticated")
                 .then().log().all()
@@ -66,9 +63,8 @@ public class BOUserRolesCBOAddNewRoleTest extends TestBase {
     @Test(priority = 4)
     public void test_BOServices_v1_user_role_create(){
         given()
-                .log().uri().log().headers()
+                .spec(app.requestSpecBO)
                 .cookie(cookie)
-                .contentType("application/json")
                 .queryParam("roleId", "a_roleID")
                 .queryParam("roleName","a_roleName")
                 .when()
@@ -80,9 +76,8 @@ public class BOUserRolesCBOAddNewRoleTest extends TestBase {
     @Test(priority = 5)
     public void test_BOServices_v1_user_roles(){
         Response res =given()
-                .log().uri().log().headers()
+                .spec(app.requestSpecBO)
                 .cookie(cookie)
-                .contentType("application/json")
                 .when()
                 .get( "/v1/user/roles");
         res.then().log().all().statusCode(200);
@@ -94,9 +89,8 @@ public class BOUserRolesCBOAddNewRoleTest extends TestBase {
     @Test(priority = 6)
     public void test_BOServices_v1_user_role_saveRights(){
         given()
-                .log().uri().log().headers()
+                .spec(app.requestSpecBO)
                 .cookie(cookie)
-                .contentType("application/json")
                 .body("{\n" +
                         "  \"roleId\" : \"a_roleID\",\n" +
                         "  \"checkCodes\" : [ \"BO\" ]\n" +
@@ -110,9 +104,8 @@ public class BOUserRolesCBOAddNewRoleTest extends TestBase {
     @Test(priority = 7)
     public void test_BOServices_v1_user_role_delete(){
         given()
-                .log().uri().log().headers()
+                .spec(app.requestSpecBO)
                 .cookie(cookie)
-                .contentType("application/json")
                 .queryParam("roleId", "a_roleID")
                 .when()
                 .post( "/v1/user/role/delete")
