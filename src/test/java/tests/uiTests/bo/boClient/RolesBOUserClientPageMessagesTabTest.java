f7aabe377f6a7008e34f00ba3f970132dc431ec9
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-06-15 15:56:42 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageMessagesTabTest.java
+++ src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageMessagesTabTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import static org.testng.Assert.assertTrue;
+
 public class RolesBOUserClientPageMessagesTabTest extends UITestBase {
     SoftAssert softAssert = new SoftAssert();
     String phone = "380634413376";
@@ -19,15 +21,22 @@ public class RolesBOUserClientPageMessagesTabTest extends UITestBase {
 
         softAssert.assertTrue(areElementsPresent(new String[]{"//table //th[contains(text(), 'Chanel')]",
                 "//table //th[contains(text(), 'Created')]", "//table //th[contains(text(), 'Send')]",
-        "//table //th[contains(text(), 'Messages')]", "//table //th[contains(text(), 'Error message')]"}));
+        "//table //th[contains(text(), 'Messages')]", "//table //th[contains(text(), 'Error message')]"}), "Incorrect headers");
 
         waitFor(By.cssSelector("td[ng-reflect-text='S']"));
-        click(By.cssSelector("p-dropdown[placeholder='All']"));
-        waitForElementToBeClickable(By.cssSelector("li[aria-label='S']"));
-        click(By.cssSelector("li[aria-label='S']"));
+        setDropDownClientPageFilter("channel", "S");
+        softAssert.assertTrue(areElementsPresentAfterSorting(By.cssSelector("td[ng-reflect-text='S']")), "Incorrect channel filter");
+        softAssert.assertFalse(areElementsPresentAfterSorting(By.cssSelector("td[ng-reflect-text='P']")), "Incorrect channel filter");
+        clearFilter(By.cssSelector("i.p-dropdown-clear-icon"));
 
-        softAssert.assertTrue(areElementsPresentAfterSorting(By.cssSelector("td[ng-reflect-text='S']")));
-        softAssert.assertFalse(areElementsPresentAfterSorting(By.cssSelector("td[ng-reflect-text='P']")));
+        setClientPageFilter("message", "Password change code: 839429 (#3)");
+        softAssert.assertTrue(areElementsPresentAfterSorting(By.xpath("//td[contains(text(), 'Password change code: 839429 (#3)')]")), "Incorrect message filter");
+        deleteTextFromTextarea(By.cssSelector("p-columnfilter[field='message'] input[type='text']"));
+
+        setClientPageFilter("errMsg", "java");
+        softAssert.assertTrue(areElementsPresentAfterSorting(By.xpath("//td[contains(text(), 'java.sql.SQLException: ORA-12899')]")), "Incorrect error message filter");
+        deleteTextFromTextarea(By.cssSelector("p-columnfilter[field='errMsg'] input[type='text']"));
+
         softAssert.assertAll();
     }
 }
