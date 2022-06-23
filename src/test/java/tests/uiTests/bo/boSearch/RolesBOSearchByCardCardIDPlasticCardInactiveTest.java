cf3b6e54378baca593734a5bce4b35ab0028a2a1
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-06-23 15:45:33 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boSearch/RolesBOSearchByCardCardIDPlasticCardInactiveTest.java
+++ src/test/java/tests/uiTests/bo/boSearch/RolesBOSearchByCardCardIDPlasticCardInactiveTest.java
@@ -24,4 +24,22 @@ public class RolesBOSearchByCardCardIDPlasticCardInactiveTest extends UITestBase
         "//app-button[@ng-reflect-label='Show client info']", "//app-button[@ng-reflect-label='Operations']"}), "Incorrect buttons");
         softAssert.assertAll();
     }
+
+    @Test
+    public void testRolesBOSearchByCardCardIDPlasticCardInactive_() throws InterruptedException {
+        gotoBOSiteAndLoginWithBOUserRole(app.BOuserLogin, app.BOuserPass);
+        gotoSearchPage();
+        gotoCardSearchTab();
+        searchByCard("id", cardId);
+        gotoCardDetailsPage(cardId);
+        waitFor(By.xpath("//a[@role='tab'] //span[contains(text(), 'Transactions')]"));
+
+        String maskedPan = getText(By.xpath("//span[@class='maskedPan'] //span[1]"));
+
+        //softAssert.assertTrue(areElementsPresent(new String[]{"//span[@class='maskedPan']"}), "Incorrect tabs");
+
+        //softAssert.assertTrue(areButtonsPresent(new String[]{"//app-button[@ng-reflect-label='Back to search']",
+        //        "//app-button[@ng-reflect-label='Show client info']", "//app-button[@ng-reflect-label='Operations']"}), "Incorrect buttons");
+        softAssert.assertAll();
+    }
 }
