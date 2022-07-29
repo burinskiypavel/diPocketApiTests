ea465a50c33cd472fa10bd02c7ea07410a1ee074
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-07-29 22:03:21 +0300

    Refactor

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -22,34 +22,4 @@ public class TestBase {
         String className = this.getClass().getCanonicalName();
         System.out.println("Class Name: " + className + "  Current thred id: " + Thread.currentThread().getId());
     }
-
-    //    public void deleteClientFromDB2() throws SQLException, ClassNotFoundException {
-//        //Connection con = DriverManager.getConnection(dbUrl,username,password);
-//        String dbUrl = "jdbc:oracle:thin:@dipocket1.intranet/dip";
-//        //Database Username
-//        String username = "Dipocket";
-//        String password = "c67";
-//        //Querry to Execute
-//        String query = "BEGIN\n" +
-//                "PKI_CLIENT.CLEARCLIENTBYPHONE(p_Site=>'DIPOCKET',p_Phone=>'380685448615');\n" +
-//                "END;";
-//
-//        String query2 = "commit";
-//
-//        Class.forName("oracle.jdbc.driver.OracleDriver");
-//
-//        //Create Connection to DB
-//        Connection con = DriverManager.getConnection(dbUrl, username, password);
-//
-//        //Create Statement Object
-//        Statement stmt = con.createStatement();
-//
-//        // Execute the SQL Query. Store results in ResultSet
-//        ResultSet rs= stmt.executeQuery(query);
-//        ResultSet rs2= stmt.executeQuery(query2);
-//
-//        // closing DB Connection
-//        con.close();
-//    }
-
 }
