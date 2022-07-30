59589a344f3a9bdc09518578a915a16b9e112b73
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-07-30 22:55:52 +0300

    Refactor

--- src/test/java/appmanager/ApplicationManager.java
+++ src/test/java/appmanager/ApplicationManager.java
@@ -263,51 +263,6 @@ public class ApplicationManager {
         driver.quit();
     }
 
-//    public void unirest() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException, UnirestException {
-//        //unirest ssl
-//        SSLContext sslcontext = SSLContexts.custom()
-//                .loadTrustMaterial(null, new TrustSelfSignedStrategy())
-//                .build();
-//
-//        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext,SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-//        CloseableHttpClient httpclient = HttpClients.custom()
-//                .setSSLSocketFactory(sslsf)
-//                .build();
-//        Unirest.setHttpClient(httpclient);
-//
-//        HttpResponse<JsonNode> postResponse = Unirest.put("https://dipocket3.intranet:8900/ClientServices/v1/userRegistration/registrationSavePoint2?value=com.cs.dipocketback.pojo.registration.RegSavepointData@7ae86ada")
-//                .header("site", "DIPOCKET")
-//                .header("deviceuuid", "eC10LFCnS1mDsuNoQaa-KH")
-//                .header("Content-Type", "application/json")
-//                .body("{\n" +
-//                        "  \"deviceUUID\" : \"eC10LFCnS1mDsuNoQaa-KH\",\n" +
-//                        "  \"langId\" : 4,\n" +
-//                        "  \"mainPhone\" : \"380685448615\",\n" +
-//                        "  \"stepNo\" : 1,\n" +
-//                        "  \"registeredAddrAsmail\" : true,\n" +
-//                        "  \"address\" : {\n" +
-//                        "    \"typeId\" : 0\n" +
-//                        "  },\n" +
-//                        "  \"regAddress\" : {\n" +
-//                        "    \"typeId\" : 3\n" +
-//                        "  },\n" +
-//                        "  \"attachedCardsList\" : [ ],\n" +
-//                        "  \"smsCode\" : \"336052\",\n" +
-//                        "  \"isSkipped\" : false,\n" +
-//                        "  \"address1\" : {\n" +
-//                        "    \"typeId\" : 0\n" +
-//                        "  },\n" +
-//                        "  \"attachedCardIds\" : [ ]\n" +
-//                        "}")
-//                .asJson();
-//        //.asString();
-//        //.getBody();
-//        System.out.println(postResponse.getBody());
-//        //bearer = postResponse.getBody().getObject().getString("bearer");
-//        //System.out.println("bearer " + postResponse.getBody().getObject().getString("bearer"));
-//        Assert.assertNotNull(postResponse.getBody());
-//    }
-
     public String generateRandomString(int amount){
         String randomString =  RandomStringUtils.random(amount, true, true);
         return randomString;
@@ -352,11 +307,4 @@ public class ApplicationManager {
     public UIBOHelper getUiboHelper() { return uiboHelper; }
 
     public UIUpAndGoHelper getUiUpAndGoHelper() { return uiUpAndGoHelper; }
-
-//    public MailHelper mail(){
-//        if(mailHelper == null){
-//            mailHelper = new MailHelper(this);
-//        }
-//        return mailHelper;
-//    }
 }
