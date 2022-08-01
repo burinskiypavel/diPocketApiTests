285fe7af301d2bc1cc841d637f6cf8b97abd2fe4
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-08-01 17:15:17 +0300

    Refactor

--- src/test/java/tests/uiTests/bo/boOperations/AddAndDeleteRowInTariffPlanTests.java
+++ src/test/java/tests/uiTests/bo/boOperations/AddAndDeleteRowInTariffPlanTests.java
@@ -4,6 +4,8 @@ import base.UITestBase;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertTrue;
+
 public class AddAndDeleteRowInTariffPlanTests extends UITestBase {
 
     @Test
@@ -29,6 +31,24 @@ public class AddAndDeleteRowInTariffPlanTests extends UITestBase {
         app.getUiboHelper().closePopUp(By.cssSelector("div.p-dialog-header-icons"));
 
         app.getUiboHelper().waitForInvisibilityOfElement(By.xpath("//div[@role='dialog']"));
+    }
 
+    @Test
+    public void testAddRowInTariffPlanWithoutFillingInTheRequiredFields() throws InterruptedException {
+        app.getUiboHelper().gotoBOSiteAndLoginWithCBOUserRole(app.CBOuserLogin, app.CBOuserPass);
+        app.getUiboHelper().gotoOperations();
+        app.getUiboHelper().gotoFeeTariffPlanTab();
+        app.getUiboHelper().click(By.xpath("//p-button[@ng-reflect-label='+ Add row']"));
+        app.getUiboHelper().click(By.xpath("//p-button[@ng-reflect-label='Add']"));
+
+        assertTrue(app.getUiboHelper().areElementsPresent(new String[]{
+                "//small[contains(text(), 'Rule is required')]",
+                "//small[contains(text(), 'Fee percent is required')]",
+                "//small[contains(text(), 'Currency is required')]",
+                "//small[contains(text(), 'Fee currency is required')]",
+                "//small[contains(text(), 'Min fee amount is required')]",
+                "//small[contains(text(), 'Max fee amount is required')]",
+                "//small[contains(text(), 'Currency is required')]",
+                "//small[contains(text(), 'Flat fee amount is required')]",}), "Account limits has incorrect headers ");
     }
 }
