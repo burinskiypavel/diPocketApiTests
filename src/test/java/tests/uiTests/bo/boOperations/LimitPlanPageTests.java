e31191e4a5bfaccb0cc41f512fe9d8cb70752d8e
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-08-08 15:44:11 +0300

    Refactor

--- src/test/java/tests/uiTests/bo/boOperations/LimitPlanPageTests.java
+++ src/test/java/tests/uiTests/bo/boOperations/LimitPlanPageTests.java
@@ -2,10 +2,10 @@ package tests.uiTests.bo.boOperations;
 
 import base.UITestBase;
 import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertTrue;
+import org.testng.asserts.SoftAssert;
 
 public class LimitPlanPageTests extends UITestBase {
+    SoftAssert softAssert = new SoftAssert();
 
     @Test
     public void testOpeningALimitPlanPage() throws InterruptedException {
@@ -13,13 +13,14 @@ public class LimitPlanPageTests extends UITestBase {
         app.getUiboHelper().gotoOperations();
         app.getUiboHelper().gotoLimitPlanTab();
 
-        assertTrue(app.getUiboHelper().areElementsPresent(new String[]{"//h5[contains(text(), 'Limit plan')]",
+        softAssert.assertTrue(app.getUiboHelper().areElementsPresent(new String[]{"//h5[contains(text(), 'Limit plan')]",
                 "//h5[contains(text(), 'Limit level')]", "//h5[contains(text(), 'Base currency')]"}), "Text Verifications");
 
-        assertTrue(app.getUiboHelper().areElementsPresent(new String[]{"//th[contains(text(), 'Row #')]",
+        softAssert.assertTrue(app.getUiboHelper().areElementsPresent(new String[]{"//th[contains(text(), 'Row #')]",
                 "//th[contains(text(), 'Group')]", "//th[contains(text(), 'Type')]", "//th[contains(text(), 'Amount')]"}), "Columns verifications");
 
-        assertTrue(app.getUiboHelper().areButtonsPresent(new String[]{"//p-button[@ng-reflect-label='Add limit plan']", "//p-button[@ng-reflect-label='Duplicate limit plan']",
+        softAssert.assertTrue(app.getUiboHelper().areButtonsPresent(new String[]{"//p-button[@ng-reflect-label='Add limit plan']", "//p-button[@ng-reflect-label='Duplicate limit plan']",
                 "//p-button[@ng-reflect-label='Rename limit plan']", "//p-button[@ng-reflect-label='Delete limit plan']", "//p-button[@ng-reflect-label='+ Add row']"}), "Buttons verifications");
+        softAssert.assertAll();
     }
 }
