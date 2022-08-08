297df60f2d536f48c9c3777f67e33189a99f0a35
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-08-08 16:11:31 +0300

    Refactor

--- src/test/java/tests/uiTests/bo/boOperations/LimitPlanPageTests.java
+++ src/test/java/tests/uiTests/bo/boOperations/LimitPlanPageTests.java
@@ -1,8 +1,13 @@
 package tests.uiTests.bo.boOperations;
 
 import base.UITestBase;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
 
 public class LimitPlanPageTests extends UITestBase {
     SoftAssert softAssert = new SoftAssert();
@@ -22,5 +27,22 @@ public class LimitPlanPageTests extends UITestBase {
         softAssert.assertTrue(app.getUiboHelper().areButtonsPresent(new String[]{"//p-button[@ng-reflect-label='Add limit plan']", "//p-button[@ng-reflect-label='Duplicate limit plan']",
                 "//p-button[@ng-reflect-label='Rename limit plan']", "//p-button[@ng-reflect-label='Delete limit plan']", "//p-button[@ng-reflect-label='+ Add row']"}), "Buttons verifications");
         softAssert.assertAll();
+    }
+
+
+    @Test
+    public void testOpeningALimitPlan() throws InterruptedException {
+        app.getUiboHelper().gotoBOSiteAndLoginWithCBOUserRole(app.CBOuserLogin, app.CBOuserPass);
+        app.getUiboHelper().gotoOperations();
+        app.getUiboHelper().gotoLimitPlanTab();
+
+        app.getUiboHelper().selectFromDropDown(By.xpath("//div[@class='dropdowns'] //p-dropdown[@optionlabel='name']"), "Getsby");
+        app.getUiboHelper().selectFromDropDown(By.xpath("//div[@class='dropdowns'] //p-dropdown[@optionlabel='label']"), "FDD");
+        app.getUiboHelper().selectFromDropDown(By.xpath("//div[@class='dropdowns'] //p-dropdown[@optionlabel='code']"), "EUR");
+        Thread.sleep(1000);
+        List<String> actualElementsText = app.getUiboHelper().getActualText2(By.xpath("//app-limit-plan-tab //table //tbody //tr"));
+        List<String> expectedElementsText = app.getUiboHelper().getDateFromFile2("files/bo/boOperations/openingALimitPlan.txt");
+
+        assertEquals(actualElementsText, expectedElementsText);
     }
 }
