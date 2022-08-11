d476ea80cff582eb96dfc16883320153f2b74738
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-08-11 17:27:01 +0300

    Refactor

--- src/test/java/tests/uiTests/bo/boOperations/OpeningABankTransfersPageTests.java
+++ src/test/java/tests/uiTests/bo/boOperations/OpeningABankTransfersPageTests.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
 public class OpeningABankTransfersPageTests extends UITestBase {
     SoftAssert softAssert = new SoftAssert();
 
@@ -25,5 +29,33 @@ public class OpeningABankTransfersPageTests extends UITestBase {
         softAssert.assertTrue(app.getUiboHelper().isButtonEnabled(By.xpath("//p-button[@label='Search']")), "Search button is not present");
         softAssert.assertTrue(app.getUiboHelper().isElementPresent(By.xpath("//p-button[@label='Check operation'][@ng-reflect-disabled='true']")), "Button Check operation is not disabled");
         softAssert.assertAll();
+    }
+
+    @Test
+    public void OpeningABankTransfersWithStateError() throws InterruptedException {
+        app.getUiboHelper().gotoBOSiteAndLoginWithCBOUserRole(app.CBOuserLogin, app.CBOuserPass);
+        app.getUiboHelper().gotoOperations();
+        app.getUiboHelper().gotoBankTransfersTab();
+
+        app.getUiboHelper().click(By.xpath("//app-search-by-period //p-dropdown[@optionlabel='value']"));
+
+        app.getUiboHelper().click(By.xpath("//p-dropdownitem[@ng-reflect-label='For period']"));
+
+        app.getUiboHelper().type(By.xpath("//p-calendar[@placeholder='From'] //input"), "03.03.2019");
+        app.getUiboHelper().type(By.xpath("//p-calendar[@placeholder='Till'] //input"), "11.08.2022");
+
+        app.getUiboHelper().click(By.xpath("//p-button[@label='Search']"));
+
+        app.getUiboHelper().click(By.xpath("//p-columnfilter[@field='stateName']"));
+        app.getUiboHelper().click(By.xpath("//p-dropdownitem[@ng-reflect-label='Error']"));
+
+
+        app.getUiboHelper().click(By.xpath("//td[@ng-reflect-text='Error']"));
+
+
+        List<String> actualElementsText = app.getUiboHelper().getActualText(By.xpath("//app-transactions-details //table //tbody //tr"));
+        List<String> expectedElementsText = app.getUiboHelper().getDateFromFile("files/bo/boOperations/OpeningABankTransfersWithStateError.txt");
+
+        assertEquals(actualElementsText, expectedElementsText);
     }
 }
