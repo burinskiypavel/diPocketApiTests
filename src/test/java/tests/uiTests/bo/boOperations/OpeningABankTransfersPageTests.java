f543947a0f2e119a429c3fa78ef23ae5a99a8990
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-08-11 15:52:32 +0300

    Refactor

--- src/test/java/tests/uiTests/bo/boOperations/OpeningABankTransfersPageTests.java
+++ src/test/java/tests/uiTests/bo/boOperations/OpeningABankTransfersPageTests.java
@@ -1,11 +1,12 @@
 package tests.uiTests.bo.boOperations;
 
 import base.UITestBase;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertTrue;
+import org.testng.asserts.SoftAssert;
 
 public class OpeningABankTransfersPageTests extends UITestBase {
+    SoftAssert softAssert = new SoftAssert();
 
     @Test
     public void testOpeningABankTransfersPage() throws InterruptedException {
@@ -13,8 +14,16 @@ public class OpeningABankTransfersPageTests extends UITestBase {
         app.getUiboHelper().gotoOperations();
         app.getUiboHelper().gotoBankTransfersTab();
 
-        assertTrue(app.getUiboHelper().areElementsPresent(new String[]{"//table //th[contains(text(), 'Tran id')]",
+        softAssert.assertTrue(app.getUiboHelper().areElementsPresent(new String[]{"//table //th[contains(text(), 'Tran id')]",
                 "//table //th[contains(text(), 'State')]", "//table //th[contains(text(), 'Payer id')]",
                 "//table //th[contains(text(), 'Payer')]", "//table //th[contains(text(), 'Older date')]"}), "Incorrect headers");
+
+        softAssert.assertTrue(app.getUiboHelper().areElementsPresent(new String[]{
+                "//p-columnfilter[@field='tranId']", "//p-columnfilter[@field='stateName']",
+                "//p-columnfilter[@field='payerId']", "//p-columnfilter[@field='payer']"}), "Incorrect filters");
+
+        softAssert.assertTrue(app.getUiboHelper().isButtonEnabled(By.xpath("//p-button[@label='Search']")), "Search button is not present");
+        softAssert.assertTrue(app.getUiboHelper().isElementPresent(By.xpath("//p-button[@label='Check operation'][@ng-reflect-disabled='true']")), "Button Check operation is not disabled");
+        softAssert.assertAll();
     }
 }
