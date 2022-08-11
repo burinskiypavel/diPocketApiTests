ba3f0184668e0f75f7584cac1206376822b14f55
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-08-11 14:53:55 +0300

    Refactor

--- src/test/java/appmanager/ui/UIBOHelper.java
+++ src/test/java/appmanager/ui/UIBOHelper.java
@@ -101,6 +101,10 @@ public class UIBOHelper extends UIHelperBase {
         click(By.xpath("//a[@role='tab'] //span[contains(text(), 'Limit plan')]"));
     }
 
+    public void gotoResetSMSCounterTab() {
+        click(By.xpath("//a[@role='tab'] //span[contains(text(), 'Reset SMS counter')]"));
+    }
+
     public void addRole(String roleID, String roleName) throws InterruptedException {
         click(By.cssSelector("p-button[label='+ Add']"));
         waitForSeveralItems(new String[]{"Role ID:", "Role name:", "Add Role"});
---
+++ src/test/java/tests/uiTests/bo/boOperations/SMSCounterResetTest.java
@@ -0,0 +1,24 @@
+package tests.uiTests.bo.boOperations;
+
+import base.UITestBase;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class SMSCounterResetTest extends UITestBase {
+    SoftAssert softAssert = new SoftAssert();
+
+    @Test
+    public void testTheUserChangedHisMindAboutAddLimitPlan() throws InterruptedException {
+        app.getUiboHelper().gotoBOSiteAndLoginWithCBOUserRole(app.CBOuserLogin, app.CBOuserPass);
+        app.getUiboHelper().gotoOperations();
+        app.getUiboHelper().gotoResetSMSCounterTab();
+        app.getUiboHelper().click(By.xpath("//button[@label='Reset']"));
+
+        app.getUiboHelper().waitFor(By.xpath("//*[contains(text(), 'SMS counter was successfully reset')]"));
+
+        softAssert.assertTrue(app.getUiboHelper().isElementPresent(By.xpath("//p-columnfilter[@ng-reflect-field='phone']")), "phone field filter");
+        softAssert.assertTrue(app.getUiboHelper().isElementPresent(By.xpath("//p-columnfilter[@field='site']")), "site");
+        softAssert.assertAll();
+    }
+}
