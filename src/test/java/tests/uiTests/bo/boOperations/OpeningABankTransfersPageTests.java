56fb40fef8b16eece677c1aead691480bd77274f
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-08-12 10:20:10 +0300

    Refactor

--- src/test/java/tests/uiTests/bo/boOperations/OpeningABankTransfersPageTests.java
+++ src/test/java/tests/uiTests/bo/boOperations/OpeningABankTransfersPageTests.java
@@ -2,9 +2,12 @@ package tests.uiTests.bo.boOperations;
 
 import base.UITestBase;
 import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static org.testng.Assert.assertEquals;
@@ -31,18 +34,26 @@ public class OpeningABankTransfersPageTests extends UITestBase {
         softAssert.assertAll();
     }
 
-    @Test
-    public void OpeningABankTransfersWithStateError() throws InterruptedException {
+    @Test(dataProvider = "OpeningABankTransfersWithStateErrorData")
+    public void OpeningABankTransfersWithStateError(String from, String till, String stateName, String file) throws InterruptedException {
         app.getUiboHelper().gotoBOSiteAndLoginWithCBOUserRole(app.CBOuserLogin, app.CBOuserPass);
         app.getUiboHelper().gotoOperations();
         app.getUiboHelper().gotoBankTransfersTab();
-        app.getUiboHelper().searchForPeriod("03.03.2019", "11.08.2022");
-        app.getUiboHelper().selectFromDropDown(By.xpath("//p-columnfilter[@field='stateName']"), "Error");
-        app.getUiboHelper().selectsTransfer(By.xpath("//td[@ng-reflect-text='Error']"));
+        app.getUiboHelper().searchForPeriod(from, till);
+        app.getUiboHelper().selectFromDropDown(By.xpath("//p-columnfilter[@field='stateName']"), stateName);
+        app.getUiboHelper().selectsTransfer(By.xpath("//td[@ng-reflect-text='"+stateName+"']"));
 
         List<String> actualElementsText = app.getUiboHelper().getActualText(By.xpath("//app-transactions-details //table //tbody //tr"));
-        List<String> expectedElementsText = app.getUiboHelper().getDateFromFile("files/bo/boOperations/OpeningABankTransfersWithStateError.txt");
+        List<String> expectedElementsText = app.getUiboHelper().getDateFromFile("files/bo/boOperations/"+file+"");
 
         assertEquals(actualElementsText, expectedElementsText);
+    }
+
+    @DataProvider
+    public Iterator<Object[]> OpeningABankTransfersWithStateErrorData(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {"03.03.2019", "11.08.2022", "Error", "OpeningABankTransfersWithStateError.txt"});
+        list.add(new Object[] {"03.03.2019", "11.08.2022", "To do", "OpeningABankTransfersWithStateToDo.txt"});
+        return list.iterator();
     }
 }
