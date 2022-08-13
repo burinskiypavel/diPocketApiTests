ec6fbc1d3db9a35a6c3735284416291b00121bea
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-08-13 23:19:26 +0300

    Refactor

--- src/test/java/appmanager/ApplicationManager.java
+++ src/test/java/appmanager/ApplicationManager.java
@@ -114,22 +114,6 @@ public class ApplicationManager {
     public String BOuserPass = "ZG7Y5gS";
     public String BOusername = "PAVELB_AUTO_BO";
 
-
-    //private MailHelper mailHelper;
-
-//    public java.util.Properties loadDataFromConfigFile(){
-//        FileInputStream input = null;
-//        try {
-//            input = new FileInputStream("src\\test\\java\\config\\config.properties");
-//            prop.load(input);
-//        } catch (Exception exp) {
-//            System.out.println(exp.getMessage());
-//            System.out.println(exp.getCause());
-//            exp.printStackTrace();
-//        }
-//        return prop;
-//    }
-
     public void initStart() {
 //        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
 //        ChromeOptions chromeOptions = new ChromeOptions();
