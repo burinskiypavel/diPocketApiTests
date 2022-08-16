b6f7c9ff3ca35b7ac86ef8b3c0b4b5b08d92f5a3
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-08-16 15:23:34 +0300

    Refactor

--- src/test/java/tests/uiTests/bo/boTicket/OpeningTicketPageTest.java
+++ src/test/java/tests/uiTests/bo/boTicket/OpeningTicketPageTest.java
@@ -2,6 +2,7 @@ package tests.uiTests.bo.boTicket;
 
 import base.UITestBase;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
@@ -27,8 +28,8 @@ public class OpeningTicketPageTest extends UITestBase {
         app.getUiboHelper().gotoTakeTicket();
 
         app.getUiboHelper().click(By.xpath("//app-button[@ng-reflect-label='Edit']"));
-        app.getUiboHelper().click(By.xpath("//app-select-async[@ng-reflect-name='gender']"));
-        app.getUiboHelper().click(By.xpath("//p-dropdownitem[@ng-reflect-label='M']"));
+
+        app.getUiboHelper().selectFromDropDown(By.xpath("//app-select-async[@ng-reflect-name='gender']"), "M");
 
 
         app.getUiboHelper().selectFromDropDown(By.xpath("//app-select-async[@ng-reflect-name='photoIdTypeId']"), "Passport");
@@ -47,6 +48,84 @@ public class OpeningTicketPageTest extends UITestBase {
 
         app.getUiboHelper().click(By.xpath("//app-button[@ng-reflect-label='Approve']"));
 
+
+        app.getUiboHelper().waitFor(By.xpath("//*[contains(text(), 'Ticket approved successfully')]"));
+
+        app.getUiboHelper().waitFor(By.xpath("//*[contains(text(), 'Take Ticket')]"));
+
+        assertTrue(app.getUiboHelper().areElementsPresent(new String[]{"//*[contains(text(), 'Take Ticket')]", "//*[contains(text(), 'Search')]"}));
+    }
+
+    @Test
+    public void  testApproveSDDTicketWithoutGenderChoice() throws InterruptedException {
+        app.getUiboHelper().gotoBOSiteAndLoginWithBOUserRole(app.BOuserLogin, app.BOuserPass);
+        app.getUiboHelper().gotoTakeTicket();
+
+
+
+//        if(app.getUiboHelper().areElementsPresent(new String[]{"//td[contains(text(), 'Gender')]"})){
+//            WebElement table = app.getUiboHelper().findElement(By.cssSelector("div.data-table tbody"));
+//            WebElement row = table.findElement(By.xpath("//tr[3]"));
+//            String rows = row.getText();
+//
+//            WebElement td = row.findElement(By.xpath("//tr[3] //td[2]"));
+//
+//            String tdd = td.getText();
+//            System.out.println(tdd);
+//
+//            app.getUiboHelper().click(By.xpath("//app-button[@ng-reflect-label='Approve']"));
+//
+//            app.getUiboHelper().waitFor(By.xpath("//*[contains(text(), 'Ticket approved successfully')]"));
+//
+//
+//            app.getUiboHelper().waitFor(By.xpath("//p[contains(text(), 'Take Ticket')]"));
+//            app.getUiboHelper().click(By.xpath("//p[contains(text(), 'Take Ticket')]"));
+//
+//        }
+
+//        app.getUiboHelper().click(By.xpath("//a[@role='tab'] //span[contains(text(), 'Profile data')]"));
+//
+//        WebElement table = app.getUiboHelper().findElements(By.cssSelector("div.client-data tbody")).get(0);
+//        WebElement row = table.findElement(By.xpath("//tr[3]"));
+//        String rows = row.getText();
+//
+//        WebElement td = row.findElement(By.xpath("//tr[3] //td[1]"));
+//
+//        String tdd = td.getText();
+//        System.out.println(tdd);
+
+        app.getUiboHelper().click(By.xpath("//app-button[@ng-reflect-label='Edit']"));
+
+
+        //app-select-async[@ng-reflect-name='gender'] //span[contains(text(), 'All')]
+
+        if(app.getUiboHelper().isElementPresent(By.xpath("//app-select-async[@ng-reflect-name='gender'] //span[contains(text(), 'All')]"))){
+            app.getUiboHelper().click(By.cssSelector("div.p-dialog-header-icons"));
+        }
+
+
+        app.getUiboHelper().click(By.xpath("//app-button[@ng-reflect-label='Approve']"));
+
+        app.getUiboHelper().waitFor(By.xpath("//*[contains(text(), 'Impossible to approve ticket. “Gender” field should be filled')]"));
+
+        app.getUiboHelper().click(By.xpath("//app-button[@ng-reflect-label='Edit']"));
+        ;
+
+        app.getUiboHelper().selectFromDropDown(By.xpath("//app-select-async[@ng-reflect-name='gender']"), "M");
+
+
+
+        //app.getUiboHelper().selectFromDropDown(By.xpath("//app-select-async[@ng-reflect-name='photoIdTypeId']"), "Passport");
+        //app.getUiboHelper().type(By.xpath("//app-input[@ng-reflect-name='photoIdNo'] //input"), "12345678");
+        //app.getUiboHelper().type(By.xpath("//app-input[@ng-reflect-name='identifyCode'] //input"), "12345678");
+        //app.getUiboHelper().selectFromDropDown(By.xpath("//app-select-async[@ng-reflect-name='photoIdCountryId']"), "Poland");
+
+        Thread.sleep(1500);
+        app.getUiboHelper().click(By.xpath("//p-button[@ng-reflect-label='Save']"));
+
+        app.getUiboHelper().waitFor(By.xpath("//*[contains(text(), 'Client data updated successfully')]"));
+
+        app.getUiboHelper().click(By.xpath("//app-button[@ng-reflect-label='Approve']"));
 
         app.getUiboHelper().waitFor(By.xpath("//*[contains(text(), 'Ticket approved successfully')]"));
 
