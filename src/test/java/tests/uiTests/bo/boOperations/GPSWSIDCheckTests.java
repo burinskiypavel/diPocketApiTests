ff752cab19744647a587f49e71e1663825772791
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-08-17 17:37:50 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boOperations/GPSWSIDCheckTests.java
+++ src/test/java/tests/uiTests/bo/boOperations/GPSWSIDCheckTests.java
@@ -11,7 +11,7 @@ public class GPSWSIDCheckTests extends UITestBase {
         app.getUiboHelper().gotoBOSiteAndLoginWithCBOUserRole(app.CBOuserLogin2, app.CBOuserPass2);
         app.getUiboOperationsHelper().gotoOperations();
         app.getUiboOperationsHelper().gotoGPSWSIDCheckTab();
-        app.getUiboHelper().type(By.cssSelector("app-gps-wsid-check-tap p-inputnumber input"), "12345");
+        app.getUiboHelper().type(By.cssSelector("app-gps-wsid-check-tap p-inputnumber input"), "20669371");
         app.getUiboHelper().click(By.xpath("//p-button[@ng-reflect-label='Check']"));
     }
 }
