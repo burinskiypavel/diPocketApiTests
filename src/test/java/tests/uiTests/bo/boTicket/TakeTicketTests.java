bebe31282c7864f01891a7100b929f30abf752b6
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-09-02 16:55:02 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boTicket/TakeTicketTests.java
+++ src/test/java/tests/uiTests/bo/boTicket/TakeTicketTests.java
@@ -266,6 +266,38 @@ public class TakeTicketTests extends UITestBase {
         assertTrue(app.getUiboHelper().areElementsPresent(new String[]{"//*[contains(text(), 'Take Ticket')]", "//*[contains(text(), 'Search')]"}));
     }
 
+    @Test
+    public void testTheUserChangedHisMindAboutEscalateToCBOTicketWithoutReason() throws InterruptedException, SQLException, ClassNotFoundException {
+        app.getUiboHelper().gotoBOSiteAndLoginWithBOUserRole(app.BOuserLogin, app.BOuserPass);
+        app.getUiboTicketHelper().gotoTakeTicketWithReg();
+
+        if(app.getUiboHelper().findElements(By.id("takeTicketContent")).size() == 0){
+            Login_RegistrationHelper login_registrationHelper = new Login_RegistrationHelper();
+            login_registrationHelper.dipocketRegistration();
+            app.getUiboTicketHelper().gotoTakeTicket();
+        }
+
+        if(app.getUiboHelper().isElementPresent(By.xpath("//*[contains(text(), 'Video Call')]"))){
+            app.getUiboTicketHelper().delayTicketForOneMinute();
+            app.getUiboTicketHelper().gotoTakeTicket();
+        }
+
+        for(int i = 0; i < 3; i++) {
+            if (app.getUiboHelper().isElementPresent(By.xpath("//*[contains(text(), 'FDD - check client')]"))) {
+                app.getUiboTicketHelper().delayTicketForOneMinute();
+                app.getUiboTicketHelper().gotoTakeTicket();
+            }
+        }
+
+        if(app.getUiboHelper().isElementPresent(By.xpath("//*[contains(text(), 'SDD - check client')]"))){
+            app.getUiboHelper().click(By.xpath("//app-button[@ng-reflect-label='Escalate to CBO']"));
+            app.getUiboHelper().closePopUp(By.cssSelector("div.p-dialog-header-icons"));
+            app.getUiboHelper().waitForInvisibilityOfElement(By.xpath("//div[@role='dialog']"));
+        } else {
+            Assert.fail("There is no sdd ticket");
+        }
+    }
+
 
     @Test
     public void testTheUserNeedsToEscalateToCBOTicketWithReason() throws InterruptedException, SQLException, ClassNotFoundException {
