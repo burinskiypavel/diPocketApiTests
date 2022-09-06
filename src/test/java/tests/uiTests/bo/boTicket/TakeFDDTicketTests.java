9209e2469262c8afb014587f3f7e8287ea1ffa5b
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-09-06 18:40:50 +0300

    Update RegistrationTest

--- src/test/java/tests/uiTests/bo/boTicket/TakeFDDTicketTests.java
+++ src/test/java/tests/uiTests/bo/boTicket/TakeFDDTicketTests.java
@@ -12,12 +12,41 @@ public class TakeFDDTicketTests extends UITestBase {
     @Test(priority = 1)
     public void testApproveFDDTiketWithDocumentTypePassport() throws InterruptedException, SQLException, ClassNotFoundException {
         app.getUiboHelper().gotoBOSiteAndLoginWithBOUserRole(app.BOuserLogin, app.BOuserPass);
-        app.getUiboTicketHelper().gotoTakeTicket();
+        app.getUiboTicketHelper().gotoTakeTicketWithReg();
 
         if (app.getUiboHelper().findElements(By.id("takeTicketContent")).size() == 0) {
             Login_RegistrationHelper login_registrationHelper = new Login_RegistrationHelper();
             login_registrationHelper.dipocketRegistration();
             app.getUiboTicketHelper().gotoTakeTicket();
+
+            String id = app.getUiboHelper().getText(By.xpath("//*[contains(text(), 'ID:')] //span"));
+            app.getUiboTicketHelper().editAndSaveSDDTicket("M", "", "", "", "");
+            app.getUiboTicketHelper().approveTicketSuccessfully();
+            app.getUiboHelper().gotoSearchPage();
+            app.getUiboHelper().search("id", id);
+            app.getUiboHelper().goToClientPage("380685448615");
+
+            app.getUiboHelper().click(By.xpath("//p-button[@ng-reflect-label='Upload docs']"));
+            app.getUiboHelper().selectFromDropDown(By.xpath("//app-select-async[@ng-reflect-name='typeId']"), "PhotoID");
+            app.getUiboHelper().uploadFile(By.xpath("//input[@type='file']"), "C:/Work/Files/self.jpg");
+            Thread.sleep(1000);
+            app.getUiboHelper().click(By.xpath("//p-button[@ng-reflect-label='Confirm']"));
+
+            app.getUiboHelper().click(By.xpath("//p-button[@ng-reflect-label='Upload docs']"));
+            app.getUiboHelper().selectFromDropDown(By.xpath("//app-select-async[@ng-reflect-name='typeId']"), "Proof of address");
+            app.getUiboHelper().uploadFile(By.xpath("//input[@type='file']"), "C:/Work/Files/self.jpg");
+            Thread.sleep(1000);
+            app.getUiboHelper().click(By.xpath("//p-button[@ng-reflect-label='Confirm']"));
+
+            app.getUiboHelper().click(By.xpath("//p-button[@ng-reflect-label='Upload docs']"));
+            app.getUiboHelper().selectFromDropDown(By.xpath("//app-select-async[@ng-reflect-name='typeId']"), "PhotoID Back");
+            app.getUiboHelper().uploadFile(By.xpath("//input[@type='file']"), "C:/Work/Files/self.jpg");
+            Thread.sleep(1000);
+            app.getUiboHelper().click(By.xpath("//p-button[@ng-reflect-label='Confirm']"));
+            app.getUiboHelper().click(By.xpath("//p-button[@ng-reflect-label='Home']"));
+            app.getUiboHelper().waitFor(By.cssSelector("div[ng-reflect-router-link='take_ticket']"));
+            app.getUiboTicketHelper().gotoTakeTicketWithReg();
+
         }
 
         if (app.getUiboHelper().isElementPresent(By.xpath("//*[contains(text(), 'Video Call')]"))) {
