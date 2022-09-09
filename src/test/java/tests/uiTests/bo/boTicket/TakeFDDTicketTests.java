b1b3f1135cf292d982a8a46ee7738cf7938d553d
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-09-09 15:39:30 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boTicket/TakeFDDTicketTests.java
+++ src/test/java/tests/uiTests/bo/boTicket/TakeFDDTicketTests.java
@@ -70,4 +70,25 @@ public class TakeFDDTicketTests extends UITestBase {
             app.getUiboTicketHelper().approveTicketSuccessfully();
         }
     }
+
+    @Test(enabled = false)//bug DEV-3437
+    public void testApproveFDDTicketWithoutFillingInThePESELFieldForResidentsOfPoland() throws InterruptedException, SQLException, ClassNotFoundException {
+        app.getUiboHelper().gotoBOSiteAndLoginWithBOUserRole(app.BOuserLogin, app.BOuserPass);
+        app.getUiboTicketHelper().gotoTakeTicketWithReg();
+
+        if (app.getUiboHelper().findElements(By.id("takeTicketContent")).size() == 0) {
+            Login_RegistrationHelper login_registrationHelper = new Login_RegistrationHelper();
+            login_registrationHelper.dipocketRegistration(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION");
+            app.getUiboTicketHelper().gotoTakeTicket();
+            app.getUiboTicketHelper().initFDDTicketDisplain();
+        }
+
+        app.getUiboTicketHelper().skipVideoCall(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION");
+        app.getUiboTicketHelper().skipSDDCheckClient();
+
+        if (app.getUiboHelper().isElementPresent(By.xpath("//*[contains(text(), 'FDD - check client')]"))) {
+            app.getUiboTicketHelper().editAndSaveFDDTicket("", "Passport", "11111111111", "", "Poland");
+            app.getUiboTicketHelper().approveTicketSuccessfully();
+        }
+    }
 }
