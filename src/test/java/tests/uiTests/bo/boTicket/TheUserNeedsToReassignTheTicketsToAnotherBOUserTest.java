5e69f3c93288d020ee3d4646a96c75fff9c5bdae
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-10-12 17:29:24 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boTicket/TheUserNeedsToReassignTheTicketsToAnotherBOUserTest.java
+++ src/test/java/tests/uiTests/bo/boTicket/TheUserNeedsToReassignTheTicketsToAnotherBOUserTest.java
@@ -4,13 +4,28 @@ import appmanager.Login_RegistrationHelper;
 import base.UITestBase;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class TheUserNeedsToReassignTheTicketsToAnotherBOUserTest extends UITestBase {
     String clientId2 = "29818";
     String phone2 = "380980316499";
+    String actualTicketType = null;
+
+    @DataProvider
+    public Iterator<Object[]> docs(){
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {"PhotoID"});
+        list.add(new Object[] {"Proof of address"});
+        list.add(new Object[] {"PhotoID Back"});
+        list.add(new Object[] {"Second ID"});
+        return list.iterator();
+    }
 
     @Test
     public void testTheUserNeedsToReassignTheSDDTicketToAnotherBOUser_TheUserChangedHisMindAboutReassignTheSDDTicket() throws InterruptedException, SQLException, ClassNotFoundException {
@@ -23,8 +38,27 @@ public class TheUserNeedsToReassignTheTicketsToAnotherBOUserTest extends UITestB
             app.getUiboTicketHelper().gotoTakeTicket();
         }
 
-        app.getUiboTicketHelper().skipVideoCall(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION");
-        app.getUiboTicketHelper().skipFDDCheckClient();
+        actualTicketType = app.getUiboTicketHelper().getActualTicketType();
+
+        for(int i = 0; i < 12; i++) {
+            actualTicketType = app.getUiboTicketHelper().getActualTicketType();
+            if(actualTicketType.contains("SDD - check client")){
+                break;
+            }
+
+            if (!actualTicketType.contains("SDD - check client")) {
+                app.getUiboTicketHelper().delayTicketForSeveralMinutes();
+                app.getUiboTicketHelper().gotoTakeTicketWithReg();
+            }
+
+            if(app.getUiboHelper().findElements(By.id("takeTicketContent")).size() == 0){
+                Login_RegistrationHelper login_registrationHelper = new Login_RegistrationHelper();
+                login_registrationHelper.dipocketRegistration(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION");
+                app.getUiboTicketHelper().gotoTakeTicket();
+            }
+        }
+        //app.getUiboTicketHelper().skipVideoCall(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION");
+        //app.getUiboTicketHelper().skipFDDCheckClient();
 
         if (app.getUiboHelper().isElementPresent(By.xpath("//*[contains(text(), 'SDD - check client')]"))) {
             app.getUiboTicketHelper().verifyTheUserChangedHisMindAboutReassignTheTicketToAnotherBOUser();
@@ -44,8 +78,27 @@ public class TheUserNeedsToReassignTheTicketsToAnotherBOUserTest extends UITestB
             app.getUiboTicketHelper().initFDDTicketDisplain();
         }
 
-        app.getUiboTicketHelper().skipVideoCall(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION");
-        app.getUiboTicketHelper().skipSDDCheckClient();
+        actualTicketType = app.getUiboTicketHelper().getActualTicketType();
+
+        for(int i = 0; i < 12; i++) {
+            actualTicketType = app.getUiboTicketHelper().getActualTicketType();
+            if(actualTicketType.contains("FDD - check client")){
+                break;
+            }
+
+            if (!actualTicketType.contains("FDD - check client")) {
+                app.getUiboTicketHelper().delayTicketForSeveralMinutes();
+                app.getUiboTicketHelper().gotoTakeTicketWithReg();
+            }
+
+            if(app.getUiboHelper().findElements(By.id("takeTicketContent")).size() == 0){
+                Login_RegistrationHelper login_registrationHelper = new Login_RegistrationHelper();
+                login_registrationHelper.dipocketRegistration(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION");
+                app.getUiboTicketHelper().gotoTakeTicket();
+            }
+        }
+        //app.getUiboTicketHelper().skipVideoCall(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION");
+        //app.getUiboTicketHelper().skipSDDCheckClient();
 
         if (app.getUiboHelper().isElementPresent(By.xpath("//*[contains(text(), 'FDD - check client')]"))) {
             app.getUiboTicketHelper().verifyTheUserChangedHisMindAboutReassignTheTicketToAnotherBOUser();
@@ -61,13 +114,33 @@ public class TheUserNeedsToReassignTheTicketsToAnotherBOUserTest extends UITestB
         app.getUiboHelper().gotoHomePageWithBOUser();
         app.getUiboTicketHelper().gotoTakeTicketWithReg();
 
-        app.getUiboTicketHelper().skipVideoCall(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION");
-        app.getUiboTicketHelper().skipSDDCheckClient();
+        actualTicketType = app.getUiboTicketHelper().getActualTicketType();
+
+        for(int i = 0; i < 12; i++) {
+            actualTicketType = app.getUiboTicketHelper().getActualTicketType();
+            if(actualTicketType.equals("Update Selfie")){
+                break;
+            }
+
+            if (!actualTicketType.equals("Update Selfie")) {
+                app.getUiboTicketHelper().delayTicketForSeveralMinutes();
+                app.getUiboTicketHelper().gotoTakeTicketWithReg();
+            }
+
+            if(app.getUiboHelper().findElements(By.id("takeTicketContent")).size() == 0){
+                Login_RegistrationHelper login_registrationHelper = new Login_RegistrationHelper();
+                login_registrationHelper.dipocketRegistration(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION");
+                app.getUiboTicketHelper().gotoTakeTicket();
+            }
+        }
+        //app.getUiboTicketHelper().skipVideoCall(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION");
+        //app.getUiboTicketHelper().skipSDDCheckClient();
 
         if (app.getUiboHelper().isElementPresent(By.xpath("//*[contains(text(), 'Update Selfie')]"))) {
             app.getUiboTicketHelper().verifyTheUserChangedHisMindAboutReassignTheTicketToAnotherBOUser();
             app.getUiboTicketHelper().reassignTicketSuccessfully("BOAUTOTEST", "test");
         } else {
+            System.out.println("actualTicketType: " + actualTicketType);
             Assert.fail("There are no Update Selfie Ticket");
         }
     }
@@ -80,13 +153,34 @@ public class TheUserNeedsToReassignTheTicketsToAnotherBOUserTest extends UITestB
         app.getUiboHelper().gotoHomePageWithBOUser();
         app.getUiboTicketHelper().gotoTakeTicketWithReg();
 
-        app.getUiboTicketHelper().skipVideoCall(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION");
-        app.getUiboTicketHelper().skipSDDCheckClient();
+        actualTicketType = app.getUiboTicketHelper().getActualTicketType();
+
+        for(int i = 0; i < 12; i++) {
+            actualTicketType = app.getUiboTicketHelper().getActualTicketType();
+            if(actualTicketType.equals("Update document")){
+                break;
+            }
+
+            if (!actualTicketType.equals("Update document")) {
+                app.getUiboTicketHelper().delayTicketForSeveralMinutes();
+                app.getUiboTicketHelper().gotoTakeTicketWithReg();
+            }
+
+            if(app.getUiboHelper().findElements(By.id("takeTicketContent")).size() == 0){
+                Login_RegistrationHelper login_registrationHelper = new Login_RegistrationHelper();
+                login_registrationHelper.dipocketRegistration(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION");
+                app.getUiboTicketHelper().gotoTakeTicket();
+            }
+        }
+
+        //app.getUiboTicketHelper().skipVideoCall(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION");
+        //app.getUiboTicketHelper().skipSDDCheckClient();
 
         if (app.getUiboHelper().isElementPresent(By.xpath("//*[contains(text(), 'Update document')]"))) {
             app.getUiboTicketHelper().verifyTheUserChangedHisMindAboutReassignTheTicketToAnotherBOUser();
             app.getUiboTicketHelper().reassignTicketSuccessfully("BOAUTOTEST", "test");
         } else {
+            System.out.println("actualTicketType: " + actualTicketType);
             Assert.fail("There are no Update document Ticket");
         }
     }
