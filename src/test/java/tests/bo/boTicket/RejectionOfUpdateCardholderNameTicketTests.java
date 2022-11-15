faef05a4498158d3ebe382ad28d9da067c23dce6
pa.burinsky <pa.burinsky@csltd.com.ua>
2022-11-15 15:27:24 +0200

    refactor

--- src/test/java/tests/bo/boTicket/RejectionOfUpdateCardholderNameTicketTests.java
+++ src/test/java/tests/bo/boTicket/RejectionOfUpdateCardholderNameTicketTests.java
@@ -16,21 +16,25 @@ import static org.testng.Assert.assertEquals;
 
 public class RejectionOfUpdateCardholderNameTicketTests extends TestBase {
     String cliSessionId = null;
-    String phone = app.homePageLoginPhone;
+    String phone = "420703666872";
     String pass = app.homePagePass;
     String newCardHolderName = null;
     String oldCardHolderName = null;
     String actualCardHolderName = null;
     String cookie = null;
     String username = app.BOusername;
-    int clientId = app.homePageClientId;
+    int clientId;
     int ticketId = 0;
     String actualTypeName = null;
     String tomorrow = null;
+    String email = "testdipocket002@gmail.com";
 
     @Test(priority = 1)
-    public void test_ClientServices_v1_homePage_AutintificateMobileApp() throws SQLException, ClassNotFoundException, ParseException {
+    public void test_ClientServices_v1_homePage_AutintificateMobileApp() throws SQLException, ClassNotFoundException, ParseException, InterruptedException {
         tomorrow = app.getTimeStampWithAddSomeAmountOfDays("dd.MM.yyyy HH:mm:ss", 2);
+        app.getLogin_registrationHelper().dipocketRegistration(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION", app.homePagePass, "1230768000000", phone, HelperBase.prop.getProperty("mobile.registration.email"));
+        clientId = Integer.parseInt(app.getDbHelper().getClientIdFromDB(HelperBase.prop.getProperty("mobile.registration.email"), "DIPOCKET"));
+        app.getDbHelper().updateClientEmailFromDB(email, String.valueOf(clientId));
         cliSessionId = app.getLogin_registrationHelper().loginDipocket(phone, pass, HelperBase.prop.getProperty("mobile.login.deviceuuid"));
     }
 
