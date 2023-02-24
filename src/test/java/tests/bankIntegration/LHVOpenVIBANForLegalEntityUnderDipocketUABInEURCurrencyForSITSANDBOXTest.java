38e532e70a7511f5cc14f86b381a56188b699aa8
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-02-24 18:50:57 +0200

    refactor

--- src/test/java/tests/bankIntegration/LHVOpenVIBANForLegalEntityUnderDipocketUABInEURCurrencyForSITSANDBOXTest.java
+++ src/test/java/tests/bankIntegration/LHVOpenVIBANForLegalEntityUnderDipocketUABInEURCurrencyForSITSANDBOXTest.java
@@ -1,17 +1,30 @@
 package tests.bankIntegration;
 
 import base.TestBase;
+import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
 
+import java.sql.SQLException;
+
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasItem;
 
 public class LHVOpenVIBANForLegalEntityUnderDipocketUABInEURCurrencyForSITSANDBOXTest extends TestBase {
     String sandboxLogin = "SANDBOX";
     String sandboxPass = "W6qQnx7";
+    String  cboUserLogin = "PavelB_CBO";
+    String cboUserPass = "CGc22S0";
+    String username = "PAVELB_CBO";
+    String legalClientId = null;
+    String cookie = null;
+    String sms = null;
+    String firstName = "Pavel";
+    String lastName = "Burinskiy";
+    int individualClientId = 55588;
 
     @Test(priority = 1)
     public void test_CustomerServicesDev_v1_company_register(){
-        given()
+        String response = given()
                 .log().uri().log().headers().log().body()
                 .contentType("application/json")
                 .auth().basic(sandboxLogin, sandboxPass)
@@ -38,6 +51,91 @@ public class LHVOpenVIBANForLegalEntityUnderDipocketUABInEURCurrencyForSITSANDBO
                         "\n" +
                         "} ")
                 .post("https://api.dipocket.site/CustomerServices/v1/company/register")
+                .then().log().all()
+                .statusCode(200).extract().response().asString();
+
+        JsonPath jsonPath = new JsonPath(response);
+        legalClientId = jsonPath.getString("clientId");
+        System.out.println("clientId : " + legalClientId);
+    }
+
+    @Test(priority = 2)
+    public void test_BOServices_v1_auth_authentication() throws SQLException, ClassNotFoundException {
+        cookie = app.getBoRequestsHelper().boServices_v1_auth_authentication_test(cboUserLogin, cboUserPass, username);
+    }
+
+    @Test(priority = 3)
+    public void test_BOServices_v1_representative_createScreened() throws SQLException, ClassNotFoundException {
+        String message = app.getDbHelper().getBOLoginSMSCodeFromTestDB();
+        sms = message.substring(13);
+
+        given()
+                .spec(app.requestSpecBOTest)
+                .cookie(cookie)
+                .header("bo-auth-token", sms)
+                .contentType("application/json")
+                .body("{\n" +
+                        "  \"corpClientId\" : "+ legalClientId +",\n" +
+                        "  \"firstName\" : \""+firstName+"\",\n" +
+                        "  \"lastName\" : \""+lastName+"\",\n" +
+                        "  \"cardholderName\" : \"Pavel Burinskiy\",\n" +
+                        "  \"birthDate\" : \"14.02.1992\",\n" +
+                        "  \"phoneNumber\" : \"38068"+app.generateRandomNumber(7)+"\",\n" +
+                        "  \"email\" : \"pavelburinskiy"+app.generateRandomNumber(7)+"@gmail.com\",\n" +
+                        "  \"ddStatus\" : \"FDD\",\n" +
+                        "  \"currencyId\" : 978,\n" +
+                        "  \"langId\" : 1,\n" +
+                        "  \"identifyCode\" : \"13124124124\",\n" +
+                        "  \"citizenshipCountryId\" : 440,\n" +
+                        "  \"residenceCountryId\" : 440,\n" +
+                        "  \"streetLine1\" : \"Address\",\n" +
+                        "  \"streetLine2\" : \"Address\",\n" +
+                        "  \"city\" : \"Kharkiv\",\n" +
+                        "  \"zip\" : \"12314124124124\"\n" +
+                        "}")
+                .post("/v1/representative/createScreened")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test(priority = 4)
+    public void test_BOServices_v1_representative_legalClientId(){
+        given()
+                .spec(app.requestSpecBOTest)
+                .header("bo-auth-token", sms)
+                .cookie(cookie)
+                .pathParam("legalClientId", legalClientId)
+                .get("/v1/representative/{legalClientId}")
+                .then().log().all()
+                .statusCode(200)
+                .body("firstName", hasItem(firstName),
+                        "lastName", hasItem(lastName));
+    }
+
+    @Test(priority = 6)
+    public void test_BOServices_v1_representative_link(){
+        given()
+                .spec(app.requestSpecBOTest)
+                .header("bo-auth-token", sms)
+                .cookie(cookie)
+                .body("{\n" +
+                        "  \"clientId\" : "+individualClientId+",\n" +
+                        "  \"corpClientId\" : "+ legalClientId +"\n" +
+                        "}")
+                .post("/v1/representative/link")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    @Test(priority = 7)
+    public void test_BOServices_v1_representative_legalClientId_unlink_individualClientId(){
+        given()
+                .spec(app.requestSpecBOTest)
+                .header("bo-auth-token", sms)
+                .cookie(cookie)
+                .pathParam("legalClientId", legalClientId)
+                .pathParam("individualClientId", individualClientId)
+                .post("/v1/representative/{legalClientId}/unlink/{individualClientId}")
                 .then().log().all()
                 .statusCode(200);
     }
