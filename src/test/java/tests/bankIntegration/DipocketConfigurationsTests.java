5a665afa57cde18bce276cf6e3f5155b74da1fdf
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-02-28 17:18:17 +0200

    refactor

--- src/test/java/tests/bankIntegration/DipocketConfigurationsTests.java
+++ src/test/java/tests/bankIntegration/DipocketConfigurationsTests.java
@@ -14,24 +14,24 @@ import static org.testng.Assert.assertEquals;
 public class DipocketConfigurationsTests extends TestBase {
     List<String> actualrow = new ArrayList<>();
 
-    @Test(priority = 1)
+    @Test
     public void test_verifySiteDipocketRowWithMasterACCIDSiteCountryIdCurrencyIdFromLHV_EE_MASTERSITE() throws SQLException, ClassNotFoundException {
         actualrow = app.getDbHelper().getAllRow_FromLHV_EE_MASTERSITE_FromTestDB(Site.DIPOCKET.toString(), 1);
         assertEquals(actualrow, Arrays.asList("1", "DIPOCKET", "440", "978", null));
     }
-    @Test(priority = 2)
+    @Test
     public void test_verifySiteDipocketRowWithMasterACCIDSiteCountryIdCurrencyIdFromLHV_EE_MASTERSITE_2() throws SQLException, ClassNotFoundException {
         actualrow = app.getDbHelper().getAllRow_FromLHV_EE_MASTERSITE_FromTestDB(Site.DIPOCKET.toString(), 2);
         assertEquals(actualrow, Arrays.asList("2", "DIPOCKET", "440", "826", null));
     }
 
-    @Test(priority = 3)
+    @Test
     public void test_verifySiteDipocketRowWithMasterACCIDSiteCountryIdCurrencyIdFromLHV_EE_MASTERSITE_3() throws SQLException, ClassNotFoundException {
         actualrow = app.getDbHelper().getAllRow_FromLHV_EE_MASTERSITE_FromTestDB(Site.DIPOCKET.toString(), 3);
         assertEquals(actualrow, Arrays.asList("3", "DIPOCKET", "826", "978", null));
     }
 
-    @Test(priority = 4)
+    @Test
     public void test_verifySiteDipocketRowWithMasterACCIDSiteCountryIdCurrencyIdFromLHV_EE_MASTERSITE_4() throws SQLException, ClassNotFoundException {
         actualrow = app.getDbHelper().getAllRow_FromLHV_EE_MASTERSITE_FromTestDB(Site.DIPOCKET.toString(), 4);
         assertEquals(actualrow, Arrays.asList("4", "DIPOCKET", "826", "826", null));
