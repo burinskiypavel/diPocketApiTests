c06c1f845ef237d4864201c9dc02cd3de9ae54d3
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-03-21 11:42:35 +0200

    refactor

--- src/test/java/tests/bankIntegration/BankTransferSwiftTest.java
+++ src/test/java/tests/bankIntegration/BankTransferSwiftTest.java
@@ -1,15 +1,14 @@
 package tests.bankIntegration;
 
 import base.TestBase;
-import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class BankTransferSwiftTest extends TestBase {
     String city = "Vilnius";
-    String login = "ASPSP.DIPOCKET";
-    String pass = "W6qQnx7";
+    String login = "APIOLENA";
+    String pass = "pU9N1Lu";
 
     @Test
     public void test_CustomerServices_v1_bankTransfer_SWIFT(){
@@ -18,30 +17,28 @@ public class BankTransferSwiftTest extends TestBase {
                 .contentType("application/json")
                 .auth().basic(login, pass)
                 .body("{\n" +
-                        "  \"accountId\": 117202,\n" +
-                        "  \"amount\": 10,\n" +
-                        "  \"bankId\": \"SWIFT\",\n" +
-                        "  \"beneficiaryAccount\": \"EE657777000012110759\",\n" +
-                        "  \"beneficiaryType\": \"INDIVIDUAL\",\n" +
+                        "  \"accountId\": 111231,\n" +
+                        "  \"amount\": 148,\n" +
+                        "  \"bankId\": \"LHVBEE22\",\n" +
+                        "  \"beneficiaryAccount\": \"EE407777000012064164\",\n" +
+                        "  \"beneficiaryType\": \"COMPANY\",\n" +
                         "  \"city\": \""+city+"\",\n" +
-                        "  \"companyName\": \"TestCompanyBankTransferSwift\",\n" +
-                        "  \"countryCode\": \"440\",\n" +
+                        "  \"companyName\": \"OCORPTEST\",\n" +
+                        "  \"countryCode\": \"LT\",\n" +
                         "  \"currencyCode\": \"EUR \",\n" +
                         "  \"feeAmount\": 250,\n" +
                         "  \"feeCurrencyCode\": \"EUR\",\n" +
                         "  \"firstName\": \"Pavel\",\n" +
                         "  \"lastName\": \"Burinskiy\",\n" +
-                        "  \"reference\": \"cross border transfer test version1\",\n" +
+                        "  \"reference\": \"oki\",\n" +
                         "  \"requestId\": \"d1f202fe-df2e-46da-94ba-"+app.generateRandomString(12)+"\",\n" +
                         "  \"streetLine1\": \"streetLine1\",\n" +
                         "  \"streetLine2\": \"streetLine2\",\n" +
                         "  \"zip\": \"123456\"\n" +
                         "}")
-                .post("https://api.dipocket.site/CustomerServices/v1/bankTransfer/SWIFT")
+                .post("https://api.dipocket.site/CustomerServices/v1/account/bankTransfer/SWIFT")
                 .then().log().all()
                 .statusCode(200).extract().response().asString();
 
-        //JsonPath jsonPath = new JsonPath(response);
-        //clientIdSandbox = jsonPath.getInt("clientId");
     }
 }
