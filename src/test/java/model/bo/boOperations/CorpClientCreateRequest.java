a35f46a9fc16920e6e9c9d4b7724948e3e64092d
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-03-22 17:24:54 +0200

    refactor

---
+++ src/test/java/model/bo/boOperations/CorpClientCreateRequest.java
@@ -0,0 +1,266 @@
+package model.bo.boOperations;
+
+public class CorpClientCreateRequest {
+
+    private String accName;
+    private int accTypeId;
+    private int accountContractCountryId;
+    private long identifyCode;
+    private String stateMail;
+    private String stateReg;
+    private String site;
+    private String clientType;
+    private int langId;
+    private String companyName;
+    private int currencyId;
+    private String ddStatus;
+    private int feeTariffPlanId;
+    private int defCardProgramId;
+    private int limitPlanId;
+    private int operLimitPlanId;
+    private String streetLine1Reg;
+    private String streetLine2Reg;
+    private String cityReg;
+    private int zipReg;
+    private int countryIdReg;
+    private boolean registeredAddrAsMail;
+    private String streetLine1Mail;
+    private String streetLine2Mail;
+    private String cityMail;
+    private int zipMail;
+    private int countryIdMail;
+    private int contractCountryId;
+    private String citizenship;
+
+    public String getAccName() {
+        return accName;
+    }
+
+    public void setAccName(String accName) {
+        this.accName = accName;
+    }
+
+    public int getAccTypeId() {
+        return accTypeId;
+    }
+
+    public void setAccTypeId(int accTypeId) {
+        this.accTypeId = accTypeId;
+    }
+
+    public int getAccountContractCountryId() {
+        return accountContractCountryId;
+    }
+
+    public void setAccountContractCountryId(int accountContractCountryId) {
+        this.accountContractCountryId = accountContractCountryId;
+    }
+
+    public long getIdentifyCode() {
+        return identifyCode;
+    }
+
+    public void setIdentifyCode(long identifyCode) {
+        this.identifyCode = identifyCode;
+    }
+
+    public String getStateMail() {
+        return stateMail;
+    }
+
+    public void setStateMail(String stateMail) {
+        this.stateMail = stateMail;
+    }
+
+    public String getStateReg() {
+        return stateReg;
+    }
+
+    public void setStateReg(String stateReg) {
+        this.stateReg = stateReg;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public String getClientType() {
+        return clientType;
+    }
+
+    public void setClientType(String clientType) {
+        this.clientType = clientType;
+    }
+
+    public int getLangId() {
+        return langId;
+    }
+
+    public void setLangId(int langId) {
+        this.langId = langId;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getCurrencyId() {
+        return currencyId;
+    }
+
+    public void setCurrencyId(int currencyId) {
+        this.currencyId = currencyId;
+    }
+
+    public String getDdStatus() {
+        return ddStatus;
+    }
+
+    public void setDdStatus(String ddStatus) {
+        this.ddStatus = ddStatus;
+    }
+
+    public int getFeeTariffPlanId() {
+        return feeTariffPlanId;
+    }
+
+    public void setFeeTariffPlanId(int feeTariffPlanId) {
+        this.feeTariffPlanId = feeTariffPlanId;
+    }
+
+    public int getDefCardProgramId() {
+        return defCardProgramId;
+    }
+
+    public void setDefCardProgramId(int defCardProgramId) {
+        this.defCardProgramId = defCardProgramId;
+    }
+
+    public int getLimitPlanId() {
+        return limitPlanId;
+    }
+
+    public void setLimitPlanId(int limitPlanId) {
+        this.limitPlanId = limitPlanId;
+    }
+
+    public int getOperLimitPlanId() {
+        return operLimitPlanId;
+    }
+
+    public void setOperLimitPlanId(int operLimitPlanId) {
+        this.operLimitPlanId = operLimitPlanId;
+    }
+
+    public String getStreetLine1Reg() {
+        return streetLine1Reg;
+    }
+
+    public void setStreetLine1Reg(String streetLine1Reg) {
+        this.streetLine1Reg = streetLine1Reg;
+    }
+
+    public String getStreetLine2Reg() {
+        return streetLine2Reg;
+    }
+
+    public void setStreetLine2Reg(String streetLine2Reg) {
+        this.streetLine2Reg = streetLine2Reg;
+    }
+
+    public String getCityReg() {
+        return cityReg;
+    }
+
+    public void setCityReg(String cityReg) {
+        this.cityReg = cityReg;
+    }
+
+    public int getZipReg() {
+        return zipReg;
+    }
+
+    public void setZipReg(int zipReg) {
+        this.zipReg = zipReg;
+    }
+
+    public int getCountryIdReg() {
+        return countryIdReg;
+    }
+
+    public void setCountryIdReg(int countryIdReg) {
+        this.countryIdReg = countryIdReg;
+    }
+
+    public boolean isRegisteredAddrAsMail() {
+        return registeredAddrAsMail;
+    }
+
+    public void setRegisteredAddrAsMail(boolean registeredAddrAsMail) {
+        this.registeredAddrAsMail = registeredAddrAsMail;
+    }
+
+    public String getStreetLine1Mail() {
+        return streetLine1Mail;
+    }
+
+    public void setStreetLine1Mail(String streetLine1Mail) {
+        this.streetLine1Mail = streetLine1Mail;
+    }
+
+    public String getStreetLine2Mail() {
+        return streetLine2Mail;
+    }
+
+    public void setStreetLine2Mail(String streetLine2Mail) {
+        this.streetLine2Mail = streetLine2Mail;
+    }
+
+    public String getCityMail() {
+        return cityMail;
+    }
+
+    public void setCityMail(String cityMail) {
+        this.cityMail = cityMail;
+    }
+
+    public int getZipMail() {
+        return zipMail;
+    }
+
+    public void setZipMail(int zipMail) {
+        this.zipMail = zipMail;
+    }
+
+    public int getCountryIdMail() {
+        return countryIdMail;
+    }
+
+    public void setCountryIdMail(int countryIdMail) {
+        this.countryIdMail = countryIdMail;
+    }
+
+    public int getContractCountryId() {
+        return contractCountryId;
+    }
+
+    public void setContractCountryId(int contractCountryId) {
+        this.contractCountryId = contractCountryId;
+    }
+
+    public String getCitizenship() {
+        return citizenship;
+    }
+
+    public void setCitizenship(String citizenship) {
+        this.citizenship = citizenship;
+    }
+}
--- src/test/java/tests/bo/boOperations/CreatCorporateClientWithAccountTest.java
+++ src/test/java/tests/bo/boOperations/CreatCorporateClientWithAccountTest.java
@@ -2,6 +2,8 @@ package tests.bo.boOperations;
 
 import base.TestBase;
 import com.cs.dipocketback.base.data.Site;
+import com.google.gson.Gson;
+import model.bo.boOperations.CorpClientCreateRequest;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
@@ -14,6 +16,8 @@ public class CreatCorporateClientWithAccountTest extends TestBase {
     int countryId = 440;
     String countryCode = "LT";
     String companyName = "Predict";
+    CorpClientCreateRequest corpClientCreateRequest = new CorpClientCreateRequest();
+    Gson gson = new Gson();
 
     @Test(priority = 1)
     public void test_BOServices_v1_auth_authentication() {
@@ -34,41 +38,73 @@ public class CreatCorporateClientWithAccountTest extends TestBase {
     }
     @Test(priority = 3)
     public void test_BOServices_v1_user_corpClients_createScreened(){
+        corpClientCreateRequest.setAccName("TestAcc");
+        corpClientCreateRequest.setAccTypeId(1);
+        corpClientCreateRequest.setAccountContractCountryId(440);
+        corpClientCreateRequest.setIdentifyCode(12132131231l);
+        corpClientCreateRequest.setStateMail("String");
+        corpClientCreateRequest.setStateReg("String");
+        corpClientCreateRequest.setSite(site);
+        corpClientCreateRequest.setClientType("C");
+        corpClientCreateRequest.setLangId(1);
+        corpClientCreateRequest.setCompanyName(companyName);
+        corpClientCreateRequest.setCurrencyId(currencyId);
+        corpClientCreateRequest.setDdStatus("FDD");
+        corpClientCreateRequest.setFeeTariffPlanId(2);
+        corpClientCreateRequest.setDefCardProgramId(1);
+        corpClientCreateRequest.setLimitPlanId(2);
+        corpClientCreateRequest.setOperLimitPlanId(1);
+        corpClientCreateRequest.setStreetLine1Reg("Main str");
+        corpClientCreateRequest.setStreetLine2Reg("50");
+        corpClientCreateRequest.setCityReg("Krakiv");
+        corpClientCreateRequest.setZipReg(2123123);
+        corpClientCreateRequest.setCountryIdReg(countryId);
+        corpClientCreateRequest.setRegisteredAddrAsMail(false);
+        corpClientCreateRequest.setStreetLine1Mail("Main str");
+        corpClientCreateRequest.setStreetLine2Mail("50");
+        corpClientCreateRequest.setCityMail("Krakiv");
+        corpClientCreateRequest.setZipMail(2123123);
+        corpClientCreateRequest.setCountryIdMail(countryId);
+        corpClientCreateRequest.setContractCountryId(countryId);
+        corpClientCreateRequest.setCitizenship(countryCode);
+
+        String json = gson.toJson(corpClientCreateRequest);
+
         given()
                 .spec(app.requestSpecBO)
                 .cookie(cookie)
-                .body("{\n" +
-                        "  \"accName\" : \"TestAcc\",\n" +
-                        "  \"accTypeId\" : \"1\",\n" +
-                        "  \"accountContractCountryId\" : \"440\",\n" +
-                        "  \"identifyCode\" : \"12132131231\",\n" +
-                        "  \"stateMail\" : \"String\",\n" +
-                        "  \"stateReg\" : \"String\",\n" +
-                        "  \"stateReg\" : \"String\",\n" +
-                        "  \"site\" : \""+site+"\",\n" +
-                        "  \"clientType\" : \"C\",\n" +
-                        "  \"langId\" : 1,\n" +
-                        "  \"companyName\" : \""+companyName+"\",\n" +
-                        "  \"currencyId\" : "+currencyId+",\n" +
-                        "  \"ddStatus\" : \"FDD\",\n" +
-                        "  \"feeTariffPlanId\" : 2,\n" +
-                        "  \"defCardProgramId\" : 1,\n" +
-                        "  \"limitPlanId\" : 2,\n" +
-                        "  \"operLimitPlanId\" : 1,\n" +
-                        "  \"streetLine1Reg\" : \"Gagarina ave\",\n" +
-                        "  \"streetLine2Reg\" : \"62\",\n" +
-                        "  \"cityReg\" : \"Krakiv\",\n" +
-                        "  \"zipReg\" : \"2123123\",\n" +
-                        "  \"countryIdReg\" : "+countryId+",\n" +
-                        "  \"registeredAddrAsMail\" : false,\n" +
-                        "  \"streetLine1Mail\" : \"Gagarina ave\",\n" +
-                        "  \"streetLine2Mail\" : \"62\",\n" +
-                        "  \"cityMail\" : \"Krakiv\",\n" +
-                        "  \"zipMail\" : \"2123123\",\n" +
-                        "  \"countryIdMail\" : "+countryId+",\n" +
-                        "  \"contractCountryId\" : "+countryId+",\n" +
-                        "  \"citizenship\" : \""+countryCode+"\"\n" +
-                        "}")
+                .body(json)
+//                .body("{\n" +
+//                        "  \"accName\" : \"TestAcc\",\n" +
+//                        "  \"accTypeId\" : \"1\",\n" +
+//                        "  \"accountContractCountryId\" : \"440\",\n" +
+//                        "  \"identifyCode\" : \"12132131231\",\n" +
+//                        "  \"stateMail\" : \"String\",\n" +
+//                        "  \"stateReg\" : \"String\",\n" +
+//                        "  \"site\" : \""+site+"\",\n" +
+//                        "  \"clientType\" : \"C\",\n" +
+//                        "  \"langId\" : 1,\n" +
+//                        "  \"companyName\" : \""+companyName+"\",\n" +
+//                        "  \"currencyId\" : "+currencyId+",\n" +
+//                        "  \"ddStatus\" : \"FDD\",\n" +
+//                        "  \"feeTariffPlanId\" : 2,\n" +
+//                        "  \"defCardProgramId\" : 1,\n" +
+//                        "  \"limitPlanId\" : 2,\n" +
+//                        "  \"operLimitPlanId\" : 1,\n" +
+//                        "  \"streetLine1Reg\" : \"Gagarina ave\",\n" +
+//                        "  \"streetLine2Reg\" : \"62\",\n" +
+//                        "  \"cityReg\" : \"Krakiv\",\n" +
+//                        "  \"zipReg\" : \"2123123\",\n" +
+//                        "  \"countryIdReg\" : "+countryId+",\n" +
+//                        "  \"registeredAddrAsMail\" : false,\n" +
+//                        "  \"streetLine1Mail\" : \"Gagarina ave\",\n" +
+//                        "  \"streetLine2Mail\" : \"62\",\n" +
+//                        "  \"cityMail\" : \"Krakiv\",\n" +
+//                        "  \"zipMail\" : \"2123123\",\n" +
+//                        "  \"countryIdMail\" : "+countryId+",\n" +
+//                        "  \"contractCountryId\" : "+countryId+",\n" +
+//                        "  \"citizenship\" : \""+countryCode+"\"\n" +
+//                        "}")
                 .when()
                 .post( "/v1/user/corpClients/createScreened")
                 .then().log().all()
