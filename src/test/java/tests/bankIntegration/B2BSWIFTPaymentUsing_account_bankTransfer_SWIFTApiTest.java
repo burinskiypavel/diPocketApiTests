1d3e4bb2c7bfcc8a0758004cf9444b69ccfb7fa5
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-03-23 15:58:48 +0200

    refactor

--- src/test/java/tests/bankIntegration/B2BSWIFTPaymentUsing_account_bankTransfer_SWIFTApiTest.java
+++ src/test/java/tests/bankIntegration/B2BSWIFTPaymentUsing_account_bankTransfer_SWIFTApiTest.java
@@ -1,16 +1,43 @@
 package tests.bankIntegration;
 
 import base.TestBase;
+import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
 
+import java.sql.SQLException;
+
 import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
 
 public class B2BSWIFTPaymentUsing_account_bankTransfer_SWIFTApiTest extends TestBase {
     String city = "Vilnius";
     String login = "APIOLENA";
     String pass = "pU9N1Lu";
+    int feeAmount = 0;
+    String currencyCode = "EUR";
+    String randomRequestId = app.generateRandomString(12);
 
-    @Test
+    @Test(priority = 1)
+    public void test_CustomerServices_v1_account_calculateBankTransfer_SWIFT(){
+        String response = given()
+                .log().uri().log().headers().log().body()
+                .contentType("application/json")
+                .auth().basic(login, pass)
+                .body("{\n" +
+                        "  \"accountId\": 115975,\n" +
+                        "  \"requestId\": \"d1f202fe-df2e-46da-94ba"+randomRequestId+"\",\n" +
+                        "  \"currencyCode\": \""+currencyCode+"\",\n" +
+                        "  \"amount\": \"10\"\n" +
+                        "}")
+                .post("https://api.dipocket.site/CustomerServices/v1/account/calculateBankTransfer/SWIFT")
+                .then().log().all()
+                .statusCode(200).extract().response().asString();
+
+        JsonPath jsonPath = new JsonPath(response);
+        feeAmount = jsonPath.getInt("feeAmount");
+    }
+
+    @Test(priority = 2)
     public void test_CustomerServices_v1_account_bankTransfer_SWIFT(){
         String response = given()
                 .log().uri().log().headers().log().body()
@@ -25,9 +52,9 @@ public class B2BSWIFTPaymentUsing_account_bankTransfer_SWIFTApiTest extends Test
                         "  \"city\": \"Vilnius\",\n" +
                         "  \"companyName\": \"Mantest\",\n" +
                         "  \"countryCode\": \"LT\",\n" +
-                        "  \"currencyCode\": \"EUR\",\n" +
-                        "  \"feeAmount\": 250,\n" +
-                        "  \"feeCurrencyCode\": \"EUR\",\n" +
+                        "  \"currencyCode\": \""+currencyCode+"\",\n" +
+                        "  \"feeAmount\": "+feeAmount+",\n" +
+                        "  \"feeCurrencyCode\": \""+currencyCode+"\",\n" +
                         "  \"firstName\": \"\",\n" +
                         "  \"lastName\": \"\",\n" +
                         "  \"reference\": \"test\",\n" +
@@ -39,6 +66,11 @@ public class B2BSWIFTPaymentUsing_account_bankTransfer_SWIFTApiTest extends Test
                 .post("https://api.dipocket.site/CustomerServices/v1/account/bankTransfer/SWIFT")
                 .then().log().all()
                 .statusCode(200).extract().response().asString();
+    }
 
+    @Test(priority = 3)
+    public void test_verifyStatusPTSFromPTS_OUT_TRAN() throws SQLException, ClassNotFoundException, InterruptedException {
+        String actualPTSStatus = app.getDbHelper().getStatusPTSFromTestDB();
+        assertEquals(actualPTSStatus, "INPRCS");
     }
 }
