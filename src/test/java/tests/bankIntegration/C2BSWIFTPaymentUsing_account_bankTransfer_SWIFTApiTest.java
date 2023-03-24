72dbae5b2371020b3ccb88318c2ae877db45ff61
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-03-24 15:45:56 +0200

    refactor

--- src/test/java/tests/bankIntegration/C2BSWIFTPaymentUsing_account_bankTransfer_SWIFTApiTest.java
+++ src/test/java/tests/bankIntegration/C2BSWIFTPaymentUsing_account_bankTransfer_SWIFTApiTest.java
@@ -1,7 +1,10 @@
 package tests.bankIntegration;
 
 import base.TestBase;
+import com.google.gson.Gson;
 import io.restassured.path.json.JsonPath;
+import model.customerServices.AccountBankTransferRequest;
+import model.customerServices.CalculateBankTransferRequest;
 import org.testng.annotations.Test;
 
 import java.sql.SQLException;
@@ -16,20 +19,24 @@ public class C2BSWIFTPaymentUsing_account_bankTransfer_SWIFTApiTest extends Test
     int feeAmount = 0;
     String currencyCode = "EUR";
     int accountId = 111231;
+    Gson gson = new Gson();
+    AccountBankTransferRequest accountBankTransferRequest = new AccountBankTransferRequest();
+    CalculateBankTransferRequest calculateBankTransferRequest = new CalculateBankTransferRequest();
 
     @Test(priority = 1)
     public void test_CustomerServices_v1_account_calculateBankTransfer_SWIFT(){
+        calculateBankTransferRequest.setAccountId(accountId);
+        calculateBankTransferRequest.setRequestId("d1f202fe-df2e-46da-94ba"+app.generateRandomString(12)+"");
+        calculateBankTransferRequest.setCurrencyCode(currencyCode);
+        calculateBankTransferRequest.setAmount(10);
+        String json = gson.toJson(calculateBankTransferRequest);
+
         String response = given()
-                .log().uri().log().headers().log().body()
+                .spec(app.requestSpecCustomerServicesTest)
                 .contentType("application/json")
                 .auth().basic(login, pass)
-                .body("{\n" +
-                        "  \"accountId\": "+accountId+",\n" +
-                        "  \"requestId\": \"d1f202fe-df2e-46da-94ba"+app.generateRandomString(12)+"\",\n" +
-                        "  \"currencyCode\": \""+currencyCode+"\",\n" +
-                        "  \"amount\": \"10\"\n" +
-                        "}")
-                .post("https://api.dipocket.site/CustomerServices/v1/account/calculateBankTransfer/SWIFT")
+                .body(json)
+                .post("/v1/account/calculateBankTransfer/SWIFT")
                 .then().log().all()
                 .statusCode(200).extract().response().asString();
 
@@ -39,31 +46,32 @@ public class C2BSWIFTPaymentUsing_account_bankTransfer_SWIFTApiTest extends Test
 
     @Test(priority = 2)
     public void test_CustomerServices_v1_account_bankTransfer_SWIFT(){
+        accountBankTransferRequest.setAccountId(accountId);
+        accountBankTransferRequest.setAmount(148);
+        accountBankTransferRequest.setBankId("LHVBEE22");
+        accountBankTransferRequest.setBeneficiaryAccount("EE407777000012064164");
+        accountBankTransferRequest.setBeneficiaryType("COMPANY");
+        accountBankTransferRequest.setCity(city);
+        accountBankTransferRequest.setCompanyName("OCORPTEST");
+        accountBankTransferRequest.setCountryCode("LT");
+        accountBankTransferRequest.setCurrencyCode(currencyCode);
+        accountBankTransferRequest.setFeeAmount(feeAmount);
+        accountBankTransferRequest.setFeeCurrencyCode(currencyCode);
+        accountBankTransferRequest.setFirstName("");
+        accountBankTransferRequest.setLastName("");
+        accountBankTransferRequest.setReference("test");
+        accountBankTransferRequest.setRequestId("d1f202fe-df2e-46da-94ba"+app.generateRandomString(12)+"");
+        accountBankTransferRequest.setStreetLine1("Upes 2");
+        accountBankTransferRequest.setStreetLine2("Upes 2");
+        accountBankTransferRequest.setZip(11111);
+        String json = gson.toJson(accountBankTransferRequest);
+
         String response = given()
-                .log().uri().log().headers().log().body()
+                .spec(app.requestSpecCustomerServicesTest)
                 .contentType("application/json")
                 .auth().basic(login, pass)
-                .body("{\n" +
-                        "  \"accountId\": "+accountId+",\n" +
-                        "  \"amount\": 148,\n" +
-                        "  \"bankId\": \"LHVBEE22\",\n" +
-                        "  \"beneficiaryAccount\": \"EE407777000012064164\",\n" +
-                        "  \"beneficiaryType\": \"COMPANY\",\n" +
-                        "  \"city\": \""+city+"\",\n" +
-                        "  \"companyName\": \"OCORPTEST\",\n" +
-                        "  \"countryCode\": \"LT\",\n" +
-                        "  \"currencyCode\": \""+currencyCode+"\",\n" +
-                        "  \"feeAmount\": "+feeAmount+",\n" +
-                        "  \"feeCurrencyCode\": \""+currencyCode+"\",\n" +
-                        "  \"firstName\": \"Pavel\",\n" +
-                        "  \"lastName\": \"Burinskiy\",\n" +
-                        "  \"reference\": \"test\",\n" +
-                        "  \"requestId\": \"d1f202fe-df2e-46da-94ba"+app.generateRandomString(12)+"\",\n" +
-                        "  \"streetLine1\": \"Upes 2\",\n" +
-                        "  \"streetLine2\": \"Upes 2\",\n" +
-                        "  \"zip\": \"09100\"\n" +
-                        "}")
-                .post("https://api.dipocket.site/CustomerServices/v1/account/bankTransfer/SWIFT")
+                .body(json)
+                .post("/v1/account/bankTransfer/SWIFT")
                 .then().log().all()
                 .statusCode(200).extract().response().asString();
     }
