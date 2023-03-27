5aa2b4f479fe5b36b846892026b4483ae929e2f1
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-03-27 19:53:15 +0300

    refactor

--- src/test/java/requests/customerServices/CustomerServicesRequests.java
+++ src/test/java/requests/customerServices/CustomerServicesRequests.java
@@ -2,6 +2,7 @@ package requests.customerServices;
 
 import appmanager.DBHelper;
 import appmanager.HelperBase;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
@@ -55,5 +56,51 @@ public class CustomerServicesRequests {
                 .body(json)
                 .post( "/v1/card/activate")
                 .then().log().all().statusCode(200);
+    }
+
+    public String customerServices_v1_account_calculateBankTransfer_SEPA_test(String authLogin, String authPass, String json){
+        String response = given()
+                .spec(requestSpecCustomerServices)
+                .baseUri(HelperBase.prop.getProperty("test.base.url"))
+                .auth().basic(authLogin, authPass)
+                .body(json)
+                .post("/v1/account/calculateBankTransfer/SEPA")
+                .then().log().all()
+                .statusCode(200).extract().response().asString();
+        return response;
+    }
+
+    public String customerServices_v1_account_calculateBankTransfer_SWIFT_test(String authLogin, String authPass, String json){
+        String response = given()
+                .spec(requestSpecCustomerServices)
+                .baseUri(HelperBase.prop.getProperty("test.base.url"))
+                .auth().basic(authLogin, authPass)
+                .body(json)
+                .post("/v1/account/calculateBankTransfer/SWIFT")
+                .then().log().all()
+                .statusCode(200).extract().response().asString();
+        return response;
+    }
+
+    public void customerServices_v1_account_bankTransfer_SEPA_test(String authLogin, String authPass, String json){
+        given()
+                .spec(requestSpecCustomerServices)
+                .baseUri(HelperBase.prop.getProperty("test.base.url"))
+                .auth().basic(authLogin, authPass)
+                .body(json)
+                .post("/v1/account/bankTransfer/SEPA")
+                .then().log().all()
+                .statusCode(200);
+    }
+
+    public void customerServices_v1_account_bankTransfer_SWIFT_test(String authLogin, String authPass, String json){
+        given()
+                .spec(requestSpecCustomerServices)
+                .baseUri(HelperBase.prop.getProperty("test.base.url"))
+                .auth().basic(authLogin, authPass)
+                .body(json)
+                .post("/v1/account/bankTransfer/SWIFT")
+                .then().log().all()
+                .statusCode(200);
     }
 }
