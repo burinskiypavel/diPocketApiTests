4783aac01d0dfdf636eb375e9f17eefd8aa86309
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-04-06 14:40:09 +0300

    refactor

--- src/test/java/tests/bo/boSearch/RolesCBOSearchByClientEmailTests.java
+++ src/test/java/tests/bo/boSearch/RolesCBOSearchByClientEmailTests.java
@@ -4,18 +4,14 @@ import base.TestBase;
 import com.cs.dipocketback.base.data.Site;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.basePath;
-import static io.restassured.RestAssured.baseURI;
-
 public class RolesCBOSearchByClientEmailTests extends TestBase {
     String cookie = null;
-    String username = "VIKTORIA";
     String phone = "447459005206";
     String email = "vikarez20@gmail.com";
 
     @Test(priority = 1)
     public void test_BOServices_v1_auth_authentication() {
-        cookie = app.getBoRequestsHelper().boServices_v1_auth_authentication(app.CBOuserLogin, app.CBOuserPass, username);
+        cookie = app.getBoRequestsHelper().boServices_v1_auth_authentication(app.CBOuserLogin2, app.CBOuserPass2, app.CBOusername2);
     }
 
     @Test(priority = 2)
