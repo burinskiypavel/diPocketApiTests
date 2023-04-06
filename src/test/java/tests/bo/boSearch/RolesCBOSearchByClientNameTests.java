2843709663910f0b4dee02a8f3e73976e3059cc1
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-04-06 14:29:18 +0300

    refactor

--- src/test/java/tests/bo/boSearch/RolesCBOSearchByClientNameTests.java
+++ src/test/java/tests/bo/boSearch/RolesCBOSearchByClientNameTests.java
@@ -10,22 +10,22 @@ import static io.restassured.RestAssured.baseURI;
 public class RolesCBOSearchByClientNameTests extends TestBase {
     String cookie = null;
     String name = "Vika";
-    String username = "VIKTORIA";
-    String phone = "380634413376";
-    String email = "vikarezznik60@gmail.com";
+    String phone = "380980316499";
+    String phone2 = "380634413376";
+    String email = "pavelburinskiy@gmail.com";
 
     @Test(priority = 1)
     public void test_BOServices_v1_auth_authentication() {
-        cookie = app.getBoRequestsHelper().boServices_v1_auth_authentication(app.CBOuserLogin, app.CBOuserPass, username);
+        cookie = app.getBoRequestsHelper().boServices_v1_auth_authentication(app.CBOuserLogin2, app.CBOuserPass2, app.CBOusername2);
     }
 
     @Test(priority = 2)
     public void test_BOServices_v1_user_authenticated(){
-        app.getBoRequestsHelper().boServices_v1_user_authenticated(cookie, username, phone, email);
+        app.getBoRequestsHelper().boServices_v1_user_authenticated(cookie, app.CBOusername2, phone, email);
     }
 
     @Test(priority = 3)
     public void test_BOServices_v1_client_search(){
-        app.getBoRequestsHelper().boServices_v1_client_search(cookie, "firstName",  name, "Vika", "Qwerty", phone, "vikarezznik60@gmail.com", Site.PLAYIT.toString());
+        app.getBoRequestsHelper().boServices_v1_client_search(cookie, "firstName",  name, "Vika", "Qwerty", phone2, "vikarezznik60@gmail.com", Site.PLAYIT.toString());
     }
 }
