e8334785b296b208a882000012d02f0904d6b690
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-04-07 13:40:32 +0300

    refactor

--- src/test/java/appmanager/DBHelper.java
+++ src/test/java/appmanager/DBHelper.java
@@ -849,7 +849,7 @@ public class DBHelper extends HelperBase {
             massegeForPayee = rs.getString(30);
 
 
-            while (statusPts == null && count < 220){
+            while (statusPts == null && count < 160){
                 Thread.sleep(6000);
                 rs= stmt.executeQuery(query);
                 rs.next();
