bd266175d08b5a4dc2aece4be497e07ad7c9b26c
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-04-22 23:56:00 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageTest.java
+++ src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageTest.java
@@ -7,6 +7,8 @@ import org.testng.asserts.SoftAssert;
 
 public class RolesBOUserClientPageTest extends UITestBase {
     SoftAssert softAssert = new SoftAssert();
+    String clientId = "33217";
+    String phone = "380634413376";
 
     @Test
     public void testRolesBOUserClientPage() throws InterruptedException {
@@ -21,19 +23,54 @@ public class RolesBOUserClientPageTest extends UITestBase {
                 "//app-input[@ng-reflect-name='firstName']", "//app-input[@ng-reflect-name='lastName']", "//p-calendar //input[@type='text']",
         "//app-input[@ng-reflect-name='mailingAddress']", "//app-input[@ng-reflect-name='companyName']"}));
 
-        app.getUiboHelper().type(By.cssSelector("app-input-number[ng-reflect-name='id'] input.p-inputnumber-input"), "33217");
-        app.getUiboHelper().waitFor(By.cssSelector("td[ng-reflect-text='380634413376']"));
+        app.getUiboHelper().type(By.cssSelector("input[id*='input_id']"), clientId);
+        app.getUiboHelper().waitFor(By.cssSelector("td span[ng-reflect-text='380634413376']"));
 
-        softAssert.assertFalse(!app.getUiboHelper().isElementPresent(By.cssSelector("td[ng-reflect-text='380634413376']")));
-        softAssert.assertFalse(!app.getUiboHelper().isElementPresent(By.cssSelector("td[ng-reflect-text='vikarez20@gmail.com']")));
+        softAssert.assertFalse(!app.getUiboHelper().isElementPresent(By.cssSelector("td span[ng-reflect-text='380634413376']")));
+        softAssert.assertFalse(!app.getUiboHelper().isElementPresent(By.cssSelector("td span[ng-reflect-text='vikarez20@gmail.com']")));
 
-        app.getUiboHelper().click(By.cssSelector("td[ng-reflect-text='380634413376']"));
+        app.getUiboHelper().click(By.cssSelector("td span[ng-reflect-text='380634413376']"));
         app.getUiboHelper().waitFor(By.cssSelector("p.user-name"));
 
 
+
+
+//        String actualUsername = app.getUiboHelper().getText(By.cssSelector("p.user-name"));
+//        String actualEmail = app.getUiboHelper().getText(By.cssSelector("p.email"));
+//
+//
+//        String actualState = app.getUiboHelper().getText(By.xpath("//*[contains(text(), 'State:')]"));
+//        String actualSite = app.getUiboHelper().getText(By.xpath("//*[contains(text(), 'Site:')]"));
+//
+//        String actualRisk = app.getUiboHelper().getText(By.xpath("//*[contains(text(), 'Risk')]"));
+//        String actualRegistryAdd = app.getUiboHelper().getText(By.xpath("//*[contains(text(), 'Registry add:')]"));
+//        String actualMailingAdd = app.getUiboHelper().getText(By.xpath("//*[contains(text(), 'Mailing add:')]"));
+//
+//        softAssert.assertFalse(!app.getUiboHelper().areElementsPresent(new String[]{
+//                "//*[contains(text(), '06.10.1976')]", "//*[contains(text(), '380634413376')]", "//*[contains(text(), '33217')]",
+//                "//*[contains(text(), 'FDD')]", "//*[contains(text(), 'EUR - standart')]", "//*[contains(text(), 'EUR')]",
+//                "//*[contains(text(), 'Ukrainian')]",}));
+//
+//        softAssert.assertEquals(actualUsername, "Nona Qwerty");
+//        softAssert.assertEquals(actualEmail, "vikarez20@gmail.com (verified)");
+//        softAssert.assertEquals(actualState, "State: Active");
+//        softAssert.assertEquals(actualSite, "Site: DIPOCKET");
+//        softAssert.assertEquals(actualRisk, "Risk: 3");
+//        softAssert.assertEquals(actualRegistryAdd, "Registry add: Address44, City, UA");
+//        softAssert.assertEquals(actualMailingAdd, "Mailing add: Address, City, AT");
+
+        softAssert.assertAll();
+    }
+
+    @Test
+    public void testRolesBOUserVerifyClientInfoOnClientPage() throws InterruptedException {
+        app.getUiboHelper().gotoBOSiteAndLoginWithBOUserRole(app.BOuserLogin, app.BOuserPass);
+        app.getUiboHelper().gotoSearchPage();
+        app.getUiboHelper().search("id", clientId, phone);
+        app.getUiboClientHelper().goToClientPage(phone);
+
         String actualUsername = app.getUiboHelper().getText(By.cssSelector("p.user-name"));
         String actualEmail = app.getUiboHelper().getText(By.cssSelector("p.email"));
-
 
         String actualState = app.getUiboHelper().getText(By.xpath("//*[contains(text(), 'State:')]"));
         String actualSite = app.getUiboHelper().getText(By.xpath("//*[contains(text(), 'Site:')]"));
@@ -54,17 +91,25 @@ public class RolesBOUserClientPageTest extends UITestBase {
         softAssert.assertEquals(actualRisk, "Risk: 3");
         softAssert.assertEquals(actualRegistryAdd, "Registry add: Address44, City, UA");
         softAssert.assertEquals(actualMailingAdd, "Mailing add: Address, City, AT");
+        softAssert.assertAll();
+    }
 
+    @Test
+    public void testRolesBOUserVerifyButtonsAndTabsOnClientPage() throws InterruptedException {
+        app.getUiboHelper().gotoBOSiteAndLoginWithBOUserRole(app.BOuserLogin, app.BOuserPass);
+        app.getUiboHelper().gotoSearchPage();
+        app.getUiboHelper().search("id", clientId, phone);
+        app.getUiboClientHelper().goToClientPage(phone);
 
         softAssert.assertFalse(!app.getUiboHelper().areElementsPresent(new String[]{
                 "//a[@role='tab'] //span[contains(text(), 'Tiles')]", "//a[@role='tab'] //span[contains(text(), 'Messages')]", "//a[@role='tab'] //span[contains(text(), 'Client iban')]",
                 "//a[@role='tab'] //span[contains(text(), 'Payee')]", "//a[@role='tab'] //span[contains(text(), 'Selfie')]", "//a[@role='tab'] //span[contains(text(), 'Docs')]",
                 "//a[@role='tab'] //span[contains(text(), 'Accounts')]", "//a[@role='tab'] //span[contains(text(), '3rd party cards')]", "//a[@role='tab'] //span[contains(text(), 'Transaction')]",
-                "//a[@role='tab'] //span[contains(text(), 'Tickets')]", "//a[@role='tab'] //span[contains(text(), 'Supervisor requests')]"}));
+                "//a[@role='tab'] //span[contains(text(), 'Tickets')]", "//a[@role='tab'] //span[contains(text(), 'Supervisor requests')]"}), "Tabs incorrect");
 
-        softAssert.assertFalse(!app.getUiboHelper().areButtonsPresent(new String[]{"//app-button[@ng-reflect-label='Search']", "//app-button[@ng-reflect-label='Block client']", "//app-button[@ng-reflect-label='Ban client']",
-                    "//app-button[@ng-reflect-label='Forget client']", "//app-button[@ng-reflect-label='Change credentials']", "//app-button[@ng-reflect-label='Send statements']",
-                    "//app-button[@ng-reflect-label='Upload docs']", "//app-button[@ng-reflect-label='Upload selfies']", "//app-button[@ng-reflect-label='Transfer back']"}));
+        softAssert.assertFalse(!app.getUiboHelper().areButtonsPresent(new String[]{"//p-button[@ng-reflect-label='Search']", "//p-button[@ng-reflect-label='Block client']", "//p-button[@ng-reflect-label='Ban client']",
+                "//p-button[@ng-reflect-label='Forget client']", "//p-button[@ng-reflect-label='Change credentials']", "//p-button[@ng-reflect-label='Send statements']",
+                "//p-button[@ng-reflect-label='Upload docs']", "//p-button[@ng-reflect-label='Upload selfies']", "//p-button[@ng-reflect-label='Transfer back']"}), "Buttons incorrect");
         softAssert.assertAll();
     }
 }
