018212a7bb8f3cc6d1e44fabee18269ba16ae949
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-05-08 12:14:01 +0300

    refactor

--- src/test/java/appmanager/ApplicationManager.java
+++ src/test/java/appmanager/ApplicationManager.java
@@ -307,13 +307,6 @@ public class ApplicationManager {
         RestAssured.useRelaxedHTTPSValidation();
     }
 
-//    public RestAssuredConfig aspspSslConfig = RestAssuredConfig.config().sslConfig(
-//            SSLConfig.sslConfig()
-//                    .trustStore("files/certs/truststoreSandboxCompany.jks", "123456").trustStoreType("JKS")
-//                    .keyStore("files/certs/client_created.p12", "123456").keystoreType("PKCS12")
-//                    .allowAllHostnames()
-//    );
-
     public void closeDriver() {
         driver.quit();
     }
