34263b815de55da5ea9075f8cdbc77cfee084372
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-05-15 18:30:53 +0300

    refactor

--- src/test/java/tests/bo/boClient/RolesBOUserClientPageBlockUnblockBanUnbanClientTests.java
+++ src/test/java/tests/bo/boClient/RolesBOUserClientPageBlockUnblockBanUnbanClientTests.java
@@ -18,39 +18,39 @@ public class RolesBOUserClientPageBlockUnblockBanUnbanClientTests extends TestBa
     String email = "testdipocket2@gmail.com";
 
     @Test(priority = 1)
-    public void test_BOServices_v1_auth_authentication(){
-        cookie = app.getBoRequestsHelper().boServices_v1_auth_authentication(app.CBOuserLogin, app.CBOuserPass, "VIKTORIA");
+    public void test_BOServices_v1_auth_authentication() {
+        cookie = app.getBoRequestsHelper().boServices_v1_auth_authentication(app.CBOuserLogin, app.CBOuserPass, app.CBOusername);
     }
 
     @Test(priority = 2)
-    public void test_BOServices_v1_client_states(){
+    public void test_BOServices_v1_client_states() {
         app.getBoRequestsHelper().boServices_v1_client_states(cookie);
     }
 
     @Test(priority = 3)
-    public void test_BOServices_v1_client_sites(){
+    public void test_BOServices_v1_client_sites() {
         given()
                 .spec(app.requestSpecBO)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/client/sites")
+                .get("/v1/client/sites")
                 .then().log().all()
                 .body("site", hasItems(Site.DIPOCKET.toString(), Site.GETSBY.toString(), Site.PEAK.toString(), Site.PLAYIT.toString(), Site.SODEXO.toString()),
                         "name", hasItems("Salarify", "Sandbox", "Nexo", "Figame"));
     }
 
     @Test(priority = 4)
-    public void test_BOServices_v1_client_search(){
+    public void test_BOServices_v1_client_search() {
         app.getBoRequestsHelper().boServices_v1_client_search(cookie, clientId, firstName, lastName, phone, email, Site.SODEXO.toString());
     }
 
     @Test(priority = 5)
-    public void test_BOServices_v1_supervisor_34138_reqList(){
+    public void test_BOServices_v1_supervisor_clientId_reqList() {
         Response res = app.getBoRequestsHelper().boServices_v1_supervisor_reqList(cookie, clientId);
     }
 
     @Test(priority = 6)
-    public void test_BOServices_v1_client_34138(){
+    public void test_BOServices_v1_client_clientId() {
         Response res = app.getBoRequestsHelper().boServices_v1_client_(cookie, clientId);
         res.then().body("mainPhone", equalTo(phone),
                 "firstName", equalTo(firstName),
@@ -59,73 +59,74 @@ public class RolesBOUserClientPageBlockUnblockBanUnbanClientTests extends TestBa
     }
 
     @Test(priority = 7)
-    public void test_BOServices_v1_user_all(){
-                given()
+    public void test_BOServices_v1_user_all() {
+        given()
                 .cookie(cookie)
                 .spec(app.requestSpecBO)
                 .when()
-                .get( "/v1/user/all")
+                .get("/v1/user/all")
                 .then().log().all()
                 .statusCode(200)
-                .body("firstName", hasItem("Agnieszka"),
-                        "lastName", hasItem("Szewczyk"),
-                        "roleId", hasItem("PORTAL"),
+                .body("firstName", hasItem("Olga"),
+                        "lastName", hasItem("Gocałek-Hepner"),
+                        "roleId", hasItem("EXTBO"),
                         "stateId", hasItem(10),
                         "stateName", hasItem("Active"),
-                        "username", hasItem("A.SZEWCZYK"),
-                        "phone", hasItem("48663647283"),
-                        "email", hasItem("Agnieszka.Szewczyk@sodexo.com"),
-                        "site", hasItem(Site.DIPOCKET.toString()));
+                        "username", hasItem("OLGA.GOCALEK"),
+                        "phone", hasItem("48508055604"),
+                        "email", hasItem("Olga.GOCALEK-HEPNER@sodexo.com"),
+                        "site", hasItem(Site.SODEXO.toString()));
     }
 
     @Test(priority = 8)
-    public void test_BOServices_v1_ticket_states(){
+    public void test_BOServices_v1_ticket_states() {
         app.getBoRequestsHelper().boServices_v1_ticket_states(cookie);
     }
 
     @Test(priority = 9)
-    public void test_BOServices_v1_ticket_types(){
+    public void test_BOServices_v1_ticket_types() {
         app.getBoRequestsHelper().boServices_v1_ticket_types(cookie);
     }
 
     @Test(priority = 10)
-    public void test_BOServices_v1_clientImage_34138_selfie(){
+    public void test_BOServices_v1_clientImage_clientId_selfie() {
         Response res = given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/clientImage/"+clientId+"/selfie");
+                .get("/v1/clientImage/{clientId}/selfie");
         res.then().log().all().statusCode(200);
     }
 
     @Test(priority = 11)
-    public void test_BOServices_v1_tran_states(){
+    public void test_BOServices_v1_tran_states() {
         app.getBoRequestsHelper().boServices_v1_tran_states(cookie);
     }
 
     @Test(priority = 12)
-    public void test_BOServices_v1_dashboard_tranTypes(){
+    public void test_BOServices_v1_dashboard_tranTypes() {
         given()
                 .spec(app.requestSpecBO)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/dashboard/tranTypes")
+                .get("/v1/dashboard/tranTypes")
                 .then().log().all()
                 .statusCode(200).body("", hasItems("Balance Check at ATM", "Card transaction", "Direct Debit", "Reversed Crowd-Payment", "Top Up"));
     }
 
     @Test(priority = 13)
-    public void test_BOServices_v1_account_other_client_34138(){
+    public void test_BOServices_v1_account_other_client_clientId() {
         app.getBoRequestsHelper().boServices_v1_account_other_client_(cookie, clientId);
     }
 
     @Test(priority = 14)
-    public void test_BOServices_v1_account_states(){
+    public void test_BOServices_v1_account_states() {
         given()
                 .spec(app.requestSpecBO)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/account/states")
+                .get("/v1/account/states")
                 .then().log().all()
                 .statusCode(200)
                 .body("id", hasItems(20, 10, 5, 0, -50, -100),
@@ -133,36 +134,37 @@ public class RolesBOUserClientPageBlockUnblockBanUnbanClientTests extends TestBa
     }
 
     @Test(priority = 15)
-    public void test_BOServices_v1_account_client_34138(){
+    public void test_BOServices_v1_account_client_clientId() {
         Response res = app.getBoRequestsHelper().boServices_v1_account_client_(cookie, clientId);
         res.then().body("accountName", hasItems("Main Account"),
                 "clientId", hasItems(clientId));
     }
 
     @Test(priority = 16)
-    public void test_BOServices_v1_clientImage_34138_docs(){
+    public void test_BOServices_v1_clientImage_clientId_docs() {
         Response res = given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/clientImage/"+clientId+"/docs");
+                .get("/v1/clientImage/{clientId}/docs");
         res.then().log().all().statusCode(200);
     }
 
     @Test(priority = 17)
-    public void test_BOServices_v1_client_34138_paymentDetails(){
+    public void test_BOServices_v1_client_clientId_paymentDetails() {
         Response res = app.getBoRequestsHelper().boServices_v1_client_paymentDetails(cookie, clientId);
         res.then().body("accountName", hasItems("Pavel auto QA"),
                 "currencyCode", hasItems("PLN"));
     }
 
     @Test(priority = 18)
-    public void test_BOServices_v1_payee_paymentTypes(){
+    public void test_BOServices_v1_payee_paymentTypes() {
         given()
                 .spec(app.requestSpecBO)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/payee/paymentTypes")
+                .get("/v1/payee/paymentTypes")
                 .then().log().all()
                 .statusCode(200)
                 .body("id", hasItems(4, 1, 2, 3, 5),
@@ -170,44 +172,47 @@ public class RolesBOUserClientPageBlockUnblockBanUnbanClientTests extends TestBa
     }
 
     @Test(priority = 19)
-    public void test_BOServices_v1_payee_34138(){
+    public void test_BOServices_v1_payee_clientId() {
         given()
                 .spec(app.requestSpecBO)
                 .cookie(cookie)
+                .pathParam("clientId", clientId)
                 .when()
-                .get( "/v1/payee/"+clientId+"")
+                .get("/v1/payee/{clientId}")
                 .then().log().all()
                 .statusCode(200);
     }
 
     @Test(priority = 20)
-    public void test_BOServices_v1_client_availCurrencies(){
+    public void test_BOServices_v1_client_availCurrencies() {
         app.getBoRequestsHelper().boServices_v1_client_availCurrencies(cookie);
     }
 
     @Test(priority = 21)
-    public void test_BOServices_v1_client_34138_tiles(){
+    public void test_BOServices_v1_client_clientId_tiles() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/client/"+clientId+"/tiles")
+                .get("/v1/client/{clientId}/tiles")
                 .then().log().all()
                 .statusCode(200);
     }
 
     @Test(priority = 22)
-    public void test_BOServices_v1_client_34138_address(){
+    public void test_BOServices_v1_client_34138_address() {
         app.getBoRequestsHelper().boServices_v1_client_address(cookie, clientId);
     }
 
     @Test(priority = 23)
-    public void test_BOServices_v1_ticket_client_34138(){
+    public void test_BOServices_v1_ticket_client_clientId() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/ticket/client/"+clientId+"")
+                .get("/v1/ticket/client/{clientId}")
                 .then().log().all()
                 .statusCode(200)
                 .body("id", hasItem(29232),
@@ -228,12 +233,12 @@ public class RolesBOUserClientPageBlockUnblockBanUnbanClientTests extends TestBa
     }
 
     @Test(priority = 24)
-    public void test_BOServices_v1_client_citizenships(){
+    public void test_BOServices_v1_client_citizenships() {
         given()
                 .spec(app.requestSpecBO)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/client/citizenships")
+                .get("/v1/client/citizenships")
                 .then().log().all()
                 .statusCode(200)
                 .body("id", hasItems(4, 248, 8, 32),
@@ -242,28 +247,30 @@ public class RolesBOUserClientPageBlockUnblockBanUnbanClientTests extends TestBa
     }
 
     @Test(priority = 25)
-    public void test_BOServices_v1_client_34138_pushMsgs(){
-                given()
+    public void test_BOServices_v1_client_clientId_pushMsgs() {
+        given()
                 .cookie(cookie)
+                .pathParam("clientId", clientId)
                 .spec(app.requestSpecBO)
                 .when()
-                .get( "/v1/client/"+clientId+"/pushMsgs")
+                .get("/v1/client/{clientId}/pushMsgs")
                 .then().log().all()
                 .statusCode(200);
     }
 
     @Test(priority = 26)
-    public void test_BOServices_v1_clientImage_34138_docHistory(){
+    public void test_BOServices_v1_clientImage_clientId_docHistory() {
         app.getBoRequestsHelper().boServices_v1_clientImage_docHistory(cookie, clientId);
     }
 
     @Test(priority = 27, enabled = false)
-    public void test_BOServices_v1_clientImage_34138_selfieHistory(){
+    public void test_BOServices_v1_clientImage_clientId_selfieHistory() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/clientImage/"+clientId+"/selfieHistory")
+                .get("/v1/clientImage/{clientId}/selfieHistory")
                 .then().log().all()
                 .statusCode(200)
                 .body("id", hasItem(17159),
@@ -276,154 +283,166 @@ public class RolesBOUserClientPageBlockUnblockBanUnbanClientTests extends TestBa
     }
 
     @Test(priority = 28)
-    public void test_BOServices_v1_client_34138_block(){
+    public void test_BOServices_v1_client_clientId_block() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .body("{\n" +
-                        "  \"blockOrBanReason\" : \""+reason+"\"\n" +
+                        "  \"blockOrBanReason\" : \"" + reason + "\"\n" +
                         "}")
                 .when()
-                .post( "/v1/client/"+clientId+"/block")
+                .post("/v1/client/{clientId}/block")
                 .then().log().all()
                 .statusCode(200);
     }
 
     @Test(priority = 29)
-    public void test_BOServices_v1_client_34138_(){
+    public void test_BOServices_v1_client_clientId_() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/client/"+ clientId +"")
+                .get("/v1/client/{clientId}")
                 .then().log().all()
                 .statusCode(200)
                 .body("stateName", equalTo("Blocked"));
     }
 
     @Test(priority = 30)
-    public void test_BOServices_v1_client_34138_unblock() {
+    public void test_BOServices_v1_client_clientId_unblock() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .body("{\n" +
-                        "  \"unblockOrUnbanReason\" : \""+reason+"\"\n" +
+                        "  \"unblockOrUnbanReason\" : \"" + reason + "\"\n" +
                         "}")
                 .when()
-                .post("/v1/client/" + clientId + "/unblock")
+                .post("/v1/client/{clientId}/unblock")
                 .then().log().all()
                 .statusCode(200);
     }
 
     @Test(priority = 31)
-    public void test_BOServices_v1_client_34138__(){
+    public void test_BOServices_v1_client_clientId__() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/client/"+ clientId +"")
+                .get("/v1/client/{clientId}")
                 .then().log().all()
                 .statusCode(200)
                 .body("stateName", equalTo("Active"));
     }
 
     @Test(priority = 32)
-    public void test_BOServices_v1_client_34138_ban_WithoutBlockingClientDevice(){
+    public void test_BOServices_v1_client_clientId_ban_WithoutBlockingClientDevice() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .body("{\n" +
-                        "  \"blockOrBanReason\" : \""+reason+"\",\n" +
+                        "  \"blockOrBanReason\" : \"" + reason + "\",\n" +
                         "  \"blockClientDevice\" : false\n" +
                         "}")
                 .when()
-                .post( "/v1/client/"+clientId+"/ban")
+                .post("/v1/client/{clientId}/ban")
                 .then().log().all()
                 .statusCode(200);
     }
 
     @Test(priority = 33)
-    public void test_BOServices_v1_client_34138___(){
+    public void test_BOServices_v1_client_clientId___() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/client/"+ clientId +"")
+                .get("/v1/client/{clientId}")
                 .then().log().all()
                 .statusCode(200)
                 .body("stateName", equalTo("Banned"));
     }
 
     @Test(priority = 34)
-    public void test_BOServices_v1_client_34138_unban(){
+    public void test_BOServices_v1_client_clientId_unban() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .body("{}")
                 .when()
-                .post( "/v1/client/"+clientId+"/unban")
+                .post("/v1/client/{clientId}/unban")
                 .then().log().all()
                 .statusCode(200);
     }
 
     @Test(priority = 35)
-    public void test_BOServices_v1_client_34138____(){
+    public void test_BOServices_v1_client_clientId____() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/client/"+ clientId +"")
+                .get("/v1/client/{clientId}")
                 .then().log().all()
                 .statusCode(200)
                 .body("stateName", equalTo("Active"));
     }
 
     @Test(priority = 36)
-    public void test_BOServices_v1_client_34138_ban(){
+    public void test_BOServices_v1_client_clientId_ban() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .body("{\n" +
-                        "  \"blockOrBanReason\" : \""+reason+"\",\n" +
+                        "  \"blockOrBanReason\" : \"" + reason + "\",\n" +
                         "  \"blockClientDevice\" : true\n" +
                         "}")
                 .when()
-                .post( "/v1/client/"+clientId+"/ban")
+                .post("/v1/client/{clientId}/ban")
                 .then().log().all()
                 .statusCode(200);
     }
 
     @Test(priority = 37)
-    public void test_BOServices_v1_client_34138_____(){
+    public void test_BOServices_v1_client_clientId_____() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/client/"+ clientId +"")
+                .get("/v1/client/{clientId}")
                 .then().log().all()
                 .statusCode(200)
                 .body("stateName", equalTo("Banned"));
     }
 
     @Test(priority = 38)
-    public void test_BOServices_v1_client_34138_unban_(){
+    public void test_BOServices_v1_client_clientId_unban_() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .body("{}")
                 .when()
-                .post( "/v1/client/"+clientId+"/unban")
+                .post("/v1/client/{clientId}/unban")
                 .then().log().all()
                 .statusCode(200);
     }
 
     @Test(priority = 39)
-    public void test_BOServices_v1_client_34138______(){
+    public void test_BOServices_v1_client_clientId______() {
         given()
                 .spec(app.requestSpecBO)
+                .pathParam("clientId", clientId)
                 .cookie(cookie)
                 .when()
-                .get( "/v1/client/"+ clientId +"")
+                .get("/v1/client/{clientId}")
                 .then().log().all()
                 .statusCode(200)
                 .body("stateName", equalTo("Active"));
