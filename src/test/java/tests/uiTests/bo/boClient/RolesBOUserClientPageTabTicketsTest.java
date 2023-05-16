4cef418e714f3d2b99b049be2364b243e4b94ee9
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-05-16 22:42:17 +0300

    refactor

--- src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageTabTicketsTest.java
+++ src/test/java/tests/uiTests/bo/boClient/RolesBOUserClientPageTabTicketsTest.java
@@ -25,15 +25,17 @@ public class RolesBOUserClientPageTabTicketsTest extends UITestBase {
                 "//thead //th[contains(text(), 'Ticket State')]", "//thead //th[contains(text(), 'Closed')]",
                 "//thead //th[contains(text(), 'Last comment')]"}));
 
-
-        app.getUiboClientHelper().verifyClientPageFilter(By.cssSelector("app-tickets-tab p-columnfilter[ng-reflect-field='id'] input[type='text']"), "25596", "id", By.cssSelector("td[ng-reflect-text='25596']"));
-        app.getUiboClientHelper().verifyClientPageFilter(By.cssSelector("p-columnfilter[ng-reflect-field='created'] input[type='text']"), "02.02.2022", "created", By.cssSelector("td[ng-reflect-text='02.02.2022']"));
-        app.getUiboClientHelper().verifyDropDownClientPageFilter(By.cssSelector("p-columnfilter[ng-reflect-field='typeName']"), "FDD check");
+        app.getUiboClientHelper().verifyClientPageFilter(By.cssSelector("app-client-tickets p-columnfilter[ng-reflect-field='id'] input"), "25596", "id", By.xpath("//td[contains(text(), '25596')]"));
+        app.getUiboClientHelper().verifyClientPageFilter(By.cssSelector("app-client-tickets p-columnfilter[ng-reflect-field='created'] input[type='text']"), "2022-09-07", "created", By.cssSelector("td span[ng-reflect-text='2022-09-07']"));
+        app.getUiboClientHelper().verifyDropDownClientPageFilter(By.cssSelector("p-columnfilter[ng-reflect-field='typeName']"), "Client restriction");
         app.getUiboClientHelper().verifyDropDownClientPageFilter(By.cssSelector("p-columnfilter[ng-reflect-field='username'] "), "EVGENYA");
-        app.getUiboClientHelper().verifyDropDownClientPageFilter(By.cssSelector("app-tickets-tab p-columnfilter[ng-reflect-field='stateName'] "), "Closed");
-        app.getUiboClientHelper().verifyClientPageFilter(By.cssSelector("p-columnfilter[ng-reflect-field='closed'] input[type='text']"), "07.02.2022", "closed", By.cssSelector("td[ng-reflect-text='07.02.2022']"));
+        app.getUiboClientHelper().verifyDropDownClientPageFilter(By.cssSelector("app-client-tickets p-columnfilter[ng-reflect-field='stateName']"), "Closed");
+        app.getUiboClientHelper().verifyClientPageFilter(By.cssSelector("p-columnfilter[ng-reflect-field='closed'] input"), "2022-09-07", "closed", By.cssSelector("td span[ng-reflect-text='2022-09-07']"));
 
-        app.getUiboClientHelper().setClientPageFilter(By.cssSelector("app-tickets-tab p-columnfilter[ng-reflect-field='lastMessage'] input[type='text']"), "Ticket reassigned. Reason: test");
-        assertTrue(app.getUiboHelper().areElementsPresentAfterSorting(By.xpath("//td[contains(text(), 'Ticket reassigned. Reason: test')]")));
+        //app.getUiboClientHelper().setClientPageFilter(By.cssSelector("app-client-tickets p-columnfilter[ng-reflect-field='lastMessage'] input"), "Ticket reassigned. Reason: test");
+        //assertTrue(app.getUiboHelper().areElementsPresentAfterSorting(By.xpath("//td //span[contains(text(), 'Ticket reassigned. Reason: test')]")));
+
+        app.getUiboClientHelper().verifyClientPageFilter(By.cssSelector("app-client-tickets p-columnfilter[ng-reflect-field='lastMessage'] input"), "Ticket reassigned. Reason: test", "lastMessage", By.xpath("//td //span[contains(text(), 'Ticket reassigned. Reason: test')]"));
+
     }
 }
