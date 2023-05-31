ee972fa6badb23d9afe8235ada27b1dc2fcd627c
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-05-31 15:13:44 +0300

    refactor

--- src/test/java/tests/bo/boTicket/TheUserNeedsToReassignTheSelfieChangeTicketToAnotherBOUserTest.java
+++ src/test/java/tests/bo/boTicket/TheUserNeedsToReassignTheSelfieChangeTicketToAnotherBOUserTest.java
@@ -41,31 +41,32 @@ public class TheUserNeedsToReassignTheSelfieChangeTicketToAnotherBOUserTest exte
 
     @Test(priority = 4)
     public void test_BOServices_v1_ticket_take() {
-        for(int i = 0; i < 12; i++) {
-            Response res = app.getBoRequestsHelper().boServices_v1_ticket_take(cookie);
-            String response = res.then().extract().response().asString();
-
-            JsonPath js = new JsonPath(response);
-            ticketId = js.getInt("id");
-            actualTypeName = js.getString("typeName");
-
-            if(actualTypeName.equals("Selfie change")){
-                break;
-            }
-
-            if(!actualTypeName.equals("Selfie change")){
-                app.getBoRequestsHelper().boServices_v1_ticket_ticketId_postpone(cookie, ticketId, tomorrow);
-            }
-
-            Response res2 = app.getBoRequestsHelper().boServices_v1_ticket_take(cookie);
-            String response2 = res2.then().extract().response().asString();
-
-            JsonPath js2 = new JsonPath(response2);
-            ticketId = js2.getInt("id");
-            actualTypeName = js2.getString("typeName");
-        }
-
-        assertEquals(actualTypeName, "Selfie change");
+        ticketId = app.getBOHelper().takeSelfieChangeTicket_dev(cookie, tomorrow);
+//        for(int i = 0; i < 12; i++) {
+//            Response res = app.getBoRequestsHelper().boServices_v1_ticket_take(cookie);
+//            String response = res.then().extract().response().asString();
+//
+//            JsonPath js = new JsonPath(response);
+//            ticketId = js.getInt("id");
+//            actualTypeName = js.getString("typeName");
+//
+//            if(actualTypeName.equals("Selfie change")){
+//                break;
+//            }
+//
+//            if(!actualTypeName.equals("Selfie change")){
+//                app.getBoRequestsHelper().boServices_v1_ticket_ticketId_postpone(cookie, ticketId, tomorrow);
+//            }
+//
+//            Response res2 = app.getBoRequestsHelper().boServices_v1_ticket_take(cookie);
+//            String response2 = res2.then().extract().response().asString();
+//
+//            JsonPath js2 = new JsonPath(response2);
+//            ticketId = js2.getInt("id");
+//            actualTypeName = js2.getString("typeName");
+//        }
+//
+//        assertEquals(actualTypeName, "Selfie change");
     }
 
     @Test(priority = 5)
