91fd154810c219f1830ceb0a0a3ac10d9f5d7c5b
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-06-05 13:36:15 +0300

    refactor

--- src/test/java/tests/bankIntegration/aspspApi/pis/PisTests.java
+++ src/test/java/tests/bankIntegration/aspspApi/pis/PisTests.java
@@ -10,7 +10,8 @@ import static org.hamcrest.Matchers.notNullValue;
 
 public class PisTests extends APIUITestBase {
     String partnerId = "654321";
-    String paymentId = null;
+    String actualPaymentId = null;
+    String paymentId = "94647f10-c4d6-4009-84dd-dcbf49a082e3";
 
     @Test(priority = 1)
     public void test_pisGetPaymentRequest() {
@@ -18,8 +19,9 @@ public class PisTests extends APIUITestBase {
                 .log().uri().log().headers().log().body()
                 .config(app.getSSLCertHelper().aspspSslConfig)
                 .pathParam("partnerId", partnerId)
+                .pathParam("paymentId", paymentId)
                 .header("X-Request-ID", "ded9c406-b701-4963-9b68-5d8d7a2b3041")
-                .get("https://openbanking.dipocket.site:3443/{partnerId}/bg/v1/payments/sepa-credit-transfers/94647f10-c4d6-4009-84dd-dcbf49a082e3");
+                .get("https://openbanking.dipocket.site:3443/{partnerId}/bg/v1/payments/sepa-credit-transfers/{paymentId}");
 
         String stResponse = response.then().log().all()
                 .statusCode(200).extract().response().asString();
@@ -34,6 +36,20 @@ public class PisTests extends APIUITestBase {
                         "paymentDetails.creditorType", equalTo("INDIVIDUAL"),
                         "paymentDetails.remittanceInformationUnstructured", equalTo("domestic transfer test DPB-453 retest v1"));
 
-        paymentId = app.getResponseValidationHelper().getStringFromResponseJsonPath(stResponse, "paymentId");
+        actualPaymentId = app.getResponseValidationHelper().getStringFromResponseJsonPath(stResponse, "paymentId");
+    }
+
+    @Test(priority = 2)
+    public void test_PISGetPaymentRequestStatus(){
+        given()
+                .log().uri().log().headers().log().body()
+                .config(app.getSSLCertHelper().aspspSslConfig)
+                .pathParam("partnerId", partnerId)
+                .pathParam("paymentId", paymentId)
+                .header("X-Request-ID", "ded9c406-b701-4963-9b68-5d8d7a2b3041")
+                .get("https://openbanking.dipocket.site:3443/{partnerId}/bg/v1/payments/sepa-credit-transfers/{paymentId}/status")
+                .then().log().all()
+                .statusCode(200)
+                .body("transactionStatus", equalTo("ACSC"));
     }
 }
