cd9fba2e74ab7ec73686be440958dd28cdd965ad
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-06-27 14:56:23 +0300

    refactor

--- src/test/java/tests/bo/boTicket/ApproveSupervisorTest.java
+++ src/test/java/tests/bo/boTicket/ApproveSupervisorTest.java
@@ -17,16 +17,12 @@ import static org.hamcrest.CoreMatchers.hasItem;
 
 public class ApproveSupervisorTest extends TestBase {
     String cliSessionId = null;
-    String regPhone = "380685448615";
-    String regPass = "pasword1";
     String cookie = null;
     String username = app.BOusername;
     int ticketId = 0;
-    String actualTypeName = null;
     String childId =  null;
     int supervisorId = app.homePageClientId;
     String tomorrow = null;
-    String parentPhone = app.homePageLoginPhone;
     String parentPass = app.homePagePass;
     Gson gson = new Gson();
     SupervisorApproveRejectRequest supervisorApproveRequest = new SupervisorApproveRejectRequest();
@@ -36,42 +32,42 @@ public class ApproveSupervisorTest extends TestBase {
     public void test_registration() throws SQLException, ClassNotFoundException, InterruptedException, ParseException {
         tomorrow = app.getTimeStampHelper().getTimeStampWithAddSomeAmountOfDays("dd.MM.yyyy HH:mm:ss", 2);
         app.getLogin_registrationHelper().dipocketRegistration(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION", "pasword1", "1230768000000", prop.getProperty("mobile.registration.phoneNumber"), prop.getProperty("mobile.registration.email"), "dev");
-        //cliSessionId = app.getLogin_registrationHelper().loginDipocket(regPhone, regPass, HelperBase.prop.getProperty("mobile.login.deviceuuid"));
+        //cliSessionId = app.getLogin_registrationHelper().loginDipocket(app.mobileRegPhone, regPass, HelperBase.prop.getProperty("mobile.login.deviceuuid"));
     }
 
 
     @Test(priority = 1)
     public void test_ClientServices_v1_homePage_AutintificateMobileApp() throws SQLException, ClassNotFoundException, InterruptedException {
         //app.getLogin_registrationHelper().dipocketRegistration(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION", "pasword1", "1230768000000");
-        cliSessionId = app.getLogin_registrationHelper().loginDipocket(regPhone, regPass, prop.getProperty("mobile.login.deviceuuid"));
+        cliSessionId = app.getLogin_registrationHelper().loginDipocket(app.mobileRegPhone, app.mobileRegPass, prop.getProperty("mobile.login.deviceuuid"));
     }
 
     @Test(priority = 2)
     public void test_ClientServices_v1_supervision_inviteSupervisor(){
-        supervisionInviteSupervisorRequest.setValue(parentPhone);
+        supervisionInviteSupervisorRequest.setValue(app.homePageLoginPhone);
         String json = gson.toJson(supervisionInviteSupervisorRequest);
 
-        app.getClientServicesRequestsHelper().clientServices_v1_supervision_inviteSupervisor(prop.getProperty("mobile.base.url"), regPhone, regPass, cliSessionId, json);
+        app.getClientServicesRequestsHelper().clientServices_v1_supervision_inviteSupervisor(prop.getProperty("mobile.base.url"), app.mobileRegPhone, app.mobileRegPass, cliSessionId, json);
     }
 
     @Test(priority = 3)
     public void test_ClientServices_v1_tile_getMessages2() throws SQLException, ClassNotFoundException {
         childId = app.getDbHelper().getClientIdFromDB("testdipocket@gmail.com", Site.DIPOCKET.toString());
-        Response response = app.getClientServicesRequestsHelper().clientServices_v1_tile_getMessages2(cliSessionId, regPhone, regPass);
+        Response response = app.getClientServicesRequestsHelper().clientServices_v1_tile_getMessages2(cliSessionId, app.mobileRegPhone, app.mobileRegPass);
         response.then().body("communicationTileList.shortName", hasItem("Approval Pending"));
 
     }
 
     @Test(priority = 4)
     public void test_ClientServices_v1_homePage_AutintificateMobileApp_() throws SQLException, ClassNotFoundException {
-        cliSessionId = app.getLogin_registrationHelper().loginDipocket(parentPhone, parentPass, prop.getProperty("mobile.login.deviceuuid"));
+        cliSessionId = app.getLogin_registrationHelper().loginDipocket(app.homePageLoginPhone, parentPass, prop.getProperty("mobile.login.deviceuuid"));
     }
 
     @Test(priority = 5)
     public void test_ClientServices_v1_supervision_childId_acceptRequest() {
         given()
                 .spec(app.requestSpecDipocketHomePage)
-                .auth().preemptive().basic(parentPhone, parentPass)
+                .auth().preemptive().basic(app.homePageLoginPhone, parentPass)
                 .pathParam("childId", childId)
                 .contentType("application/json")
                 .header("clisessionid", cliSessionId)
