80f323068ed85de813bc3ad6b5735bcab6982397
WIN-KQQE8QJPANE\user <pavelburinskiy@gmail.com>
2023-07-03 12:22:49 +0300

    refactor

--- src/test/java/tests/bo/boTicket/TheUserNeedsToReassignTheSupervisionTicketToAnotherBOUserTest.java
+++ src/test/java/tests/bo/boTicket/TheUserNeedsToReassignTheSupervisionTicketToAnotherBOUserTest.java
@@ -1,9 +1,7 @@
 package tests.bo.boTicket;
 
-import appmanager.HelperBase;
 import base.TestBase;
 import com.cs.dipocketback.base.data.Site;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
@@ -13,45 +11,40 @@ import java.text.ParseException;
 import static appmanager.HelperBase.prop;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.hasItem;
-import static org.testng.Assert.assertEquals;
 
 public class TheUserNeedsToReassignTheSupervisionTicketToAnotherBOUserTest extends TestBase {
     String cliSessionId = null;
-    String regPhone = "380685448615";
-    String regPass = "pasword1";
     String cookie = null;
-    String username = app.BOusername;
+    //String username = app.BOusername;
     int ticketId = 0;
-    String actualTypeName = null;
     String childId =  null;
-    int supervisorId = app.homePageClientId;
     String tomorrow = null;
-    String parentPhone = app.homePageLoginPhone;
-    String parentPass = app.homePagePass;
+    //String parentPhone = app.homePageLoginPhone;
+    //String parentPass = app.homePagePass;
 
     @Test(priority = 0)
     public void test_registration() throws SQLException, ClassNotFoundException, InterruptedException, ParseException {
         tomorrow = app.getTimeStampHelper().getTimeStampWithAddSomeAmountOfDays("dd.MM.yyyy HH:mm:ss", 2);
         app.getLogin_registrationHelper().dipocketRegistration(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION", "pasword1", "1230768000000", prop.getProperty("mobile.registration.phoneNumber"), prop.getProperty("mobile.registration.email"), "dev");
-        //cliSessionId = app.getLogin_registrationHelper().loginDipocket(regPhone, regPass, HelperBase.prop.getProperty("mobile.login.deviceuuid"));
+        //cliSessionId = app.getLogin_registrationHelper().loginDipocket(prop.getProperty("mobile.registration.phoneNumber"), prop.getProperty("mobile.registration.pass"), HelperBase.prop.getProperty("mobile.login.deviceuuid"));
     }
 
 
     @Test(priority = 1)
     public void test_ClientServices_v1_homePage_AutintificateMobileApp() throws SQLException, ClassNotFoundException, InterruptedException {
         //app.getLogin_registrationHelper().dipocketRegistration(616, 985, "TERMS_AND_CONDITIONS_PL", "ELECTRONIC_COMMUNICATION", "pasword1", "1230768000000");
-        cliSessionId = app.getLogin_registrationHelper().loginDipocket(regPhone, regPass, prop.getProperty("mobile.login.deviceuuid"));
+        cliSessionId = app.getLogin_registrationHelper().loginDipocket(prop.getProperty("mobile.registration.phoneNumber"), prop.getProperty("mobile.registration.pass"), prop.getProperty("mobile.login.deviceuuid"));
     }
 
     @Test(priority = 2)
     public void test_ClientServices_v1_supervision_inviteSupervisor(){
         given()
                 .spec(app.requestSpecDipocketHomePage)
-                .auth().preemptive().basic(regPhone, regPass)
+                .auth().preemptive().basic(prop.getProperty("mobile.registration.phoneNumber"), prop.getProperty("mobile.registration.pass"))
                 .contentType("application/json")
                 .header("clisessionid", cliSessionId)
                 .body("{\n" +
-                        "  \"value\" : \""+parentPhone+"\"\n" +
+                        "  \"value\" : \""+app.homePageLoginPhone+"\"\n" +
                         "}")
                 .when()
                 .post("supervision/inviteSupervisor")
@@ -62,21 +55,21 @@ public class TheUserNeedsToReassignTheSupervisionTicketToAnotherBOUserTest exten
     @Test(priority = 3)
     public void test_ClientServices_v1_tile_getMessages2() throws SQLException, ClassNotFoundException {
         childId = app.getDbHelper().getClientIdFromDB("testdipocket@gmail.com", Site.DIPOCKET.toString());
-        Response response = app.getClientServicesRequestsHelper().clientServices_v1_tile_getMessages2(cliSessionId, regPhone, regPass);
+        Response response = app.getClientServicesRequestsHelper().clientServices_v1_tile_getMessages2(cliSessionId, prop.getProperty("mobile.registration.phoneNumber"), prop.getProperty("mobile.registration.pass"));
         response.then().body("communicationTileList.shortName", hasItem("Approval Pending"));
 
     }
 
     @Test(priority = 4)
     public void test_ClientServices_v1_homePage_AutintificateMobileApp_() throws SQLException, ClassNotFoundException {
-        cliSessionId = app.getLogin_registrationHelper().loginDipocket(parentPhone, parentPass, prop.getProperty("mobile.login.deviceuuid"));
+        cliSessionId = app.getLogin_registrationHelper().loginDipocket(app.homePageLoginPhone, app.homePagePass, prop.getProperty("mobile.login.deviceuuid"));
     }
 
     @Test(priority = 5)
     public void test_ClientServices_v1_supervision_childId_acceptRequest() {
         given()
                 .spec(app.requestSpecDipocketHomePage)
-                .auth().preemptive().basic(parentPhone, parentPass)
+                .auth().preemptive().basic(app.homePageLoginPhone, app.homePagePass)
                 .pathParam("childId", childId)
                 .contentType("application/json")
                 .header("clisessionid", cliSessionId)
@@ -89,7 +82,7 @@ public class TheUserNeedsToReassignTheSupervisionTicketToAnotherBOUserTest exten
 
     @Test(priority = 7)
     public void test_BOServices_v1_auth_authentication() {
-        cookie = app.getBoRequestsHelper().boServices_v1_auth_authentication(app.BOuserLogin, app.BOuserPass, username);
+        cookie = app.getBoRequestsHelper().boServices_v1_auth_authentication(app.BOuserLogin, app.BOuserPass, app.BOusername);
     }
 
     @Test(priority = 8)
